6561a9955b7bb6ac3f24d5c4c17e337ce294e9ed
elasticlog <elasticlog01@gmail.com>
2017-03-30 10:30:39 +0000

    add random and skiplist insert

--- src/base/skiplist.h
+++ src/base/skiplist.h
@@ -14,11 +14,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef RTIDB_SKIPLIST_H
-#define RTIDB_SKIPLIST_H
+#ifndef RTIDB_BASE_SKIPLIST_H
+#define RTIDB_BASE_SKIPLIST_H
 
 #include <stdint.h>
 #include <boost/atomic.hpp>
+#include "base/random.h"
 
 namespace rtidb {
 namespace base {
@@ -28,16 +29,25 @@ template<class T>
 class Node {
 
 public:
+    // Set data reference and Node height
     Node(const T& data, uint32_t height):data_(data), 
     height_(height){
         nexts_ = new boost::atomic< Node<T>* >[height];
     }
-    ~Node() {}
+    ~Node() {
+        delete[] nexts_;
+    }
 
-    // Set the next reference
+    // Set the next node with memory barrier
     void SetNext(uint32_t level, Node<T>* node) {
         assert(level < height_ && level >= 0);
         nexts_[level].store(node, boost::memory_order_release);
+    }
+
+    // Set the next node without memory barrier
+    void SetNextNoBarrier(uint32_t level, Node<T>* node) {
+        assert(level < height_ && level >= 0);
+        nexts_[level].store(node, boost::memory_order_relaxed);
     }
 
     uint32_t Height() {
@@ -49,7 +59,12 @@ public:
         return nexts_[level].load(boost::memory_order_acquire);
     }
 
-    const T GetData() const{
+    Node<T>* GetNextNoBarrier(uint32_t level) {
+        assert(level < height_ && level >= 0);
+        return nexts_[level].load(boost::memory_order_relaxed);
+    }
+
+    const T& GetData() const{
         return data_;
     }
 
@@ -57,6 +72,141 @@ private:
     T const data_;
     boost::atomic< Node<T>* >* nexts_;
     uint32_t const height_;
+};
+
+template<class T, class Comparator>
+class Skiplist {
+
+public:
+    Skiplist(uint32_t max_height, uint32_t branch, const Comparator& compare):MaxHeight(max_height),
+    Branch(branch),
+    head_(NULL),
+    compare_(compare),
+    rand_(0xdeadbeef){
+        head_ = new Node<T>(0, MaxHeight);
+        for (uint32_t i = 0; i < head_->Height(); i++) {
+            head_->SetNext(i, NULL);
+        }
+        max_height_.store(1, boost::memory_order_relaxed);
+    }
+    ~Skiplist() {}
+
+    // Insert need external synchronized
+    void Insert(const T& data) {
+        uint32_t height = RandomHeight();
+        Node<T>* after[MaxHeight];
+        FindGreaterOrEqual(data, after);
+        if (height > GetMaxHeight()) {
+            for (uint32_t i = GetMaxHeight(); i < height; i++ ) {
+                after[i] = head_;
+            }
+            max_height_.store(height, boost::memory_order_relaxed);
+        }
+        Node<T>* node = NewNode(data, height);
+        for (int i = 0; i < height; i ++) {
+            node->SetNextNoBarrier(i, after[i]->GetNextNoBarrier(i));
+            after[i]->SetNext(i, node);
+        }
+    }
+
+    class Iterator {
+    public:
+        Iterator(Skiplist<T, Comparator>* list):node_(NULL),
+        list_(list) {}
+        ~Iterator() {}
+
+        bool Valid() const {
+            return node_ != NULL;
+        }
+
+        void Next() {
+            assert(Valid());
+            node_ = node_->GetNext(0);
+        }
+
+        const T& GetData() const {
+            assert(Valid());
+            return node_->GetData();
+        }
+
+        void Seek(const T& data) {
+            node_ = list_->FindLessThan(data);
+        }
+
+    private:
+        Node<T>* node_;
+        Skiplist<T, Comparator>* const list_;
+    };
+
+    Iterator* NewIterator() {
+        return new Iterator(this);
+    }
+private:
+
+    Node<T>* NewNode(const T& data, uint32_t height) {
+        Node<T>* node = new Node<T>(data, height); 
+        return node;
+    }
+
+    uint32_t RandomHeight() {
+        uint32_t height = 1;
+        while (height < MaxHeight && (rand_.Next() % Branch) == 0) {
+            height ++;
+        }
+        return height;
+    }
+
+    void FindGreaterOrEqual(const T& data, Node<T>** nodes) {
+        assert(nodes != NULL);
+        Node<T>* node = head_;
+        uint32_t level = GetMaxHeight() - 1;
+        while (true) {
+            Node<T>* next = node->GetNext(level);
+            if (IsAfterNode(data, next)) {
+                node = next;
+            }else {
+                nodes[level] = node;
+                if (level <= 0) {
+                    return;
+                }
+                level--;
+            }
+        }
+    }
+
+    Node<T>* FindLessThan(const T& data) {
+        Node<T>* node = head_;
+        uint32_t level = GetMaxHeight() - 1;
+        while (true) {
+            assert(node == head_ || compare_(node->GetData(), data) < 0);
+            Node<T>* next = node->GetNext(level);
+            if (next == NULL || compare_(next->GetData() , data) >=0) {
+                if (level <= 0) {
+                    return node;
+                }
+                level --;
+            } else {
+                node = next;
+            }
+        }
+    }
+
+    bool IsAfterNode(const T& data, const Node<T>* node) {
+        return (node != NULL) && (compare_(data, node->GetData()) > 0);
+    } 
+
+    uint32_t GetMaxHeight() {
+        return max_height_.load(boost::memory_order_relaxed);
+    }
+
+private:
+    uint32_t const MaxHeight;
+    uint32_t const Branch;
+    Node<T>* head_;
+    Comparator const compare_;
+    boost::atomic<uint32_t> max_height_; 
+    Random rand_;
+    friend Iterator;
 };
 
 }// base
--- src/base/skiplist_test.cc
+++ src/base/skiplist_test.cc
@@ -27,7 +27,24 @@ public:
     ~NodeTest() {}
 };
 
+class SkiplistTest : public ::testing::Test {
 
+public:
+    SkiplistTest(){}
+    ~SkiplistTest() {}
+
+};
+
+struct Comparator {
+    int operator()(const uint32_t a, const uint32_t b) const {
+        if (a > b) {
+            return 1;
+        }else if (a == b) {
+            return 0;
+        }
+        return -1;
+    }
+};
 
 TEST_F(NodeTest, SetNext) {
     Node<uint32_t> node(1, 2);
@@ -36,6 +53,30 @@ TEST_F(NodeTest, SetNext) {
     const Node<uint32_t>* node_ptr = node.GetNext(1);
     const uint32_t result = node_ptr->GetData();
     ASSERT_EQ(3, result);
+}
+
+TEST_F(NodeTest, SetNext2) {
+    uint32_t data1 = 1;
+    uint32_t data2 = 3;
+    Node<uint32_t*> node(&data1, 2);
+    Node<uint32_t*> node2(&data2, 2);
+    node.SetNext(1, &node2);
+    const Node<uint32_t*>* node_ptr = node.GetNext(1);
+    const uint32_t* result = node_ptr->GetData();
+    ASSERT_EQ(3, *result);
+}
+
+TEST_F(SkiplistTest, Insert) {
+    Comparator cmp;
+    Skiplist<uint32_t, Comparator> sl(12, 4, cmp);
+    sl.Insert(1);
+    sl.Insert(2);
+    Skiplist<uint32_t, Comparator>::Iterator* it = sl.NewIterator();
+    it->Seek(0);
+    it->Next();
+    ASSERT_EQ(1, it->GetData());
+    it->Next();
+    ASSERT_EQ(2, it->GetData());
 }
 
 
