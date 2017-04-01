d0ccd28b1cecc1ea81a751cd9ad2a5154aff8d4e
elasticlog <elasticlog01@gmail.com>
2017-04-01 12:01:16 +0800

    add ritdb cmd

---
+++ src/cmd/rtidb.cc
@@ -0,0 +1,13 @@
+//
+// rtidb.cc
+// Copyright (C) 2017 4paradigm.com
+// Author wangtaize 
+// Date 2017-03-31
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "hello rtidb" << std::endl;
+    return 0;
+}
