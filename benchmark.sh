2bba1e92026593363045891e2acb9bb715eafd52
elasticlog <elasticlog01@gmail.com>
2017-04-16 22:38:00 +0800

    add benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+#
+# benchmark.sh
+
+
+
+ROOT_DIR=`pwd`
+
+clear_debug() {
+    ps -ef | grep rtidb | grep 9426 | awk '{print $2}' | while read line; do kill -9 $line; done
+}
+
+clear_debug
+
+./build/bin/rtidb --log_level=debug --gc_safe_offset=0 --gc_interval=1 --endpoint=0.0.0.0:9426 --role=tablet > log 2>&1 &
+
+sleep 2
+
+./build/bin/rtidb --cmd="create t0 1 1 0" --role=client --endpoint=127.0.0.1:9426 --interactive=false 
+
+./build/bin/rtidb --cmd="benput pk 40 10" --role=client --endpoint=127.0.0.1:9426 --interactive=false
+
+./build/bin/rtidb --cmd="benscan pk 10" --role=client --endpoint=127.0.0.1:9426 --interactive=false
+
+clear_debug
+
