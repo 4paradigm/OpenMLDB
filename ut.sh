0aad0e21fdfd8170b593276c0314acc5b628d818
wangtaize <wangtaize@4paradigm.com>
2017-05-23 15:17:32 +0800

    update ut.sh

--- ut.sh
+++ ut.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set -e -u -E # this script will exit if any sub-command fails
-export HEAPCHECK=normal
 WORK_DIR=`pwd`
 test -d /tmp/rtidb && cd /tmp/rtidb && rm -rf *
 cd $WORK_DIR
-cd $WORK_DIR/build/bin && ls | grep test | while read line; do ./$line ; done
+cd $WORK_DIR/build/bin && ls | grep test | grep -v mem | while read line; do ./$line ; done
+cd $WORK_DIR/build/bin && HEAPCHECK=normal ./tablet_impl_mem_test
