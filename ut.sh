e2909c2fe223c0714d35891dd1ea71d611f27b46
wangtaize <wangtaize@4paradigm.com>
2017-05-24 14:33:58 +0800

    add gc mem leak test

--- ut.sh
+++ ut.sh
@@ -4,4 +4,4 @@ WORK_DIR=`pwd`
 test -d /tmp/rtidb && cd /tmp/rtidb && rm -rf *
 cd $WORK_DIR
 cd $WORK_DIR/build/bin && ls | grep test | grep -v mem | while read line; do ./$line ; done
-cd $WORK_DIR/build/bin && HEAPCHECK=normal ./tablet_impl_mem_test
+cd $WORK_DIR/build/bin && ls | grep test | grep mem | while read line; do HEAPCHECK=normal ./$line; done
