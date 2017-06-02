88aab38307303ef9ccffd5a0b6311e7dbd3254ea
elasticlog <elasticlog01@gmail.com>
2017-06-02 11:27:40 +0800

    fix java ut fails

--- src/flags.cc
+++ src/flags.cc
@@ -11,6 +11,7 @@ DEFINE_int32(gc_interval, 120, "the gc interval of tablet every two hour");
 DEFINE_int32(gc_pool_size, 2, "the size of tablet gc thread pool");
 DEFINE_int32(gc_safe_offset, 1, "the safe offset of tablet gc in minute");
 DEFINE_int32(statdb_ttl, 30 * 24 * 60 , "the ttl of statdb");
+DEFINE_double(mem_release_rate, 5 , "specify memory release rate, which should be in 0 ~ 10");
 DEFINE_string(db_root_path,"/tmp/", "the root path of db");
 DEFINE_bool(enable_statdb, false, "enable statdb");
 
