9cd3d1302561433a4b825e4c9f6d518b732ac648
wangtaize <wangtaize@4paradigm.com>
2017-07-18 14:36:28 +0800

    add onebox script

---
+++ steps/leader_follower_onebox.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+#
+# cluster_run.sh
+
+ROOT=`pwd`
+
+clear_debug() {
+    ps xf | grep rtidb | grep -v grep | awk '{print $1}' | while read line; do kill -9 $line; done
+}
+
+clear_debug
+
+test -d $ROOT/binlog1 && rm -rf $ROOT/binlog1
+test -d $ROOT/binlog2 && rm -rf $ROOT/binlog2
+test -d $ROOT/binlog3 && rm -rf $ROOT/binlog3
+
+./build/bin/rtidb --binlog_root_path=$ROOT/binlog1 --binlog_single_file_max_size=8 --log_level=debug --gc_safe_offset=0 --gc_interval=1 --endpoint=0.0.0.0:19527 --role=tablet >log0 2>&1 &
+./build/bin/rtidb --binlog_root_path=$ROOT/binlog2 --binlog_single_file_max_size=8 --log_level=debug --gc_safe_offset=0 --gc_interval=1 --endpoint=0.0.0.0:19528 --role=tablet >log1 2>&1 &
+./build/bin/rtidb --binlog_root_path=$ROOT/binlog3 --binlog_single_file_max_size=8 --log_level=debug --gc_safe_offset=0 --gc_interval=1 --endpoint=0.0.0.0:19529 --role=tablet >log2 2>&1 &
+sleep 1
+
+./build/bin/rtidb --cmd="create t1 2 1 0 true" --role=client --endpoint=127.0.0.1:19527 --interactive=false 
+./build/bin/rtidb --cmd="create t1 1 1 0 true" --role=client --endpoint=127.0.0.1:19527 --interactive=false 
+
+sleep 1
+./build/bin/rtidb --cmd="benchmark" --role=client --endpoint=127.0.0.1:19527 --interactive=false  | grep Percentile
+
