f83b7dfc2c8cc6c475bf67e7bdc18f20a397e322
denglong <denglong@4paradigm.com>
2017-08-28 18:26:32 +0800

    update case

--- src/log/log_test.cc
+++ src/log/log_test.cc
@@ -17,6 +17,9 @@
 #include <boost/atomic.hpp>
 #include "base/file_util.h"
 #include <iostream>
+#include "log/crc32c.h"
+#include "log/coding.h"
+#include <vector>
 
 using ::rtidb::base::Slice;
 using ::rtidb::base::Status;
@@ -33,6 +36,85 @@ public:
 
 inline std::string GenRand() {
     return boost::lexical_cast<std::string>(rand() % 10000000 + 1);
+}
+
+uint32_t type_crc_[kMaxRecordType + 1];
+int block_offset_ = 0;
+
+void InitTypeCrc(uint32_t* type_crc) {
+    for (int i = 0; i <= kMaxRecordType; i++) {
+        char t = static_cast<char>(i);
+        type_crc[i] = Value(&t, 1);
+    }
+}
+
+void GenPhysicalRecord(RecordType t, const char* ptr, size_t n, int& block_offset_, std::vector<std::string>& rec_vec) {
+    assert(n <= 0xffff);  // Must fit in two bytes
+    assert(block_offset_ + kHeaderSize + n <= kBlockSize);
+    // Format the header
+    char buf[kHeaderSize];
+    buf[4] = static_cast<char>(n & 0xff);
+    buf[5] = static_cast<char>(n >> 8);
+    buf[6] = static_cast<char>(t);
+
+    // Compute the crc of the record type and the payload.
+    uint32_t crc = Extend(type_crc_[t], ptr, n);
+    crc = Mask(crc);                 // Adjust for storage
+    EncodeFixed32(buf, crc);
+
+    std::string str(buf, kHeaderSize);
+    str.append(ptr, n);
+    rec_vec.push_back(str);
+
+    block_offset_ += kHeaderSize + n;
+}
+
+int AddRecord(const Slice& slice, std::vector<std::string>& vec) {
+    const char* ptr = slice.data();
+    size_t left = slice.size();
+
+    InitTypeCrc(type_crc_);
+
+    // Fragment the record if necessary and emit it.  Note that if slice
+    // is empty, we still want to iterate once to emit a single
+    // zero-length record
+    Status s;
+    bool begin = true;
+    do {
+        const int leftover = kBlockSize - block_offset_;
+        assert(leftover >= 0);
+        if (leftover < kHeaderSize) {
+          // Switch to a new block
+            if (leftover > 0) {
+                // Fill the trailer (literal below relies on kHeaderSize being 7)
+                assert(kHeaderSize == 7);
+                vec.push_back(std::string("\x00\x00\x00\x00\x00\x00"));
+            }
+            block_offset_ = 0;
+        }
+        // Invariant: we never leave < kHeaderSize bytes in a block.
+        assert(kBlockSize - block_offset_ - kHeaderSize >= 0);
+
+        const size_t avail = kBlockSize - block_offset_ - kHeaderSize;
+        const size_t fragment_length = (left < avail) ? left : avail;
+
+        RecordType type;
+        const bool end = (left == fragment_length);
+        if (begin && end) {
+            type = kFullType;
+        } else if (begin) {
+            type = kFirstType;
+        } else if (end) {
+            type = kLastType;
+        } else {
+            type = kMiddleType;
+        }
+        GenPhysicalRecord(type, ptr, fragment_length, block_offset_, vec);
+        ptr += fragment_length;
+        left -= fragment_length;
+        begin = false;
+    } while (left > 0);
+    return 0;
 }
 
 TEST_F(LogWRTest, TestWriteAndRead) {
@@ -141,10 +223,8 @@ TEST_F(LogWRTest, TestWait) {
     Slice sval(val.c_str(), val.size());
     Status status = writer.AddRecord(sval);
     ASSERT_TRUE(status.ok());
-    std::string val1(1024 * 4, 'b');
-    Slice sval1(val1.c_str(), val1.size());
-    status = writer.AddRecord(sval1);
-    ASSERT_TRUE(status.ok());
+
+    block_offset_ = 1024 * 5 + kHeaderSize * 2  - kBlockSize;
 
     FILE* fd_r = fopen(full_path.c_str(), "rb");
     ASSERT_TRUE(fd_r != NULL);
@@ -158,21 +238,25 @@ TEST_F(LogWRTest, TestWait) {
     value.clear();
     scratch2.clear();
 
-    status = reader.ReadRecord(&value, &scratch2);
-    ASSERT_TRUE(status.ok());
-    ASSERT_EQ(1024 * 4, value.size());
-    value.clear();
-    scratch2.clear();
-
     // write partial record less than kHeadsize
-    status = wf->Append(Slice("xxx", strlen("xxx")));
+    std::string val3(1024 * 5, 'c');
+    Slice sval3(val3.c_str(), val3.size());
+    std::vector<std::string> vec;
+    AddRecord(sval3, vec);
+    ASSERT_EQ(2, vec.size());
+
+    status = wf->Append(Slice(vec[0].c_str(), vec[0].length()));
     ASSERT_TRUE(status.ok());
     wf->Flush();
     status = reader.ReadRecord(&value, &scratch2);
     ASSERT_TRUE(status.IsWaitRecord());
 
+
+    status = wf->Append(Slice(vec[1].c_str(), vec[1].length()));
+    wf->Flush();
     status = reader.ReadRecord(&value, &scratch2);
-    ASSERT_TRUE(status.IsWaitRecord());
+    ASSERT_TRUE(status.ok());
+    ASSERT_EQ(1024 * 5, value.size());
 }
 /*TEST_F(LogWRTest, ReadAllLogs) {
     std::string log_dir = "/tmp/";
