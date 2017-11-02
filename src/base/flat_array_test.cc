0141ae5b8baa7901dceea70cd08064a605eeda6e
wangtaize <wangtaize@4paradigm.com>
2017-11-02 21:01:02 +0800

    merge from schema

---
+++ src/base/flat_array_test.cc
@@ -0,0 +1,56 @@
+//
+// flat_array_test.cc
+// Copyright 2017 4paradigm.com 
+
+
+#include "base/flat_array.h"
+#include "gtest/gtest.h"
+#include "base/strings.h"
+#include <iostream>
+
+namespace rtidb {
+namespace base {
+
+class FlatArrayTest : public ::testing::Test {
+
+public:
+    FlatArrayTest() {}
+    ~FlatArrayTest() {}
+};
+
+TEST_F(FlatArrayTest, Encode) {
+    std::string buffer;
+    FlatArrayCodec codec(&buffer, 2);
+    bool ok = codec.Append(1.2f);
+    ASSERT_TRUE(ok);
+    ok = codec.Append("helloworld");
+    ASSERT_TRUE(ok);
+    codec.Build();
+    ASSERT_TRUE(buffer.size() == 19);
+    std::cout << ::rtidb::base::DebugString(buffer) << std::endl;
+    FlatArrayIterator it(buffer.c_str(), buffer.size());
+    ASSERT_EQ(kFloat, it.GetType());
+    ASSERT_TRUE(it.Valid());
+    ASSERT_EQ(2, it.Size());
+    float value = 0;
+    ok = it.GetFloat(&value);
+    ASSERT_TRUE(ok);
+    ASSERT_EQ(1.2f, value);
+    std::cout << value << std::endl;
+    std::string value2;
+    it.Next();
+    ASSERT_TRUE(it.Valid());
+    ok = it.GetString(&value2);
+    ASSERT_TRUE(ok);
+    ASSERT_EQ("helloworld", value2);
+    it.Next();
+    ASSERT_FALSE(it.Valid());
+}
+
+}
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
