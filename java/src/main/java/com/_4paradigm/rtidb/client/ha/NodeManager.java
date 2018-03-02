77562a70f9f37568ad32b7031fc749c0ff764157
wangtaize <wangtaize@4paradigm.com>
2018-03-02 07:31:49 +0000

    style: remove unuse endpoint set var

--- java/src/main/java/com/_4paradigm/rtidb/client/ha/NodeManager.java
+++ java/src/main/java/com/_4paradigm/rtidb/client/ha/NodeManager.java
@@ -1,7 +1,6 @@
 package com._4paradigm.rtidb.client.ha;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -16,7 +15,6 @@ import io.brpc.client.RpcBaseClient;
 public class NodeManager {
     private final static Logger logger = LoggerFactory.getLogger(NodeManager.class);
     private volatile Map<EndPoint, BrpcChannelGroup> endpoints = new HashMap<EndPoint, BrpcChannelGroup>();
-    private volatile Set<EndPoint> endpointSet = new HashSet<EndPoint>();
     private RpcBaseClient bs;
 
     public NodeManager(RpcBaseClient bs) {
@@ -28,19 +26,17 @@ public class NodeManager {
     }
 
     public Set<EndPoint> getEndpointSet() {
-        return endpointSet;
+        return endpoints.keySet();
     }
 
     public void update(Set<EndPoint> aliveEndpointSet) {
         Map<EndPoint, BrpcChannelGroup> oldEndpoints = endpoints;
-        Set<EndPoint> oldEndpointSet = endpointSet;
+        Set<EndPoint> oldEndpointSet = oldEndpoints.keySet();
         // new add endpoint
         Map<EndPoint, BrpcChannelGroup> newEndpoints = new HashMap<EndPoint, BrpcChannelGroup>();
-        Set<EndPoint> newEndpointSet = new HashSet<EndPoint>();
         Iterator<EndPoint> it = aliveEndpointSet.iterator();
         while (it.hasNext()) {
             EndPoint endpoint = it.next();
-            newEndpointSet.add(endpoint);
             if (!oldEndpoints.containsKey(endpoint)) {
                 // new add endpoint
                 newEndpoints.put(endpoint, new BrpcChannelGroup(endpoint.getIp(), endpoint.getPort(),
@@ -54,8 +50,6 @@ public class NodeManager {
 
         // swap
         endpoints = newEndpoints;
-        endpointSet = newEndpointSet;
-
         // close dead endpoint
         it = oldEndpointSet.iterator();
         while (it.hasNext()) {
@@ -68,7 +62,6 @@ public class NodeManager {
             logger.warn("close dead endpoint {}", endpoint);
         }
         oldEndpoints.clear();
-        oldEndpointSet.clear();
     }
     
     public void close() {
