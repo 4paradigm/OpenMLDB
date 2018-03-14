53672c2c3af78a966e0870151325a47969b17bf4
denglong <denglong@4paradigm.com>
2018-03-14 13:37:50 +0800

    fix: fix ut case

--- steps/java_ut.sh
+++ steps/java_ut.sh
@@ -8,7 +8,7 @@ clear_debug() {
     ps -ef | grep rtidb | grep 9501 | awk '{print $2}' | while read line; do kill -9 $line; done
 }
 PROTO_BIN=$ROOT_DIR/thirdparty/bin/protoc
-
+ulimit -c unlimited
 sed -i "/protocExecutable/c\<protocExecutable>${PROTO_BIN}<\/protocExecutable>" java/pom.xml
 mkdir -p java/src/main/proto/rtidb/api
 mkdir -p java/src/main/proto/rtidb/nameserver
@@ -16,9 +16,10 @@ cp -rf src/proto/tablet.proto java/src/main/proto/rtidb/api/
 cp -rf src/proto/name_server.proto java/src/main/proto/rtidb/nameserver/
 
 clear_debug
-test -d /tmp/ut_zookeeper && rm -rf /tmp/ut_zookeeper
 cp steps/zoo.cfg thirdsrc/zookeeper-3.4.10/conf
-cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh start && cd $ROOT_DIR
+cd thirdsrc/zookeeper-3.4.10
+test -d ut_zookeeper && rm -rf ut_zookeeper
+./bin/zkServer.sh start && cd $ROOT_DIR
 
 ./build/bin/rtidb --db_root_path=/tmp/$RANDOM --log_level=debug --gc_safe_offset=0 --gc_interval=1 --endpoint=0.0.0.0:9501 --role=tablet &
 
