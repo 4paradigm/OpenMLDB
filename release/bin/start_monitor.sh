01a70f508240354ed4fe0678bb7f78019bef339e
denglong <denglong@4paradigm.com>
2018-05-17 10:53:26 +0800

    doc: disable THP

--- release/bin/start_monitor.sh
+++ release/bin/start_monitor.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 #
-# start.sh
-CUR_DIR=`pwd`
-P_DIR=`dirname $CUR_DIR`
-cd $P_DIR && ./bin/mon ./bin/boot_monitor.sh -d -l ./logs/rtidb_mon_m.log
+# start_monitor.sh
+./bin/mon ./bin/boot_monitor.sh -d -l ./logs/rtidb_mon_m.log
 
--- release/bin/start_ns.sh
+++ release/bin/start_ns.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 #
 # start_ns.sh
-CUR_DIR=`pwd`
-P_DIR=`dirname $CUR_DIR`
-cd $P_DIR && ./bin/mon ./bin/boot_ns.sh -d -l ./logs/rtidb_ns_mon.log
+./bin/mon ./bin/boot_ns.sh -d -l ./logs/rtidb_ns_mon.log
