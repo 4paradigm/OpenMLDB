0687d2ad7a889f5a605d595629902f6999a570f2
HA <ha@denglongdeMBP.4pd.io>
2018-06-06 11:07:09 +0800

    test: add scan limit case

--- java/src/test/java/com/_4paradigm/rtidb/client/ut/ha/TableSyncClientTest.java
+++ java/src/test/java/com/_4paradigm/rtidb/client/ut/ha/TableSyncClientTest.java
@@ -168,6 +168,39 @@ public class TableSyncClientTest {
             nsc.dropTable(name);
         }
     }
+
+    @Test
+    public void testScanLimit() {
+        String name = createKvTable();
+        try {
+            boolean ok = tableSyncClient.put(name, "test1", 9527, "value0");
+            Assert.assertTrue(ok);
+            ok = tableSyncClient.put(name, "test1", 9528, "value1");
+            Assert.assertTrue(ok);
+            ok = tableSyncClient.put(name, "test1", 9529, "value2");
+            Assert.assertTrue(ok);
+            KvIterator it = tableSyncClient.scan(name, "test1", 9529, 1000, 2);
+            Assert.assertTrue(it.getCount() == 2);
+            Assert.assertTrue(it.valid());
+            byte[] buffer = new byte[6];
+            it.getValue().get(buffer);
+            String value = new String(buffer);
+            Assert.assertEquals(value, "value2");
+            it.next();
+
+            Assert.assertTrue(it.valid());
+            it.getValue().get(buffer);
+            value = new String(buffer);
+            Assert.assertEquals(value, "value1");
+            it.next();
+            Assert.assertFalse(it.valid());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }finally {
+            nsc.dropTable(name);
+        }
+    }
     
     @Test
     public void testSchemaPutForMap() {
