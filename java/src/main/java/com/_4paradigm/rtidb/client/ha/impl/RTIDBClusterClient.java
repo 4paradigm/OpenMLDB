de59335507696f7bcebd3cf4b42fdadde4429f8e
wangtaize <wangtaize@4paradigm.com>
2018-06-26 08:46:10 +0000

    fix: add null check

--- java/src/main/java/com/_4paradigm/rtidb/client/ha/impl/RTIDBClusterClient.java
+++ java/src/main/java/com/_4paradigm/rtidb/client/ha/impl/RTIDBClusterClient.java
@@ -166,7 +166,7 @@ public class RTIDBClusterClient implements Watcher, RTIDBClient {
 
     private void tryWatch() {
         try {
-            if (!zookeeper.getState().isConnected()) {
+            if (zookeeper = null && !zookeeper.getState().isConnected()) {
                 connectToZk();
                 onZkConnected();
             } 
