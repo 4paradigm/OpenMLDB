a70c3c9fa8e45950c7032665d8d7a5fc5352c25f
wangtaize <wangtaize@4paradigm.com>
2018-06-30 09:34:13 +0000

    feat:revert start oneboxt

--- onebox/start_onebox.sh
+++ onebox/start_onebox.sh
@@ -9,9 +9,9 @@ test -d tablet0-binlogs && rm -rf tablet0-binlogs
 test -d recycle_bin0 && rm -rf recycle_bin0
 ../build/bin/rtidb --db_root_path=tablet0-binlogs \
                    --recycle_bin_root_path=recycle_bin0 \
-                   --endpoint=192.168.33.10:9520 --role=tablet \
+                   --endpoint=127.0.0.1:9520 --role=tablet \
                    --binlog_notify_on_put=true\
-                   --zk_cluster=192.168.33.10:6181\
+                   --zk_cluster=127.0.0.1:6181\
                    --zk_keep_alive_check_interval=100000000\
                    --zk_root_path=/onebox > tablet0.log 2>&1 &
 test -d tablet1-binlogs && rm -rf tablet1-binlogs
@@ -21,8 +21,8 @@ test -d recycle_bin1 && rm -rf recycle_bin1
 # start tablet1
 ../build/bin/rtidb --db_root_path=tablet1-binlogs \
                    --recycle_bin_root_path=recycle_bin1 \
-                   --endpoint=192.168.33.10:9521 --role=tablet \
-                   --zk_cluster=192.168.33.10:6181\
+                   --endpoint=127.0.0.1:9521 --role=tablet \
+                   --zk_cluster=127.0.0.1:6181\
                    --binlog_notify_on_put=true\
                    --zk_keep_alive_check_interval=100000000\
                    --zk_root_path=/onebox > tablet1.log 2>&1 &
@@ -33,26 +33,26 @@ test -d recycle_bin2 && rm -rf recycle_bin2
 # start tablet2
 ../build/bin/rtidb --db_root_path=tablet2-binlogs \
                    --recycle_bin_root_path=recycle_bin2 \
-                   --endpoint=192.168.33.10:9522 --role=tablet \
+                   --endpoint=127.0.0.1:9522 --role=tablet \
                    --binlog_notify_on_put=true\
-                   --zk_cluster=192.168.33.10:6181\
+                   --zk_cluster=127.0.0.1:6181\
                    --zk_keep_alive_check_interval=100000000\
                    --zk_root_path=/onebox > tablet2.log 2>&1 &
 
 
 # start ns1 
-../build/bin/rtidb --endpoint=192.168.33.10:9622 --role=nameserver \
-                   --zk_cluster=192.168.33.10:6181\
+../build/bin/rtidb --endpoint=127.0.0.1:9622 --role=nameserver \
+                   --zk_cluster=127.0.0.1:6181\
                    --zk_root_path=/onebox > ns1.log 2>&1 &
 
 # start ns2 
-../build/bin/rtidb --endpoint=192.168.33.10:9623 --role=nameserver \
-                   --zk_cluster=192.168.33.10:6181\
+../build/bin/rtidb --endpoint=127.0.0.1:9623 --role=nameserver \
+                   --zk_cluster=127.0.0.1:6181\
                    --zk_root_path=/onebox > ns2.log 2>&1 &
 
 # start ns3 
-../build/bin/rtidb --endpoint=192.168.33.10:9624 --role=nameserver \
-                   --zk_cluster=192.168.33.10:6181\
+../build/bin/rtidb --endpoint=127.0.0.1:9624 --role=nameserver \
+                   --zk_cluster=127.0.0.1:6181\
                    --zk_root_path=/onebox > ns3.log 2>&1 &
 
 sleep 3
