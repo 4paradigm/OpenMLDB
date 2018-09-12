cece503c934ce34c1dabb035f3c39d7d1431cfbd
denglong <denglong@4paradigm.com>
2018-09-12 18:14:39 +0800

    fix: add so lib

--- docker/copy_file.sh
+++ docker/copy_file.sh
@@ -3,6 +3,12 @@ cp ./build/bin/rtidb ./docker/tablet/bin/
 cp ./build/bin/rtidb ./docker/nameserver/bin/
 cp ./release/conf/tablet.flags ./docker/tablet/
 cp ./release/conf/nameserver.flags ./docker/nameserver/
+mkdir -p ./docker/tablet/lib
+mkdir -p ./docker/nameserver/lib
+wget http://pkg.4paradigm.com:81/rtidb/libstdc++.so.6 >/dev/null
+cp libstdc++.so.6 ./docker/tablet/lib/ 
+cp libstdc++.so.6 ./docker/nameserver/lib/ 
+exit
 cd docker/tablet
 if [ -f "py_env_succ" ]
 then
