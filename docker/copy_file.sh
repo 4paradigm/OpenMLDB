0172587e0ff77d37692502d996691e795e69de78
denglong <denglong@4paradigm.com>
2018-09-12 18:19:01 +0800

    fix: rm debug code

--- docker/copy_file.sh
+++ docker/copy_file.sh
@@ -8,7 +8,6 @@ mkdir -p ./docker/nameserver/lib
 wget http://pkg.4paradigm.com:81/rtidb/libstdc++.so.6 >/dev/null
 cp libstdc++.so.6 ./docker/tablet/lib/ 
 cp libstdc++.so.6 ./docker/nameserver/lib/ 
-exit
 cd docker/tablet
 if [ -f "py_env_succ" ]
 then
