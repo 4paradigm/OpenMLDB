0cbccf4df0de658dcb5a625c671dae4a0efa1ac4
denglong <denglong@4paradigm.com>
2018-11-21 14:30:49 +0800

    test: update case

--- test-common/integrationtest/testcase/test_ns_client_offlineendpoint.py
+++ test-common/integrationtest/testcase/test_ns_client_offlineendpoint.py
@@ -153,17 +153,27 @@ class TestOfflineEndpoint(TestCaseBase):
             ('table_partition', '"{}"'.format(self.slave2), '"2-3"', 'false'),
             ('table_partition', '"{}"'.format(self.leader), '"4-6"', 'false'),
             ('table_partition', '"{}"'.format(self.slave1), '"4-6"', 'true'),
+            ('column_desc', '"k1"', '"string"', 'true'),
+            ('column_desc', '"k2"', '"string"', 'false'),
+            ('column_desc', '"k3"', '"string"', 'false')
         )
         utils.gen_table_metadata_file(m, metadata_path)
         rs1 = self.ns_create(self.ns_leader, metadata_path)
         self.assertIn('Create table ok', rs1)
+        rs = self.showtable(self.ns_leader)
+        tid = rs.keys()[0][1]
+
+        rs = self.put(self.leader, tid, 1, '', self.now(), 'pk1' ,'v2', 'v3')
+        self.assertIn('Put ok', rs)
+        rs = self.put(self.leader, tid, 2, '', self.now(), 'pk2' ,'v5', 'v6')
+        self.assertIn('Put ok', rs)
+        time.sleep(1)
 
         rs2 = self.offlineendpoint(self.ns_leader, self.leader)
         self.assertIn("offline endpoint ok", rs2)
         time.sleep(10)
 
         rs3 = self.showtable(self.ns_leader)
-        tid = rs3.keys()[0][1]
         self.assertEqual(rs3[(name, tid, '0', self.leader)], ['leader', '144000min', 'no', 'kNoCompress'])
         self.assertEqual(rs3[(name, tid, '1', self.leader)], ['leader', '144000min', 'no', 'kNoCompress'])
         self.assertEqual(rs3[(name, tid, '2', self.leader)], ['leader', '144000min', 'no', 'kNoCompress'])
@@ -171,6 +181,8 @@ class TestOfflineEndpoint(TestCaseBase):
         self.assertEqual(rs3[(name, tid, '1', self.slave1)], ['leader', '144000min', 'yes', 'kNoCompress'])
         self.assertEqual(rs3[(name, tid, '3', self.slave2)], ['leader', '144000min', 'yes', 'kNoCompress'])
         self.assertEqual(rs3[(name, tid, '4', self.leader)], ['follower', '144000min', 'no', 'kNoCompress'])
+        self.assertIn('v2', self.scan(self.slave1, tid, 1, {'k1': 'pk1'}, self.now(), 1))
+        self.assertIn('v5', self.scan(self.slave1, tid, 2, {'k1': 'pk2'}, self.now(), 1))
 
 
 if __name__ == "__main__":
