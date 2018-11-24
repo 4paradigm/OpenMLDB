3a307544a223e37f35d4cb2a09abc5d224539994
wangzixian@4paradigm.com <wangzixian@4paradigm>
2018-11-24 11:28:16 +0800

    fix:change type u to type lu

--- src/tablet/tablet_impl.cc
+++ src/tablet/tablet_impl.cc
@@ -1019,7 +1019,7 @@ void TabletImpl::MakeSnapshotInternal(uint32_t tid, uint32_t pid, std::shared_pt
     uint64_t cur_offset = replicator->GetOffset();
     uint64_t snapshot_offset = snapshot->GetOffset();
     if (cur_offset <= snapshot_offset + FLAGS_make_snapshot_threshold_offset) {
-        PDLOG(WARNING, "offset can't reach the threshold. tid[%u] pid[%u] cur_offset[%u], snapshot_offset[%lu]", tid, pid, cur_offset, snapshot_offset);
+        PDLOG(INFO, "offset can't reach the threshold. tid[%u] pid[%u] cur_offset[%lu], snapshot_offset[%lu]", tid, pid, cur_offset, snapshot_offset);
         return;
     }
     uint64_t offset = 0;
