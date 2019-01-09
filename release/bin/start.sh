2c94f13d5846a709faf310a37473dd444b1ef2a5
denglong <denglong@4paradigm.com>
2019-01-09 15:57:42 +0800

    feat: add stop server cmd

--- release/bin/start.sh
+++ release/bin/start.sh
@@ -1,5 +1,44 @@
 #! /bin/sh
 #
 # start.sh
-./bin/mon ./bin/boot.sh -d -l ./logs/rtidb_mon.log
-
+RTIDBPIDFILE="./bin/tablet.pid"
+mkdir -p "$(dirname "$RTIDBPIDFILE")"
+case $1 in
+    start)
+        echo -n "Starting tablet ... "
+        if [ -f "$RTIDBPIDFILE" ]; then
+            if kill -0 `cat "$RTIDBPIDFILE"` > /dev/null 2>&1; then
+                echo tablet already running as process `cat "$RTIDBPIDFILE"`.
+                exit 0
+            fi
+        fi
+        ./bin/mon ./bin/boot.sh -d -l ./logs/rtidb_mon.log -m $RTIDBPIDFILE
+        if [ $? -eq 0 ]
+        then
+            sleep 1
+            echo STARTED
+        else
+            echo SERVER DID NOT START
+            exit 1
+        fi
+        ;;
+    stop)
+        echo -n "Stopping tablet ... "
+        if [ ! -f "$RTIDBPIDFILE" ]
+        then
+             echo "no tablet to stop (could not find file $RTIDBPIDFILE)"
+        else
+            kill $(cat "$RTIDBPIDFILE")
+            rm "$RTIDBPIDFILE"
+            echo STOPPED
+        fi    
+        ;;
+    restart)
+        shift
+        "$0" stop ${@}
+        sleep 5
+        "$0" start ${@}
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}" >&2
+esac    
--- release/bin/start_ns.sh
+++ release/bin/start_ns.sh
@@ -1,4 +1,44 @@
 #! /bin/sh
 #
 # start_ns.sh
-./bin/mon ./bin/boot_ns.sh -d -l ./logs/rtidb_ns_mon.log
+RTIDBPIDFILE="./bin/ns.pid"
+mkdir -p "$(dirname "$RTIDBPIDFILE")"
+case $1 in
+    start)
+        echo -n "Starting nameserver ... "
+        if [ -f "$RTIDBPIDFILE" ]; then
+            if kill -0 `cat "$RTIDBPIDFILE"` > /dev/null 2>&1; then
+                echo nameserver already running as process `cat "$RTIDBPIDFILE"`.
+                exit 0
+            fi
+        fi
+        ./bin/mon ./bin/boot_ns.sh -d -l ./logs/rtidb_ns_mon.log -m $RTIDBPIDFILE
+        if [ $? -eq 0 ]
+        then
+            sleep 1
+            echo STARTED
+        else
+            echo SERVER DID NOT START
+            exit 1
+        fi
+        ;;
+    stop)
+        echo -n "Stopping nameserver ... "
+        if [ ! -f "$RTIDBPIDFILE" ]
+        then
+             echo "no nameserver to stop (could not find file $RTIDBPIDFILE)"
+        else
+            kill $(cat "$RTIDBPIDFILE")
+            rm "$RTIDBPIDFILE"
+            echo STOPPED
+        fi    
+        ;;
+    restart)
+        shift
+        "$0" stop ${@}
+        sleep 5
+        "$0" start ${@}
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}" >&2
+esac    
