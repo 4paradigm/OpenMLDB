f85e90aa251def9340774ca5bc418ecf8ffcf80c
denglong <denglong@4paradigm.com>
2019-03-21 16:53:43 +0800

    refactor: ParseKeyAndTs use find last

--- src/storage/disk_table_test.cc
+++ src/storage/disk_table_test.cc
@@ -465,25 +465,49 @@ TEST_F(DiskTableTest, CompactFilter) {
     DiskTable* table = new DiskTable("t1", 1, 1, mapping, 10, 
             ::rtidb::api::TTLType::kAbsoluteTime, ::rtidb::api::StorageMode::kHDD);
     ASSERT_TRUE(table->Init());
-    uint64_t ts = ::baidu::common::timer::get_micros() / 1000;        
-    uint64_t ts1 = ts - 20 * 60 * 1000;
-    ASSERT_TRUE(table->Put("key1", ts, "value1", 6));
-    ASSERT_TRUE(table->Put("key2", ts1, "value2", 6));
-    std::string value;
-    ASSERT_TRUE(table->Get("key1", ts, value));
-    ASSERT_EQ("value1", value);
-    ASSERT_TRUE(table->Get("key2", ts1, value));
-    ASSERT_EQ("value2", value);
-
+    uint64_t cur_time = ::baidu::common::timer::get_micros() / 1000;
+    for (int idx = 0; idx < 100; idx++) {
+        std::string key = "test" + std::to_string(idx);
+        uint64_t ts = cur_time;
+        for (int k = 0; k < 5; k++) {
+            if (k > 2) {
+                ASSERT_TRUE(table->Put(key, ts - k - 10 * 60 * 1000, "value9", 6));
+            } else {
+                ASSERT_TRUE(table->Put(key, ts - k, "value", 5));
+            }
+        }
+    }
+    for (int idx = 0; idx < 100; idx++) {
+        std::string key = "test" + std::to_string(idx);
+        uint64_t ts = cur_time;
+        for (int k = 0; k < 5; k++) {
+            std::string value;
+            if (k > 2) {
+                ASSERT_TRUE(table->Get(key, ts - k - 10 * 60 * 1000, value));
+                ASSERT_EQ("value9", value);
+            } else {
+                ASSERT_TRUE(table->Get(key, ts - k, value));
+                ASSERT_EQ("value", value);
+            }
+        }
+    }
     table->CompactDB();
-    ASSERT_TRUE(table->Get("key1", ts, value));
-    ASSERT_EQ("value1", value);
-    //ASSERT_FALSE(table->Get("key2", ts1, value));
-    //sleep(10);
+    /*for (int idx = 0; idx < 100; idx++) {
+        std::string key = "test" + std::to_string(idx);
+        uint64_t ts = cur_time;
+        for (int k = 0; k < 5; k++) {
+            std::string value;
+            if (k > 2) {
+                ASSERT_FALSE(table->Get(key, ts - k - 10 * 60 * 1000, value));
+            } else {
+                ASSERT_TRUE(table->Get(key, ts - k, value));
+                ASSERT_EQ("value", value);
+            }
+        }
+    }*/
     delete table;
     std::string path = FLAGS_hdd_root_path + "/1_1";
     RemoveData(path);
-    // printf("path: %s\n", path.c_str());
 }
 
 TEST_F(DiskTableTest, GcHead) {
