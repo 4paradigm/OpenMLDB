5e28940bedceb7ade15ebdec50461cc184694c61
wangbao <wangbao@4paradigm.com>
2019-03-26 17:24:48 +0800

    feat:add csv importer

---
+++ tools/data_importer/src/main/java/com/_4paradigm/dataimporter/parseCsv/ParseCsvUtil.java
@@ -0,0 +1,84 @@
+package com._4paradigm.dataimporter.parseCsv;
+
+import com._4paradigm.dataimporter.parseParquet.PutTask;
+import com._4paradigm.dataimporter.test.CsvExample;
+import com._4paradigm.rtidb.client.TableSyncClient;
+import com._4paradigm.rtidb.client.schema.ColumnDesc;
+import com.csvreader.CsvReader;
+import com._4paradigm.dataimporter.operator.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ParseCsvUtil {
+    private static Logger logger = LoggerFactory.getLogger(ParseCsvUtil.class);
+    private static String csvFilePath = "/Users/innerpeace/Desktop/test.csv";
+    private static String tableName = "csvTest";
+    private static char separator = ',';//csv文件字符分隔符
+    private static String encodingFormat = "UTF-8";//编码格式
+
+    /**
+     * @param path 文件的绝对路径
+     */
+    public static void putCsv(String path,String tableName, List<ColumnDesc> schemaList){
+        AtomicLong id = new AtomicLong(1);
+        TableSyncClient client ;
+        try {
+            CsvReader reader = new CsvReader(csvFilePath, separator, Charset.forName(encodingFormat));
+            // 跳过表头 如果需要表头的话，这句可以忽略
+            reader.readHeaders();
+            // 逐行读入除表头的数据,index决定哪个client执行put操作
+            for (int index = 0; reader.readRecord(); index++) {
+                logger.info("每行读到的数据为：" + reader.getRawRecord());
+                HashMap<String, Object> map = new HashMap<>();
+                for (int j = 0; j < reader.getValues().length; j++) {
+                    String columnName = schemaList.get(j).getName();
+                    switch(columnName){
+                        case "int_32":
+                            map.put(columnName, Integer.parseInt(reader.getValues()[j]));
+                            break;
+                        case "int_64":
+                            map.put(columnName, Long.parseLong(reader.getValues()[j]));
+                            break;
+                        case "int_96":
+                            map.put(columnName, reader.getValues()[j]);
+                            break;
+                        case "float_1":
+                            map.put(columnName, Float.parseFloat(reader.getValues()[j]));
+                            break;
+                        case "double_1":
+                            map.put(columnName, Double.parseDouble(reader.getValues()[j]));
+                            break;
+                        case "boolean_1":
+                            map.put(columnName, Boolean.parseBoolean(reader.getValues()[j]));
+                            break;
+                        case "binary_1":
+                            map.put(columnName, reader.getValues()[j]);
+                            break;
+                    }
+                }
+                if (index == OperateTable.sum) {
+                    index = 0;
+                }
+                client = OperateTable.getTableSyncClient()[index];
+                OperateThreadPool.getExecutor().submit(new PutTask(String.valueOf(id.getAndIncrement()), client, tableName, map));
+            }
+            OperateThreadPool.getExecutor().shutdown();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        CsvExample.writeCSV(csvFilePath);
+        OperateTable.dropTable(tableName);
+        OperateTable.createSchemaTable(tableName, OperateTable.getRtidbSchema());
+        putCsv(csvFilePath,tableName,OperateTable.getRtidbSchema());
+
+    }
+}
