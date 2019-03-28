8c5525b21cfba83f469540c169483a44c262bedd
wangbao <wangbao@4paradigm.com>
2019-03-28 13:31:59 +0800

    feat:add parquet & csv version 1.0.0

--- tools/data_importer/src/main/java/com/_4paradigm/dataimporter/initialization/InitAll.java
+++
@@ -1,9 +0,0 @@
-package com._4paradigm.dataimporter.initialization;
-
-public class InitAll {
-    public static void init(){
-        InitProperties.initProperties();
-        OperateTable.initClient();
-        OperateThreadPool.initThreadPool();
-    }
-}
