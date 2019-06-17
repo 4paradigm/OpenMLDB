b339d57e571d34ededb66494a263d615a56a1912
Wang Taize <wangtaize@4paradigm.com>
2019-06-17 19:11:48 +0800

    fix: update config

--- java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
+++ java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
@@ -1,11 +1,10 @@
 package com._4paradigm.rtidb.client.base;
 
 public class Config {
-    public static final String ZK_ENDPOINTS = "172.27.128.37:6281";
+    public static final String ZK_ENDPOINTS = "127.0.0.1:6181";
     public static final String ZK_ROOT_PATH = "/onebox";
-    public static final String ENDPOINT = "172.27.128.37:9222";
-    public static final String[] NODES = new String[]{"172.27.128.37:9222", "172.27.128.37:9221", "172.27.128.37:9220"};
+    public static final String ENDPOINT = "127.0.0.1:9501";
+    public static final String[] NODES = new String[]{"127.0.0.1:9522", "127.0.0.1:9521", "127.0.0.1:9520"};
     public static final int READ_TIMEOUT = 100000;
     public static final int WRITE_TIMEOUT = 1000000;
-
 }
