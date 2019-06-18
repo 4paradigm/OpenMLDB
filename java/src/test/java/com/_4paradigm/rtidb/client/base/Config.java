9eb543bdb8d683f9841a2c19c33aa9a52e50ec91
Wang Taize <wangtaize@4paradigm.com>
2019-06-18 14:34:43 +0800

    fix: revert config

--- java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
+++ java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
@@ -1,10 +1,10 @@
 package com._4paradigm.rtidb.client.base;
 
 public class Config {
-    public static final String ZK_ENDPOINTS = "172.27.128.37:6181";
+    public static final String ZK_ENDPOINTS = "127.0.0.1:6181";
     public static final String ZK_ROOT_PATH = "/onebox";
-    public static final String ENDPOINT = "172.27.128.37:9522";
-    public static final String[] NODES = new String[]{"172.27.128.37:9522", "172.27.128.37:9521", "172.27.128.37:9520"};
+    public static final String ENDPOINT = "127.0.0.1:9522";
+    public static final String[] NODES = new String[]{"127.0.0.1:9522", "127.0.0.1:9521", "127.0.0.1:9520"};
     public static final int READ_TIMEOUT = 100000;
     public static final int WRITE_TIMEOUT = 1000000;
 }
