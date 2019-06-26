3fb5bd066f9380ee9cf6974e3846de57b5dcbd1f
denglong <denglong@4paradigm.com>
2019-06-26 18:50:23 +0800

    feat: use tmp file

--- steps/ut.sh
+++ steps/ut.sh
@@ -8,6 +8,16 @@ test -d reports && rm -rf reports
 mkdir -p reports
 cp steps/zoo.cfg thirdsrc/zookeeper-3.4.10/conf
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh start && cd $WORK_DIR
-ls  build/bin/ | grep test | grep -v grep | while read line; do ./build/bin/$line --gtest_output=xml:./reports/$line.xml; if [ $? -ne 0 ];then code=$?; fi done
+TMPFILE="code.tmp"
+ls  build/bin/ | grep test | grep -v grep | while read line 
+do 
+    ./build/bin/$line --gtest_output=xml:./reports/$line.xml
+    RET=$?
+    if [ $RET -ne 0 ];then 
+        echo $RET >> $TMPFILE
+    fi 
+done
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh stop
+code=`cat $TMPFILE`
+rm $TMPFILE
 exit $code
