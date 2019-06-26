6f1fca40c1c897810f29b263144b9f3607a5ba07
denglong <denglong@4paradigm.com>
2019-06-26 15:27:10 +0800

    feat: update ut

--- steps/ut.sh
+++ steps/ut.sh
@@ -8,5 +8,8 @@ test -d reports && rm -rf reports
 mkdir -p reports
 cp steps/zoo.cfg thirdsrc/zookeeper-3.4.10/conf
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh start && cd $WORK_DIR
-ls  build/bin/ | grep test | grep -v grep | while read line; do ./build/bin/$line --gtest_output=xml:./reports/$line.xml; done
+#ls  build/bin/ | grep test | grep -v grep | while read line; do ./build/bin/$line --gtest_output=xml:./reports/$line.xml; done
+./build/bin/strings_test --gtest_output=xml:./reports/strings_test.xml
+code=$?
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh stop
+exit $code
