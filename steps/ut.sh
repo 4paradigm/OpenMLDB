e2b535cba52ffe8d6fa897f927931d5c563c112b
Deng Long <denglong@4paradigm.com>
2019-06-28 11:35:26 +0000

    Update ut.sh

--- steps/ut.sh
+++ steps/ut.sh
@@ -9,12 +9,13 @@ mkdir -p reports
 cp steps/zoo.cfg thirdsrc/zookeeper-3.4.10/conf
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh start && cd $WORK_DIR
 TMPFILE="code.tmp"
+echo 0 > $TMPFILE
 ls  build/bin/ | grep test | grep -v grep | while read line 
 do 
     ./build/bin/$line --gtest_output=xml:./reports/$line.xml
     RET=$?
     if [ $RET -ne 0 ];then 
-        echo $RET >> $TMPFILE
+        echo $RET > $TMPFILE
     fi 
 done
 cd thirdsrc/zookeeper-3.4.10/bin && ./zkServer.sh stop
