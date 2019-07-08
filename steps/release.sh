68aa0eae1a5e91421934cef4167bc7549c586835
denglong <denglong@4paradigm.com>
2019-07-08 20:48:13 +0800

    feat: update version format

--- steps/release.sh
+++ steps/release.sh
@@ -16,9 +16,12 @@ do
                 sed -i 's/MAJOR =.*/MAJOR = '${value}\;'/g' ${java_vesrion_file}
                 sed -i 's/RTIDB_VERSION_MAJOR .*/RTIDB_VERSION_MAJOR '${value}')/g' ${cmake_file};;
             "2")
+                sed -i 's/MEDIUM =.*/MEDIUM = '${value}\;'/g' ${java_vesrion_file}
+                sed -i 's/RTIDB_VERSION_MEDIUM .*/RTIDB_VERSION_MEDIUM '${value}')/g' ${cmake_file};;
+            "3")
                 sed -i 's/MINOR =.*/MINOR = '${value}\;'/g' ${java_vesrion_file}
                 sed -i 's/RTIDB_VERSION_MINOR .*/RTIDB_VERSION_MINOR '${value}')/g' ${cmake_file};;
-            "3")
+            "4")
                 sed -i 's/BUG =.*/BUG = '${value}\;'/g' ${java_vesrion_file}
                 sed -i 's/RTIDB_VERSION_BUG .*/RTIDB_VERSION_BUG '${value}')/g' ${cmake_file};;
             *)
