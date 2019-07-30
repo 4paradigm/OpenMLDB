d793edee06465061dc750bd98667a7c398582d3c
denglong <denglong@4paradigm.com>
2019-07-30 19:31:48 +0800

    test: update start onebox

--- onebox/start_onebox.sh
+++ onebox/start_onebox.sh
@@ -18,7 +18,9 @@ test -d tablet0-binlogs && rm -rf tablet0-binlogs
 test -d recycle_bin0 && rm -rf recycle_bin0
 ../build/bin/rtidb --db_root_path=tablet0-binlogs \
                    --hdd_root_path=tablet0-binlogs \
+                   --ssd_root_path=tablet0-binlogs \
                    --recycle_bin_root_path=recycle_bin0 \
+                   --recycle_ssd_bin_root_path=recycle_bin0 \
                    --endpoint=${TABLET1} --role=tablet \
                    --binlog_notify_on_put=true\
                    --zk_cluster=${ZK_CLUSTER}\
@@ -31,7 +33,9 @@ test -d recycle_bin1 && rm -rf recycle_bin1
 # start tablet1
 ../build/bin/rtidb --db_root_path=tablet1-binlogs \
                    --hdd_root_path=tablet1-binlogs \
+                   --ssd_root_path=tablet1-binlogs \
                    --recycle_bin_root_path=recycle_bin1 \
+                   --recycle_ssd_bin_root_path=recycle_bin1 \
                    --endpoint=${TABLET2} --role=tablet \
                    --zk_cluster=${ZK_CLUSTER}\
                    --binlog_notify_on_put=true\
@@ -44,7 +48,9 @@ test -d recycle_bin2 && rm -rf recycle_bin2
 # start tablet2
 ../build/bin/rtidb --db_root_path=tablet2-binlogs \
                    --hdd_root_path=tablet2-binlogs \
+                   --ssd_root_path=tablet2-binlogs \
                    --recycle_bin_root_path=recycle_bin2 \
+                   --recycle_ssd_bin_root_path=recycle_bin2 \
                    --endpoint=${TABLET3} --role=tablet \
                    --binlog_notify_on_put=true\
                    --zk_cluster=${ZK_CLUSTER}\
