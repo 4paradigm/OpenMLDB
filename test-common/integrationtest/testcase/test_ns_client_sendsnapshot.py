54e7cd20a09dc339523e09f0111db9f9007e4516
denglong <denglong@4paradigm.com>
2019-09-04 10:48:09 +0800

    test: add sendsnapshot case

--- test-common/integrationtest/testcase/test_ns_client_sendsnapshot.py
+++ test-common/integrationtest/testcase/test_ns_client_sendsnapshot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+import os
 from testcasebase import TestCaseBase
 from libs.deco import *
 from libs.test_loader import load
@@ -327,6 +328,38 @@ class TestSendSnapshot(TestCaseBase):
         self.start_client(self.leader)
         time.sleep(10)
 
+    def test_sendsnapshot_disk(self):
+        """
+        disktable
+        :return:
+        """
+        name = self.now()
+        tname = 'tname{}'.format(time.time())
+        metadata_path = '{}/metadata.txt'.format(self.testpath)
+        table_meta = {
+                "name": tname,
+                "ttl": 14400,
+                "storage_mode": "kHDD",
+                "replica_num" : 1,
+                "partition_num" : 1,
+                }
+        utils.gen_table_meta_file(table_meta, metadata_path)
+        rs = self.ns_create(self.ns_leader, metadata_path)
+        self.assertIn('Create table ok', rs)
+        table_info = self.showtable(self.ns_leader, tname)
+        tid = table_info.keys()[0][1]
+        pid = '0'
+        self.put_data(self.leader, tid, pid, 100)
+        self.assertIn("MakeSnapshot ok", self.makesnapshot(self.leader, tid, pid))
+        self.assertIn("PauseSnapshot ok", self.pausesnapshot(self.leader, tid, pid))
+        self.assertIn("SendSnapshot ok", self.sendsnapshot(self.leader, tid, pid, self.slave1))
+        time.sleep(1)
+        mf = self.get_manifest(self.leaderpath, tid, pid)
+        mf1 = self.get_manifest(self.slave1path, tid, pid)
+        self.assertEqual(mf, mf1)
+        snapshot_path = "/db/" + str(tid) + "_" + str(pid) + "/snapshot/" + mf["name"]
+        self.assertEqual(len(os.listdir(self.slave1path + snapshot_path)), len(os.listdir(self.leaderpath + snapshot_path)))
+        self.ns_drop(self.ns_leader, tname)
 
 if __name__ == "__main__":
     load(TestSendSnapshot)
