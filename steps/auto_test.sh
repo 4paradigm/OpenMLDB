206a4c4f2271584352016be17274056d355d5dc5
Zhao Wei01 <zhaowei01@4paradigm.com>
2019-10-11 19:18:04 +0800

    修改了TestSetTTL的一个方法来测试ssd

---
+++ steps/auto_test.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+ROOT_DIR=`pwd`
+
+PROTO_BIN=$ROOT_DIR/thirdparty/bin/protoc
+ulimit -c unlimited
+sed -i "/protocExecutable/c\<protocExecutable>${PROTO_BIN}<\/protocExecutable>" java/pom.xml
+mkdir -p java/src/main/proto/
+cp -rf src/proto/tablet.proto java/src/main/proto/
+cp -rf src/proto/name_server.proto java/src/main/proto/
+cp -rf src/proto/common.proto java/src/main/proto/
+
+cd $ROOT_DIR/java
+rtidb_version=`cat pom.xml| grep version | head -n 1 | sed -n 's/<version>\(.*\)<\/version>/\1/p'`
+mvn clean install -Dmaven.test.skip=true
+
+cd $ROOT_DIR
+source  rm -rf rtidb-auto-test; git checkout .; git submodule init; git submodule update
+cd rtidb-auto-test
+sh run.sh gl.xml $rtidb_version
