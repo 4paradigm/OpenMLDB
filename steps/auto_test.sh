253133ffed7c044c1a09e1ba8be498d6b7847378
Zhao Wei01 <zhaowei01@4paradigm.com>
2019-10-12 11:36:28 +0800

    修改了TestSetTTL的一个方法来测试ssd

--- steps/auto_test.sh
+++ steps/auto_test.sh
@@ -10,6 +10,17 @@ cp -rf src/proto/tablet.proto java/src/main/proto/
 cp -rf src/proto/name_server.proto java/src/main/proto/
 cp -rf src/proto/common.proto java/src/main/proto/
 
+cp steps/zoo.cfg thirdsrc/zookeeper-3.4.10/conf
+cd thirdsrc/zookeeper-3.4.10
+test -d ut_zookeeper && rm -rf ut_zookeeper
+netstat -anp | grep 6181 | awk '{print $NF}' | awk -F '/' '{print $1}'| xargs kill -9
+./bin/zkServer.sh start && cd $ROOT_DIR
+
+sleep 5
+
+cd onebox && sh start_onebox.sh && cd $ROOT_DIR
+sleep 3
+
 cd $ROOT_DIR/java
 rtidb_version=`cat pom.xml| grep version | head -n 1 | sed -n 's/<version>\(.*\)<\/version>/\1/p'`
 mvn clean install -Dmaven.test.skip=true
@@ -19,3 +30,10 @@ source /root/.bashrc && rm -rf rtidb-auto-test; git checkout .; git submodule in
 cd rtidb-auto-test
 git pull
 sh run.sh gl.xml $rtidb_version
+
+code=$?
+cd $ROOT_DIR
+cd onebox && sh stop_all.sh
+cd $ROOT_DIR
+cd thirdsrc/zookeeper-3.4.10 && ./bin/zkServer.sh stop
+exit $code
