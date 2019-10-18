0b13aa1c61c406dc5417849af5f3345ed3c14b2c
Zhao Wei01 <zhaowei01@4paradigm.com>
2019-10-18 18:24:05 +0800

    修改了python case

--- test-common/integrationtest/testcase/ssd_test_ns_client_auto_failover.py
+++ test-common/integrationtest/testcase/ssd_test_ns_client_auto_failover.py
@@ -104,6 +104,7 @@ class TestAutoFailover(TestCaseBase):
         ('killed','kHDD'),
         ('network_failure','kHDD'),
     )
+    @ddt.unpack
     def test_auto_failover_slave_exception(self, failover_reason,storage_mode):
         """
         auto_failover=true：从节点挂掉或断网后，showtable中从节点is_alive为no
