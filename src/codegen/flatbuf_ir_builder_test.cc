e1e93dbba281dbf63f78ed096885d9ff49acf8ec
imotai <wangtaize@4paradigm.com>
2019-10-23 21:03:24 +0800

    test: add more field test

--- src/codegen/flatbuf_ir_builder_test.cc
+++ src/codegen/flatbuf_ir_builder_test.cc
@@ -92,6 +92,89 @@ public:
     ~FlatBufIRBuilderTest() {}
 };
 
+::llvm::Type* Convert(::llvm::LLVMContext& ctx, ::fesql::type::Type& type) {
+    llvm::Type* field_type = NULL;
+    switch (type) {
+        case ::fesql::type::kBool:
+            {
+                field_type = ::llvm::Type::getInt1Ty(ctx);
+                break;
+            }
+        case ::fesql::type::kInt16:
+            {
+                field_type = ::llvm::Type::getInt16Ty(ctx);
+                break;
+            }
+        case ::fesql::type::kInt32:
+            {
+                field_type = ::llvm::Type::getInt32Ty(ctx);
+                break;
+            }
+        case ::fesql::type::kInt64:
+            {
+                field_type = ::llvm::Type::getInt64Ty(ctx);
+                break;
+            }
+        case ::fesql::type::kFloat:
+            {
+                field_type = ::llvm::Type::getFloatTy(ctx);
+                break;
+            }
+        case ::fesql::type::kDouble:
+            {
+                field_type = ::llvm::Type::getDoubleTy(ctx);
+                break;
+            }
+        default:
+            {
+                break;
+            }
+    }
+    return field_type;
+}
+template<typename T> 
+void RunGetField(const std::string& name, 
+        T expect, ::fesql::type::Type& type) {
+    ::fesql::type::TableDef table;
+    GetSchema(table);
+    auto ctx = llvm::make_unique<LLVMContext>();
+    auto m = make_unique<Module>("test_get_table", *ctx);
+    auto J = ExitOnErr(LLJITBuilder().create());
+    ::llvm::Type* ltype = Convert(*ctx, type);
+    Function *fn =
+    Function::Create(FunctionType::get(ltype,
+                                         {Type::getInt8PtrTy(*ctx)}, 
+                                         false),
+                       Function::ExternalLinkage, "fn", m.get());
+    BasicBlock *entry_block = BasicBlock::Create(*ctx, "EntryBlock", fn);
+    IRBuilder<> builder(entry_block);
+    Argument *arg0 = &*fn->arg_begin(); 
+    FlatBufDecodeIRBuilder fbuilder(&table);
+    Value *table_start_offset = NULL;
+    bool ok = fbuilder.BuildGetTableOffset(builder, arg0, *ctx, &table_start_offset);
+    ASSERT_TRUE(ok);
+    Value *vtable_start_offset = NULL;
+    ok = fbuilder.BuildGetVTable(builder, arg0, table_start_offset, *ctx, &vtable_start_offset);
+    ASSERT_TRUE(ok);
+    Value *field_offset = NULL;
+    ok = fbuilder.BuildGetOptionalFieldOffset(builder, arg0, *ctx, vtable_start_offset, name, &field_offset);
+    ASSERT_TRUE(ok);
+    Value *field_val = NULL;
+    ok = fbuilder.BuildGetField(builder, arg0, table_start_offset, *ctx, field_offset, name, &field_val);
+    ASSERT_TRUE(ok);
+    builder.CreateRet(field_val);
+    m->print(::llvm::errs(), NULL);
+    ExitOnErr(J->addIRModule(std::move(ThreadSafeModule(std::move(m), std::move(ctx)))));
+    auto fn_jit = ExitOnErr(J->lookup("fn"));
+    T (*decode)(int8_t*) = (T(*)(int8_t*))fn_jit.getAddress();
+    flatbuffers::FlatBufferBuilder fb;
+    ::flatbuffers::Offset<::flatbuffers::Table> row = Decode(fb);
+    fb.Finish(row);
+    int8_t* ptr = (int8_t*)fb.GetBufferPointer();
+    T ret = decode(ptr);
+    ASSERT_EQ(ret, expect);
+}
+
 TEST_F(FlatBufIRBuilderTest, test_get_table) {
     // Create an LLJIT instance.
     auto ctx = llvm::make_unique<LLVMContext>();
@@ -209,48 +292,34 @@ TEST_F(FlatBufIRBuilderTest, test_get_float_field_offset) {
 
 
 TEST_F(FlatBufIRBuilderTest, test_get_float_field) {
-    // Create an LLJIT instance.
-    auto ctx = llvm::make_unique<LLVMContext>();
-    auto m = make_unique<Module>("test_load_float", *ctx);
-    auto J = ExitOnErr(LLJITBuilder().create());
-    // Create the add1 function entry and insert this entry into module M.  The
-    // function will have a return type of "int" and take an argument of "int".
-    Function *load_fn =
-    Function::Create(FunctionType::get(Type::getFloatTy(*ctx),
-                                         {Type::getInt8PtrTy(*ctx)}, 
-                                         false),
-                       Function::ExternalLinkage, "load_fn", m.get());
-    BasicBlock *entry_block = BasicBlock::Create(*ctx, "EntryBlock", load_fn);
-    IRBuilder<> builder(entry_block);
-    Argument *arg0 = &*load_fn->arg_begin(); 
-    ::fesql::type::TableDef table;
-    GetSchema(table);
-    FlatBufDecodeIRBuilder fbuilder(&table);
-    Value *table_start_offset = NULL;
-    bool ok = fbuilder.BuildGetTableOffset(builder, arg0, *ctx, &table_start_offset);
-    ASSERT_TRUE(ok);
-    Value *vtable_start_offset = NULL;
-    ok = fbuilder.BuildGetVTable(builder, arg0, table_start_offset, *ctx, &vtable_start_offset);
-    ASSERT_TRUE(ok);
-    Value *field_offset = NULL;
-    ok = fbuilder.BuildGetOptionalFieldOffset(builder, arg0, *ctx, vtable_start_offset, "col1", &field_offset);
-    ASSERT_TRUE(ok);
-    Value *field_val = NULL;
-    ok = fbuilder.BuildGetField(builder, arg0, table_start_offset, *ctx, field_offset, "col1", &field_val);
-    ASSERT_TRUE(ok);
-    builder.CreateRet(field_val);
-    m->print(::llvm::errs(), NULL);
-    ExitOnErr(J->addIRModule(std::move(ThreadSafeModule(std::move(m), std::move(ctx)))));
-    auto load_fn_jit = ExitOnErr(J->lookup("load_fn"));
-    float (*decode)(int8_t*) = (float (*)(int8_t*))load_fn_jit.getAddress();
-    flatbuffers::FlatBufferBuilder fb;
-    ::flatbuffers::Offset<::flatbuffers::Table> row = Decode(fb);
-    fb.Finish(row);
-    int8_t* ptr = (int8_t*)fb.GetBufferPointer();
-    float ret = decode(ptr);
-    ASSERT_EQ(ret, 1.0f);
+    std::string col = "col1";
+    ::fesql::type::Type type = ::fesql::type::kFloat;
+    RunGetField<float>(col, 1.0f, type);
 }
 
+TEST_F(FlatBufIRBuilderTest, test_get_int16_field) {
+    std::string col2 = "col2";
+    ::fesql::type::Type type = ::fesql::type::kInt16;
+    RunGetField<int16_t>(col2, 16, type);
+}
+
+TEST_F(FlatBufIRBuilderTest, test_get_int32_field) {
+    std::string col = "col3";
+    ::fesql::type::Type type = ::fesql::type::kInt32;
+    RunGetField<int32_t>(col, 32, type);
+}
+
+TEST_F(FlatBufIRBuilderTest, test_get_int64_field) {
+    std::string col = "col4";
+    ::fesql::type::Type type = ::fesql::type::kInt64;
+    RunGetField<int64_t>(col, 64, type);
+}
+
+TEST_F(FlatBufIRBuilderTest, test_get_double_field) {
+    std::string col = "col5";
+    ::fesql::type::Type type = ::fesql::type::kDouble;
+    RunGetField<double>(col, 2.0, type);
+}
 } // namespace of codegen
 } // namespace of fesql
 
