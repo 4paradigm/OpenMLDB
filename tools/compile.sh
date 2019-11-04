a8bdb2dc666c83537bd6f282d4f50bf58f2fcad7
chenjing <chenjing@4paradigm.com>
2019-11-04 16:02:46 +0800

    recover compile shell

--- tools/compile.sh
+++ tools/compile.sh
@@ -4,4 +4,4 @@
 PWD=`pwd`
 export PATH=${PWD}/thirdparty/bin:$PATH
 mkdir -p build && cd build && cmake .. && make -j4
-#mkdir -p build && cd build && cmake .. && make fesql_proto -j4
+#mkdir -p build && cd build && cmake .. && make -j4
