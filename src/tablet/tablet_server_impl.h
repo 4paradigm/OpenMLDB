262402562e79d6cf8877d95310ec339a16ab6799
imotai <wangtaize@4paradigm.com>
2019-11-13 12:16:54 +0800

    feat: add tablet

---
+++ src/tablet/tablet_server_impl.h
@@ -0,0 +1,37 @@
+/*
+ * tablet_server_impl.h
+ * Copyright (C) 4paradigm.com 2019 wangtaize <wangtaize@4paradigm.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_TABLET_TABLET_SERVER_IMPL_H_
+#define SRC_TABLET_TABLET_SERVER_IMPL_H_
+
+#include "proto/tablet.pb.h"
+
+namespace fesql {
+namespace tablet {
+
+using ::google::protobuf::RpcController;
+using ::google::protobuf::Closure;
+
+// TODO opt db tid pid structure
+
+class TabletServerImpl : public TabletServer {
+
+};
+
+}  // namespace tablet
+}  // namespace fesql
+#endif  // SRC_TABLET_TABLET_SERVER_IMPL_H_
