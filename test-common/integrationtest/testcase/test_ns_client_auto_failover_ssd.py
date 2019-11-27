400453c83ec2f9261194d63c7a382580c12318c7
panxuchen <panxuchen@4paradigm.com>
2019-11-27 11:36:21 +0800

    print error

--- test-common/integrationtest/testcase/test_ns_client_auto_failover_ssd.py
+++ test-common/integrationtest/testcase/test_ns_client_auto_failover_ssd.py
@@ -558,6 +558,8 @@ class TestAutoFailover(TestCaseBase):
             rs_after = self.parse_tb(rs_after, ' ', [0, 1, 2, 3], [4, 5, 6, 7,8, 9,10])
             if '{}'.format(rs_after) == 'gettablestatus failed':
                 continue
+            print(rs_before.keys()[0][2])
+            print(rs_after.keys()[0][2])
             if rs_before.keys()[0][2] == rs_after.keys()[0][2]:
                 self.assertIn(rs_before.keys()[0][2], rs_after.keys()[0][2])
                 break
