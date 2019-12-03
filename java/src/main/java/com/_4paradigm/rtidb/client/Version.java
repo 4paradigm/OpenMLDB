34540f755fe92fb14d624f3ae360d14212dea54e
panxuchen <panxuchen@4paradigm.com>
2019-12-03 10:34:50 +0800

    release

--- java/src/main/java/com/_4paradigm/rtidb/client/Version.java
+++ java/src/main/java/com/_4paradigm/rtidb/client/Version.java
@@ -4,6 +4,6 @@ public class Version {
     public static int MAJOR = 1;
     public static int MEDIUM = 4;
     public static int MINOR = 2;
-    public static int BUG = 0;
+    public static int BUG = 2;
     public static String VERSION = String.valueOf(MAJOR) + "." + String.valueOf(MEDIUM) + "." + String.valueOf(MINOR) + "." + String.valueOf(BUG);
 }
