e075e8a4daa31bbaa4a00b49f561372f8d40073a
imotai <wangtaize@4paradigm.com>
2019-12-14 10:29:14 +0800

    fix: update compile script

--- tools/compile.sh
+++ tools/compile.sh
@@ -3,5 +3,9 @@
 # compile.sh
 PWD=`pwd`
 export PATH=${PWD}/thirdparty/bin:$PATH
-mkdir -p build && cd build && cmake .. && make -j1
-#mkdir -p build && cd build && cmake .. && make -j4
+mkdir -p build && cd build 
+cmake .. 
+make fesql_proto 
+make fesql_codegen
+make -j4
+
--- tools/micro_bench.sh
+++ tools/micro_bench.sh
@@ -4,7 +4,7 @@
 
 mkdir -p build && cd build 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=OFF
-make fesql_proto && make -j4 engine_bm
+make fesql_proto fesql_codegen && make -j4 engine_bm
 src/vm/engine_bm 2>/dev/null
 
 
