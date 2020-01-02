760774ce72fb59b035a1335956387ff63157696d
panxuchen <panxuchen@4paradigm.com>
2020-01-02 17:40:23 +0800

    debug abs

--- src/tablet/tablet_impl.cc
+++ src/tablet/tablet_impl.cc
@@ -748,6 +748,9 @@ int32_t TabletImpl::ScanIndex(uint64_t expire_time, uint64_t expire_cnt,
             break;
         }
         if (ttl_type == ::rtidb::api::TTLType::kAbsoluteTime) {
+            if (expire_time != 0 && it->GetKey() < expire_time) {
+                break;
+            }
             if (remove_duplicated_record && tmp.size() > 0 && 
                 last_time == it->GetKey()) {
                     it->Next();
