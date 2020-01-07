d46ef9f333425f8f07f44529867cff903bc6b629
chenjing <chenjing@4paradigm.com>
2020-01-07 20:23:35 +0800

    benchmark cmd fix

--- tools/micro_bench.sh
+++ tools/micro_bench.sh
@@ -7,12 +7,12 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DCOVERAGE_ENABL
 make -j16 engine_bm
 make -j16 fesql_client_bm
 make -j16 fesql_client_batch_run_bm
-echo "engine benchmark"
+echo "engine benchmark:"
 src/vm/engine_bm 2>/dev/null
 
-echo "fesql client one run benchmark"
-src/vm/fesql_client_bm 2>/dev/null
+echo "fesql client one run benchmark:"
+src/bm/fesql_client_bm 2>/dev/null
 
-echo "fesql client batch run benchmark"
-src/vm/fesql_client_batch_run_bm 2>/dev/null
+echo "fesql client batch run benchmark:"
+src/bm/fesql_client_batch_run_bm 2>/dev/null
 
