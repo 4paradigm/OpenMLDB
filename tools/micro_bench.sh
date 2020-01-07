a416b07db48a19c2a04e769d46c442ab39d7ad4e
chenjing <chenjing@4paradigm.com>
2020-01-07 20:12:33 +0800

    add micro bench case

--- tools/micro_bench.sh
+++ tools/micro_bench.sh
@@ -4,6 +4,15 @@
 
 mkdir -p build && cd build 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=OFF
- make -j16 engine_bm
+make -j16 engine_bm
+make -j16 fesql_client_bm
+make -j16 fesql_client_batch_run_bm
+echo "engine benchmark"
 src/vm/engine_bm 2>/dev/null
+
+echo "fesql client one run benchmark"
+src/vm/fesql_client_bm 2>/dev/null
+
+echo "fesql client batch run benchmark"
+src/vm/fesql_client_batch_run_bm 2>/dev/null
 
