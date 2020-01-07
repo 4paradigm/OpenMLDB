fc490282a2e60d5905c34308889d213c4f1556f4
imotai <wangtaize@4paradigm.com>
2020-01-07 18:01:06 +0800

    feat: add arrow dep

--- tools/get_deps.sh
+++ tools/get_deps.sh
@@ -15,7 +15,8 @@ DEPS_SOURCE=`pwd`/thirdsrc
 DEPS_PREFIX=`pwd`/thirdparty
 DEPS_CONFIG="--prefix=${DEPS_PREFIX} --disable-shared --with-pic"
 mkdir -p $DEPS_PREFIX/lib $DEPS_PREFIX/include
-
+export CXXFLAGS="${CXXFLAGS} -O3 -fPIC"
+export CFLAGS="${CFLAGS} -O3 -fPIC"
 export PATH=${DEPS_PREFIX}/bin:$PATH
 mkdir -p ${DEPS_SOURCE} ${DEPS_PREFIX}
 
@@ -48,12 +49,7 @@ else
     wget http://pkg.4paradigm.com/rtidb/dev/zlib-1.2.11.tar.gz
     tar zxf zlib-1.2.11.tar.gz 
     cd zlib-1.2.11
-    if [ ""$PLATFORM == "mac" ]
-    then
-        sed -i '/CFLAGS="${CFLAGS--O3}"/c\  CFLAGS="${CFLAGS--O3} -fPIC"' configure
-    else
-        sed -i '/CFLAGS="${CFLAGS--O3}"/c\  CFLAGS="${CFLAGS--O3} -fPIC"' configure
-    fi
+    sed -i '/CFLAGS="${CFLAGS--O3}"/c\  CFLAGS="${CFLAGS--O3} -fPIC"' configure
     ./configure --static --prefix=${DEPS_PREFIX} >/dev/null
     make -j2 >/dev/null
     make install
@@ -137,11 +133,7 @@ else
     wget http://pkg.4paradigm.com/rtidb/dev/gperftools-2.5.tar.gz
     tar -zxvf gperftools-2.5.tar.gz
     cd gperftools-2.5
-    #wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.7/gperftools-2.7.tar.gz
-#    tar xaf gperftools-2.7.tar.gz
-#    cd gperftools-2.7
     ./configure --enable-cpu-profiler --enable-heap-checker --enable-heap-profiler  --enable-static --prefix=${DEPS_PREFIX}
-#    ./configure --prefix=${DEPS_PREFIX}
     make -j2 >/dev/null
     make install
     cd -
@@ -165,12 +157,7 @@ else
     wget http://pkg.4paradigm.com/rtidb/dev/leveldb.tar.gz
     tar -zxvf leveldb.tar.gz
     cd leveldb
-    if [ ""$PLATFORM == "mac" ]
-    then
-        sed -i 's/^OPT ?= -O2 -DNDEBUG/OPT ?= -O2 -DNDEBUG -fPIC/' Makefile
-    else
-        sed -i 's/^OPT ?= -O2 -DNDEBUG/OPT ?= -O2 -DNDEBUG -fPIC/' Makefile
-    fi
+    sed -i 's/^OPT ?= -O2 -DNDEBUG/OPT ?= -O2 -DNDEBUG -fPIC/' Makefile
     make -j8
     cp -rf include/* ${DEPS_PREFIX}/include
     cp out-static/libleveldb.a ${DEPS_PREFIX}/lib
@@ -202,7 +189,7 @@ else
     wget --no-check-certificate -O glogs-v0.4.tar.gz https://github.com/google/glog/archive/v0.4.0.tar.gz
     tar zxf glogs-v0.4.tar.gz
     cd glog-0.4.0
-    ./autogen.sh && ./configure --prefix=${DEPS_PREFIX} && make install
+    ./autogen.sh && CXXFLAGS=-fPIC ./configure --prefix=${DEPS_PREFIX} && make install
     cd -
     touch glog_succ
 fi
@@ -221,12 +208,7 @@ else
     tar -zxvf incubator-brpc.tar.gz
     BRPC_DIR=$DEPS_SOURCE/incubator-brpc
     cd incubator-brpc
-    if [ ""$PLATFORM != "mac" ]
-    then
-        sh config_brpc.sh --with-glog --headers=${DEPS_PREFIX}/include --libs=${DEPS_PREFIX}/lib
-    else
-        sh config_brpc.sh --with-glog --headers=${DEPS_PREFIX}/include --libs=${DEPS_PREFIX}/lib --cc=clang --cxx=clang++
-    fi
+    sh config_brpc.sh --with-glog --headers=${DEPS_PREFIX}/include --libs=${DEPS_PREFIX}/lib
     make -j5 libbrpc.a
     make output/include
     cp -rf output/include/* ${DEPS_PREFIX}/include
@@ -315,7 +297,7 @@ else
 fi
 
 
-if [ -d "xz-5.2.4" ] 
+if [ -f "xz_succ" ] 
 then
     echo "zx exist"
 else
@@ -326,3 +308,173 @@ else
     touch xz_succ
 fi
 
+if [ -f "boost_succ" ] 
+then
+    echo "boost exist"
+else
+    if [ -f "boost_1_69_0.tar.gz" ]
+    then
+        echo "boost exist"
+    else
+        wget --no-check-certificate -O boost_1_69_0.tar.gz http://pkg.4paradigm.com/fesql/boost_1_69_0.tar.gz
+    fi
+    tar -zxvf boost_1_69_0.tar.gz
+    cd boost_1_69_0 && ./bootstrap.sh && ./b2 install --prefix=${DEPS_PREFIX}
+    cd -
+    touch boost_succ
+fi
+
+if [ -f "brotli_succ" ] 
+then
+    echo "brotli exist"
+else
+    if [ -f "v1.0.7.tar.gz" ]
+    then
+        echo "brotli exist"
+    else
+        wget --no-check-certificate -O v1.0.7.tar.gz https://github.com/google/brotli/archive/v1.0.7.tar.gz
+    fi
+    tar -zxvf v1.0.7.tar.gz
+    cd brotli-1.0.7  && ./bootstrap && ./configure --prefix=${DEPS_PREFIX} && make -j4 && make install
+    cd -
+    touch brotli_succ
+fi
+
+if [ -f "double-conversion_succ" ]
+then 
+    echo "double-conversion exist"
+else
+    if [ -f "v3.1.5.tar.gz" ]
+    then
+        echo "double-conversion pkg exist"
+    else
+        wget --no-check-certificate -O v3.1.5.tar.gz https://github.com/google/double-conversion/archive/v3.1.5.tar.gz
+    fi
+    tar -zxvf v3.1.5.tar.gz
+    cd double-conversion-3.1.5 && mkdir -p build
+    cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX} -DCMAKE_CXX_FLAGS=-fPIC .. >/dev/null
+    make -j4 && make install
+    cd ${DEPS_SOURCE}
+    touch double-conversion_succ
+fi
+
+if [ -f "lz4_succ" ]
+then
+    echo " lz4 exist"
+else
+    if [ -f "lz4-1.7.5.tar.gz" ]
+    then
+        echo "lz4 tar exist"
+    else
+        wget --no-check-certificate -O lz4-1.7.5.tar.gz http://pkg.4paradigm.com/fesql/lz4-1.7.5.tar.gz
+    fi
+    tar -zxvf lz4-1.7.5.tar.gz 
+    cd lz4-1.7.5 && make -j4 && make install PREFIX=${DEPS_PREFIX}
+    cd ${DEPS_SOURCE}
+    touch lz4_succ
+fi
+
+if [ -f "bzip2_succ" ]
+then
+    echo "bzip2 installed"
+else
+    if [ -f "bzip2-1.0.8.tar.gz" ]
+    then
+        echo "bzip2-1.0.8.tar.gz  downloaded"
+    else
+        wget --no-check-certificate -O bzip2-1.0.8.tar.gz http://pkg.4paradigm.com/fesql/bzip2-1.0.8.tar.gz
+    fi
+    tar -zxvf bzip2-1.0.8.tar.gz 
+    cd bzip2-1.0.8 && make -j4 && make install PREFIX=${DEPS_PREFIX}
+    cd -
+    touch bzip2_succ
+fi
+
+if [ -f "jemalloc_succ" ]
+then
+    echo "jemalloc installed"
+else
+    if [ -f "jemalloc-5.2.1.tar.gz" ]
+    then
+        echo "jemalloc-5.2.1.tar.gz downloaded"
+    else
+        wget --no-check-certificate -O jemalloc-5.2.1.tar.gz http://pkg.4paradigm.com/fesql/jemalloc-5.2.1.tar.gz
+    fi
+    tar -zxvf jemalloc-5.2.1.tar.gz
+    cd jemalloc-5.2.1 && ./autogen.sh && ./configure --prefix=${DEPS_PREFIX} && make -j4 && make install
+    cd - 
+    touch jemalloc_succ
+fi
+
+if [ -f "flatbuffer_succ" ]
+then
+    echo "flatbuffer installed"
+else
+    if [ -f "flatbuffers-1.11.0.tar.gz" ]
+    then
+        echo "flatbuffers-1.11.0.tar.gz downloaded"
+    else
+        wget --no-check-certificate -O flatbuffers-1.11.0.tar.gz http://pkg.4paradigm.com/fesql/flatbuffers-1.11.0.tar.gz
+    fi
+    tar -zxvf flatbuffers-1.11.0.tar.gz
+    cd flatbuffers-1.11.0 && mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX} -DCMAKE_CXX_FLAGS=-fPIC ..
+    make -j4 && make install
+    cd ${DEPS_SOURCE}
+    touch flatbuffer_succ
+fi
+
+if [ -f "zstd_succ" ]
+then
+    echo "zstd installed"
+else
+    if [ -f "zstd-1.4.4.tar.gz" ]
+    then
+        echo "zstd-1.4.4.tar.gz  downloaded"
+    else
+        wget --no-check-certificate -O zstd-1.4.4.tar.gz http://pkg.4paradigm.com/fesql/zstd-1.4.4.tar.gz
+    fi
+    tar -zxvf zstd-1.4.4.tar.gz
+    cd zstd-1.4.4 && make -j4 && make install PREFIX=${DEPS_PREFIX}
+    cd ${DEPS_SOURCE}
+    touch zstd_succ
+fi
+
+if [ -f "thrift_succ" ]
+then
+    echo "thrift installed"
+else
+    if [ -f "thrift-0.12.0.tar.gz" ]
+    then
+        echo "thrift-0.12.0.tar.gz  downloaded"
+    else
+        wget --no-check-certificate -O thrift-0.12.0.tar.gz  http://pkg.4paradigm.com/fesql/thrift-0.12.0.tar.gz
+    fi
+    tar -zxvf thrift-0.12.0.tar.gz
+    cd thrift-0.12.0 && ./configure --with-python=no --with-nodejs=no --prefix=${DEPS_PREFIX} && make -j4 && make install
+    cd ${DEPS_SOURCE}
+    touch thrift_succ
+fi
+
+if [ -f "arrow_succ" ]
+then
+    echo "arrow installed"
+else
+    if [ -f "apache-arrow-0.15.1.tar.gz" ]
+    then
+        echo "apache-arrow-0.15.1.tar.gz   downloaded"
+    else
+        wget --no-check-certificate -O apache-arrow-0.15.1.tar.gz http://pkg.4paradigm.com/fesql/apache-arrow-0.15.1.tar.gz
+    fi
+    tar -zxvf apache-arrow-0.15.1.tar.gz
+    export ARROW_BUILD_TOOLCHAIN=${DEPS_PREFIX}
+    export JEMALLOC_HOME=${DEPS_PREFIX}
+    cd apache-arrow-0.15.1/cpp && mkdir -p build && cd build
+    cmake  -DARROW_JEMALLOC=OFF -DARROW_MIMALLOC=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX} -DARROW_CUDA=OFF -DARROW_FLIGHT=OFF -DARROW_GANDIVA=OFF \
+    -DARROW_GANDIVA_JAVA=OFF -DARROW_HDFS=ON -DARROW_HIVESERVER2=OFF \
+    -DARROW_ORC=OFF -DARROW_PARQUET=ON -DARROW_PLASMA=OFF\
+    -DARROW_PLASMA_JAVA_CLIENT=OFF -DARROW_PYTHON=OFF -DARROW_BUILD_TESTS=OFF \
+    -DARROW_BUILD_UTILITIES=OFF ..
+    make -j4 parquet && make install 
+    cd ${DEPS_SOURCE}
+    touch arrow_succ
+fi
