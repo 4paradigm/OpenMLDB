58f085e5bd94d8a375f15805d1d8ff1748e0ee83
chenjing <chenjing@4paradigm.com>
2020-02-04 10:32:11 +0800

    update onebox script

--- onebox/start_all.sh
+++ onebox/start_all.sh
@@ -3,5 +3,6 @@
 # start_dbms.sh
 mkdir -p log/dbms
 mkdir -p log/tablet
-../build/src/fesql --role=dbms --tablet_endpoint=127.0.0.1:9212 --port=9211 --log_dir=./log/dbms  &
-../build/src/fesql --role=tablet --port=9212 --log_dir=./log/tablet &
+BUILD_DIR=../build
+${BUILD_DIR}/src/fesql --role=dbms --tablet_endpoint=127.0.0.1:9212 --port=9211 --log_dir=./log/dbms  &
+${BUILD_DIR}/src/fesql --role=tablet --port=9212 --log_dir=./log/tablet &
--- onebox/start_cli.sh
+++ onebox/start_cli.sh
@@ -2,6 +2,7 @@
 #
 # start_cli.sh
 mkdir -p log/cli
-../build/src/fesql --role=client --tablet_endpoint=127.0.0.1:9212 --endpoint=127.0.0.1:9211 --log_dir=log/cli
+BUILD_DIR=../build
+${BUILD_DIR}/src/fesql --role=client --tablet_endpoint=127.0.0.1:9212 --endpoint=127.0.0.1:9211 --log_dir=log/cli
 
 
