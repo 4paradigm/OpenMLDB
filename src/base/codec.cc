8534a04d9844649e6a2269dd29be3d49163e91fd
panxuchen <panxuchen@4paradigm.com>
2020-02-25 15:58:04 +0800

    add type.proto

--- src/base/codec.cc
+++ src/base/codec.cc
@@ -29,14 +29,14 @@ namespace base {
 
 #define BitMapSize(size) (((size) >> 3) + !!((size)&0x07))
 
-static const std::unordered_map<::rtidb::common::DataType, uint8_t> TYPE_SIZE_MAP = {
-    {::rtidb::common::kBool, sizeof(bool)},
-    {::rtidb::common::kInt16, sizeof(int16_t)},
-    {::rtidb::common::kInt32, sizeof(int32_t)},
-    {::rtidb::common::kFloat, sizeof(float)},
-    {::rtidb::common::kInt64, sizeof(int64_t)},
-    {::rtidb::common::kTimestamp, sizeof(int64_t)},
-    {::rtidb::common::kDouble, sizeof(double)}};
+static const std::unordered_map<::rtidb::type::DataType, uint8_t> TYPE_SIZE_MAP = {
+    {::rtidb::type::kBool, sizeof(bool)},
+    {::rtidb::type::kInt16, sizeof(int16_t)},
+    {::rtidb::type::kInt32, sizeof(int32_t)},
+    {::rtidb::type::kFloat, sizeof(float)},
+    {::rtidb::type::kInt64, sizeof(int64_t)},
+    {::rtidb::type::kTimestamp, sizeof(int64_t)},
+    {::rtidb::type::kDouble, sizeof(double)}};
 
 static inline uint8_t GetAddrLength(uint32_t size) {
     if (size <= UINT8_MAX) {
@@ -62,7 +62,7 @@ RowBuilder::RowBuilder(const Schema& schema)
     str_field_start_offset_ = HEADER_LENGTH + BitMapSize(schema.size());
     for (int idx = 0; idx < schema.size(); idx++) {
         const ::rtidb::common::ColumnDesc& column = schema.Get(idx);
-        if (column.data_type() == ::rtidb::common::kVarchar) {
+        if (column.data_type() == ::rtidb::type::kVarchar) {
             offset_vec_.push_back(str_field_cnt_);
             str_field_cnt_++;
         } else {
@@ -113,7 +113,7 @@ uint32_t RowBuilder::CalTotalLength(uint32_t string_length) {
     return 0;
 }
 
-bool RowBuilder::Check(::rtidb::common::DataType type) {
+bool RowBuilder::Check(::rtidb::type::DataType type) {
     if ((int32_t)cnt_ >= schema_.size()) {
         return false;
     }
@@ -121,7 +121,7 @@ bool RowBuilder::Check(::rtidb::common::DataType type) {
     if (column.data_type() != type) {
         return false;
     }
-    if (column.data_type() != ::rtidb::common::kVarchar) {
+    if (column.data_type() != ::rtidb::type::kVarchar) {
         auto iter = TYPE_SIZE_MAP.find(column.data_type());
         if (iter == TYPE_SIZE_MAP.end()) {
             return false;
@@ -134,7 +134,7 @@ bool RowBuilder::AppendNULL() {
     int8_t* ptr = buf_ + HEADER_LENGTH + (cnt_ >> 3);
     *(reinterpret_cast<uint8_t*>(ptr)) |= 1 << (cnt_ & 0x07);
     const ::rtidb::common::ColumnDesc& column = schema_.Get(cnt_);
-    if (column.data_type() == ::rtidb::common::kVarchar) {
+    if (column.data_type() == ::rtidb::type::kVarchar) {
         ptr = buf_ + str_field_start_offset_ +
               str_addr_length_ * offset_vec_[cnt_];
         if (str_addr_length_ == 1) {
@@ -155,7 +155,7 @@ bool RowBuilder::AppendNULL() {
 }
 
 bool RowBuilder::AppendBool(bool val) {
-    if (!Check(::rtidb::common::kBool)) return false;
+    if (!Check(::rtidb::type::kBool)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<uint8_t*>(ptr)) = val ? 1 : 0;
     cnt_++;
@@ -163,7 +163,7 @@ bool RowBuilder::AppendBool(bool val) {
 }
 
 bool RowBuilder::AppendInt32(int32_t val) {
-    if (!Check(::rtidb::common::kInt32)) return false;
+    if (!Check(::rtidb::type::kInt32)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<int32_t*>(ptr)) = val;
     cnt_++;
@@ -171,7 +171,7 @@ bool RowBuilder::AppendInt32(int32_t val) {
 }
 
 bool RowBuilder::AppendInt16(int16_t val) {
-    if (!Check(::rtidb::common::kInt16)) return false;
+    if (!Check(::rtidb::type::kInt16)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<int16_t*>(ptr)) = val;
     cnt_++;
@@ -179,7 +179,7 @@ bool RowBuilder::AppendInt16(int16_t val) {
 }
 
 bool RowBuilder::AppendTimestamp(int64_t val) {
-    if (!Check(::rtidb::common::kTimestamp)) return false;
+    if (!Check(::rtidb::type::kTimestamp)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<int64_t*>(ptr)) = val;
     cnt_++;
@@ -187,7 +187,7 @@ bool RowBuilder::AppendTimestamp(int64_t val) {
 }
 
 bool RowBuilder::AppendInt64(int64_t val) {
-    if (!Check(::rtidb::common::kInt64)) return false;
+    if (!Check(::rtidb::type::kInt64)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<int64_t*>(ptr)) = val;
     cnt_++;
@@ -195,7 +195,7 @@ bool RowBuilder::AppendInt64(int64_t val) {
 }
 
 bool RowBuilder::AppendFloat(float val) {
-    if (!Check(::rtidb::common::kFloat)) return false;
+    if (!Check(::rtidb::type::kFloat)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<float*>(ptr)) = val;
     cnt_++;
@@ -203,7 +203,7 @@ bool RowBuilder::AppendFloat(float val) {
 }
 
 bool RowBuilder::AppendDouble(double val) {
-    if (!Check(::rtidb::common::kDouble)) return false;
+    if (!Check(::rtidb::type::kDouble)) return false;
     int8_t* ptr = buf_ + offset_vec_[cnt_];
     *(reinterpret_cast<double*>(ptr)) = val;
     cnt_++;
@@ -211,7 +211,7 @@ bool RowBuilder::AppendDouble(double val) {
 }
 
 bool RowBuilder::AppendString(const char* val, uint32_t length) {
-    if (val == NULL || !Check(::rtidb::common::kVarchar)) return false;
+    if (val == NULL || !Check(::rtidb::type::kVarchar)) return false;
     if (str_offset_ + length > size_) return false;
     int8_t* ptr =
         buf_ + str_field_start_offset_ + str_addr_length_ * offset_vec_[cnt_];
@@ -268,7 +268,7 @@ bool RowView::Init() {
     uint32_t offset = HEADER_LENGTH + BitMapSize(schema_.size());
     for (int idx = 0; idx < schema_.size(); idx++) {
         const ::rtidb::common::ColumnDesc& column = schema_.Get(idx);
-        if (column.data_type() == ::rtidb::common::kVarchar) {
+        if (column.data_type() == ::rtidb::type::kVarchar) {
             offset_vec_.push_back(string_field_cnt_);
             string_field_cnt_++;
         } else {
@@ -313,7 +313,7 @@ bool RowView::Reset(const int8_t* row) {
     return true;
 }
 
-bool RowView::CheckValid(uint32_t idx, ::rtidb::common::DataType type) {
+bool RowView::CheckValid(uint32_t idx, ::rtidb::type::DataType type) {
     if (row_ == NULL || !is_valid_) {
         return false;
     }
@@ -331,7 +331,7 @@ int32_t RowView::GetBool(uint32_t idx, bool* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kBool)) {
+    if (!CheckValid(idx, ::rtidb::type::kBool)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -351,7 +351,7 @@ int32_t RowView::GetInt32(uint32_t idx, int32_t* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kInt32)) {
+    if (!CheckValid(idx, ::rtidb::type::kInt32)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -366,7 +366,7 @@ int32_t RowView::GetTimestamp(uint32_t idx, int64_t* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kTimestamp)) {
+    if (!CheckValid(idx, ::rtidb::type::kTimestamp)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -381,7 +381,7 @@ int32_t RowView::GetInt64(uint32_t idx, int64_t* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kInt64)) {
+    if (!CheckValid(idx, ::rtidb::type::kInt64)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -396,7 +396,7 @@ int32_t RowView::GetInt16(uint32_t idx, int16_t* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kInt16)) {
+    if (!CheckValid(idx, ::rtidb::type::kInt16)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -411,7 +411,7 @@ int32_t RowView::GetFloat(uint32_t idx, float* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kFloat)) {
+    if (!CheckValid(idx, ::rtidb::type::kFloat)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -426,7 +426,7 @@ int32_t RowView::GetDouble(uint32_t idx, double* val) {
     if (val == NULL) {
         return -1;
     }
-    if (!CheckValid(idx, ::rtidb::common::kDouble)) {
+    if (!CheckValid(idx, ::rtidb::type::kDouble)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
@@ -438,23 +438,23 @@ int32_t RowView::GetDouble(uint32_t idx, double* val) {
 }
 
 int32_t RowView::GetInteger(const int8_t* row, uint32_t idx,
-                            ::rtidb::common::DataType type, int64_t* val) {
+                            ::rtidb::type::DataType type, int64_t* val) {
     int32_t ret = 0;
     switch (type) {
-        case ::rtidb::common::kInt16: {
+        case ::rtidb::type::kInt16: {
             int16_t tmp_val = 0;
             ret = GetValue(row, idx, type, &tmp_val);
             if (ret == 0) *val = tmp_val;
             break;
         }
-        case ::rtidb::common::kInt32: {
+        case ::rtidb::type::kInt32: {
             int32_t tmp_val = 0;
             GetValue(row, idx, type, &tmp_val);
             if (ret == 0) *val = tmp_val;
             break;
         }
-        case ::rtidb::common::kTimestamp:
-        case ::rtidb::common::kInt64: {
+        case ::rtidb::type::kTimestamp:
+        case ::rtidb::type::kInt64: {
             int64_t tmp_val = 0;
             GetValue(row, idx, type, &tmp_val);
             if (ret == 0) *val = tmp_val;
@@ -467,7 +467,7 @@ int32_t RowView::GetInteger(const int8_t* row, uint32_t idx,
 }
 
 int32_t RowView::GetValue(const int8_t* row, uint32_t idx,
-                          ::rtidb::common::DataType type, void* val) {
+                          ::rtidb::type::DataType type, void* val) {
     if (schema_.size() == 0 || row == NULL) {
         return -1;
     }
@@ -486,7 +486,7 @@ int32_t RowView::GetValue(const int8_t* row, uint32_t idx,
     }
     uint32_t offset = offset_vec_.at(idx);
     switch (type) {
-        case ::rtidb::common::kBool: {
+        case ::rtidb::type::kBool: {
             int8_t v = v1::GetBoolField(row, offset);
             if (v == 1) {
                 *(reinterpret_cast<bool*>(val)) = true;
@@ -495,20 +495,20 @@ int32_t RowView::GetValue(const int8_t* row, uint32_t idx,
             }
             break;
         }
-        case ::rtidb::common::kInt16:
+        case ::rtidb::type::kInt16:
             *(reinterpret_cast<int16_t*>(val)) = v1::GetInt16Field(row, offset);
             break;
-        case ::rtidb::common::kInt32:
+        case ::rtidb::type::kInt32:
             *(reinterpret_cast<int32_t*>(val)) = v1::GetInt32Field(row, offset);
             break;
-        case ::rtidb::common::kTimestamp:
-        case ::rtidb::common::kInt64:
+        case ::rtidb::type::kTimestamp:
+        case ::rtidb::type::kInt64:
             *(reinterpret_cast<int64_t*>(val)) = v1::GetInt64Field(row, offset);
             break;
-        case ::rtidb::common::kFloat:
+        case ::rtidb::type::kFloat:
             *(reinterpret_cast<float*>(val)) = v1::GetFloatField(row, offset);
             break;
-        case ::rtidb::common::kDouble:
+        case ::rtidb::type::kDouble:
             *(reinterpret_cast<double*>(val)) = v1::GetDoubleField(row, offset);
             break;
         default:
@@ -526,7 +526,7 @@ int32_t RowView::GetValue(const int8_t* row, uint32_t idx, char** val,
         return false;
     }
     const ::rtidb::common::ColumnDesc& column = schema_.Get(idx);
-    if (column.data_type() != ::rtidb::common::kVarchar) {
+    if (column.data_type() != ::rtidb::type::kVarchar) {
         return false;
     }
     uint32_t size = GetSize(row);
@@ -551,7 +551,7 @@ int32_t RowView::GetString(uint32_t idx, char** val, uint32_t* length) {
         return -1;
     }
 
-    if (!CheckValid(idx, ::rtidb::common::kVarchar)) {
+    if (!CheckValid(idx, ::rtidb::type::kVarchar)) {
         return -1;
     }
     if (IsNULL(row_, idx)) {
