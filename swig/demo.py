7ef62612fecad5d830834371e6cd4703d4a93e17
kongquan <kongquan@4paradigm.com>
2020-02-26 21:40:06 +0800

    fix error tips

--- swig/demo.py
+++ swig/demo.py
@@ -1,5 +1,13 @@
 import client
-nsc = client.RtidbNSClient("172.27.128.33:6181", "/multidc2", "")
+nsc = client.RtidbNSClient()
+ok = nsc.Init("172.27.128.33:6182", "/multidc2", "")
+if not ok:
+  print("initial ns client failed!")
+
+ok = nsc.Init("172.27.128.33:6181", "/multidc2", "")
+if ok:
+  print("initial ns client success!")
+
 resp = nsc.ShowTable("")
 for i in resp:
     print(i)
