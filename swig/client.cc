5b9ad6127e2e1f6ec43ea5b325deb125fb412eb9
kongquan <kongquan@4paradigm.com>
2020-02-26 21:23:44 +0800

    sdk to class

--- swig/client.cc
+++ swig/client.cc
@@ -1,43 +1,41 @@
 #include "client.h"
 
-rtidb::client::NsClient* InitNsClient(const std::string& zk_cluster, const std::string& zk_path, const std::string& endpoint) {
-    std::shared_ptr<rtidb::zk::ZkClient> zk_client;
+RtidbNSClient::RtidbNSClient(const std::string& zk_cluster, const std::string& zk_path, const std::string& endpoint) {
     std::string value;
     if (!zk_cluster.empty()) {
         zk_client = std::make_shared<rtidb::zk::ZkClient>(zk_cluster, 1000, "", zk_path);
         if (!zk_client->Init()) {
             std::cerr << "zk client init failed" << std::endl;
-            return NULL;
+            return;
         }
         std::string node_path = zk_path + "/leader";
         std::vector<std::string> children;
         if (!zk_client->GetChildren(node_path, children) || children.empty()) {
             std::cerr << "get children failed" << std::endl;
-            return NULL;
+            return;
         }
         std::string leader_path = node_path + "/" + children[0];
         if (!zk_client->GetNodeValue(leader_path, value)) {
             std::cerr << "get leader failed" << std::endl;
-            return NULL;
+            return;
         }
         std::cout << "ns leader: " << value << std::endl;
     } else if (!endpoint.empty()) {
         value = endpoint;
     } else {
         std::cerr << "start failed! not set endpoint or zk_cluster";
-        return NULL;
+        return;
     }
 
-    rtidb::client::NsClient* client = new rtidb::client::NsClient(value);
+    client = std::make_shared<rtidb::client::NsClient>(value);
     if (client->Init() < 0) {
         std::cerr << "client init failed" << std::endl;
-        delete client;
-        return NULL;
+        return;
     }
-    return client;
+    return;
 }
 
-std::vector<std::string>* ShowTable(rtidb::client::NsClient* client, const std::string& name) {
+std::vector<std::string>* RtidbNSClient::ShowTable(const std::string& name) {
     std::vector<rtidb::nameserver::TableInfo> tables;
     std::string msg;
     std::vector<std::string>* table_names = new std::vector<std::string>;
@@ -50,3 +48,4 @@ std::vector<std::string>* ShowTable(rtidb::client::NsClient* client, const std::
     }
     return table_names;
 }
+
--- swig/client.h
+++ swig/client.h
@@ -2,5 +2,14 @@
 #include "zk/zk_client.h"
 #include <string>
 
-rtidb::client::NsClient* InitNsClient(const std::string& zk_cluster, const std::string& zk_path, const std::string& endpoint);
-std::vector<std::string>* ShowTable(rtidb::client::NsClient* client, const std::string& name);
+class RtidbNSClient {
+private:
+    std::shared_ptr<rtidb::zk::ZkClient> zk_client;
+    std::shared_ptr<rtidb::client::NsClient> client;
+
+public:
+    ~RtidbNSClient() {};
+    RtidbNSClient(const std::string& zk_cluster, const std::string& zk_path, const std::string& endpoint);
+    std::vector<std::string>* ShowTable(const std::string& name);
+
+};
