480b525d34703876af7887af1d07bd978ab2eeec
chenjing <chenjing@4paradigm.com>
2020-03-09 14:08:35 +0800

    fix compile script

--- tools/compile_and_test_in_docker.sh
+++ tools/compile_and_test_in_docker.sh
@@ -3,5 +3,5 @@
 # compile_in_docker.sh
 rm thirdparty
 ln -sf /depends/thirdparty thirdparty
-source /opt/rh/devtoolset-8/enable && bash tools/compile_and_test.sh
+source /opt/rh/devtoolset-7/enable && bash tools/compile_and_test.sh
 
