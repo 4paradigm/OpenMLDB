7c924b2c07d8745224c20de6fca6bedaba1b5b3b
imotai <wangtaize@4paradigm.com>
2020-03-26 11:53:40 +0800

    feat: support swig and py3

--- tools/compile.sh
+++ tools/compile.sh
@@ -2,7 +2,7 @@
 #
 # compile.sh
 PWD=`pwd`
-export PATH=${PWD}/thirdparty/bin:$PATH
+export JAVA_HOME=${PWD}/thirdparty/jdk1.8.0_141
+export PATH=${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH
 mkdir -p build && cd build 
 cmake .. && make fesql_proto && make fesql_parser && make -j20
-
