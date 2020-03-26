0db274369c1c8cee4a6e87012f45942ef4ea6e56
imotai <wangtaize@4paradigm.com>
2020-03-26 14:23:52 +0800

    feat: add python __init__.py

---
+++ python/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 wangtaize <wangtaize@m7-pce-dev01>
+#
+# Distributed under terms of the MIT license.
+
+from . import fesql
+from .fesql import *
