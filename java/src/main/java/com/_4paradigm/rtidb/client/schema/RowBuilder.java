2c314a27919ec5ccaaef623d8c20345b99fe74fe
kongquan <kongquan@4paradigm.com>
2020-04-07 13:44:46 +0800

    remove unused package

--- java/src/main/java/com/_4paradigm/rtidb/client/schema/RowBuilder.java
+++ java/src/main/java/com/_4paradigm/rtidb/client/schema/RowBuilder.java
@@ -5,7 +5,6 @@ import com._4paradigm.rtidb.client.type.DataType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.crypto.Data;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
