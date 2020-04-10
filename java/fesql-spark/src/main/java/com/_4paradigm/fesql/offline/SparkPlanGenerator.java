fb839c011f574a1aff10a7dbfa61cf5086af69c1
tobe <tobeg3oogle@gmail.com>
2020-04-10 14:29:51 +0800

    Add fesql-spark maven project

---
+++ java/fesql-spark/src/main/java/com/_4paradigm/fesql/offline/SparkPlanGenerator.java
@@ -0,0 +1,9 @@
+package com._4paradigm.fesql.offline;
+
+import com._4paradigm.fesql.DBMSSdk;
+import com._4paradigm.fesql.Status;
+import com._4paradigm.fesql.fesql;
+
+public class SparkPlanGenerator {
+
+}
