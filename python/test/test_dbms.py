2cf155060dccaa8ed19be75d16d346ac83a3a48f
baoxinqi <baoxinqi@4paradigm.com>
2020-04-13 14:09:35 +0800

    fix: Fix python test

--- python/test/test_dbms.py
+++ python/test/test_dbms.py
@@ -9,21 +9,21 @@
 """
 
 from test_base import *
-import fesql_interface
+import fesql
 
 
 def test_dbms_create_db():
-    dbms = fesql_interface.CreateDBMSSdk(dbms_endpoint)
+    dbms = fesql.CreateDBMSSdk(dbms_endpoint)
     cases= [("ns1", 0), ('ns2', 0)]
     for case in cases:
         yield run_db_check, dbms, case[0], case[1]
 
 def run_db_check(dbms, ns, result):
     assert dbms
-    status = fesql_interface.Status()
+    status = fesql.Status()
     dbms.CreateDatabase(ns, status)
     assert status.code == result
 
 def test_dbms_valid():
-    dbms = fesql_interface.CreateDBMSSdk("xxxxx")
+    dbms = fesql.CreateDBMSSdk("xxxxx")
     assert not dbms
