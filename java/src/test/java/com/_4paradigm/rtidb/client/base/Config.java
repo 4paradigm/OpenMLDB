28006dfce75932a52b93fb448ad88ce2b9bffe87
wangbao <wangbao@4paradigm.com>
2020-04-16 15:25:08 +0800

    Merge branch 'develop' of /Users/innerpeace/rtidb-client/rtidb with conflicts.

--- java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
+++ java/src/test/java/com/_4paradigm/rtidb/client/base/Config.java
@@ -1,10 +1,10 @@
 package com._4paradigm.rtidb.client.base;
 
 public class Config {
-    public static final String ZK_ENDPOINTS = "127.0.0.1:6181";
-    public static final String ZK_ROOT_PATH = "/onebox";
-    public static final String ENDPOINT = "127.0.0.1:9522";
-    public static final String[] NODES = new String[]{"127.0.0.1:9522", "127.0.0.1:9521", "127.0.0.1:9520"};
+    public static final String ZK_ENDPOINTS = "172.27.128.37:7181,172.27.128.37:7182,172.27.128.37:7183";
+    public static final String ZK_ROOT_PATH = "/rtidb_cluster";
+    public static final String ENDPOINT = "172.27.128.37:9533";
+    public static final String[] NODES = new String[]{"172.27.128.37:9533", "172.27.128.37:9532", "172.27.128.37:9531"};
     public static final int READ_TIMEOUT = 1000000;
     public static final int WRITE_TIMEOUT = 1000000;
 }
