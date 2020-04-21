fe0786b7e336996970c6e06ab801114458960a58
panxuchen <panxuchen@4paradigm.com>
2020-04-21 22:10:40 +0800

    change parameter

--- src/base/glog_wapper.h
+++ src/base/glog_wapper.h
@@ -1,3 +1,8 @@
+//
+// glog_wapper.cc
+// Copyright 2017 4paradigm.com
+
+
 #ifndef GLOG_WAPPER_H_
 #define GLOG_WAPPER_H_
 
@@ -10,13 +15,15 @@ using google::INFO;
 using google::WARNING;
 using google::ERROR;
 using google::FATAL;
+using rtidb::base::DEBUG;
 
 namespace rtidb {
 namespace base {
 
+    const int DEBUG = -1;
+
     template<typename... Arguments>
-    std::string FormatArgs(const std::string& fmt, const Arguments&... args)
-    {
+    std::string FormatArgs(const char* fmt, const Arguments&... args) {
         boost::format f(fmt);
         std::initializer_list<char> {(static_cast<void>(
             f % args
