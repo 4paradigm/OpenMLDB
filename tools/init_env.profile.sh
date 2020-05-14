551727e8df0a2df9631984f292607e1cb41368b4
imotai <wangtaize@4paradigm.com>
2020-05-14 14:03:19 +0800

    fix: update  tools/init_env.profile.sh

--- tools/init_env.profile.sh
+++ tools/init_env.profile.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 echo "CICD environment tag: ${CICD_RUNNER_TAG}"
 echo "Third party packages path: ${CICD_RUNNER_THIRDPARTY_PATH}"
-if [[ ${CICD_RUNNER_TAG} == "macos" ]]; then
-    echo "Run in mac environment"
-    ln -sf ${CICD_RUNNER_THIRDPARTY_PATH} thirdparty
-else
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
     ln -sf /depends/thirdparty thirdparty
     source /opt/rh/python27/enable
     source /opt/rh/devtoolset-7/enable
     export JAVA_HOME=${PWD}/thirdparty/jdk1.8.0_141
     export PATH=${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH
+else
+    ln -sf ${CICD_RUNNER_THIRDPARTY_PATH} thirdparty
 fi
-
