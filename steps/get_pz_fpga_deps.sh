bfea8138d1654c4d72e2dca3dd6ed5f6b2e211bc
Yang Jun01 <yangjun01@4paradigm.com>
2020-05-14 18:48:27 +0800

    fix

--- steps/get_pz_fpga_deps.sh
+++ steps/get_pz_fpga_deps.sh
@@ -5,4 +5,4 @@ curl ftp://172.27.12.154/pub/team_hpc/pz_rtidb/rocksdb-1a3eeee5.tar.gz -o rocksd
 tar zxf rocksdb.tar.gz -C ./thirdparty
 wget "https://nexus.4pd.io/repository/raw-hosted/fpga-rte/dev/aclrte-linux64-rte-19.2.0.57-fa510q-190430-4pdx900-200408.tar.gz"
 tar zxf aclrte-linux64-rte-19.2.0.57-fa510q-190430-4pdx900-200408.tar.gz -C ./thirdparty
-ln -s ./thirdparty/aclrte-linux64/lib/* ./thirdparty/lib/
+cp -L ./thirdparty/aclrte-linux64/lib/* ./thirdparty/lib/
