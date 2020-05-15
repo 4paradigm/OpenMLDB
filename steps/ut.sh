6eb51321a30d51c442cbd30d572a3d8dc5c69376
Yang Jun01 <yangjun01@4paradigm.com>
2020-05-16 00:01:27 +0800

    Save core dumps

--- steps/ut.sh
+++ steps/ut.sh
@@ -11,6 +11,7 @@ cd thirdsrc/zookeeper-3.4.14 && ./bin/zkServer.sh start && cd $WORK_DIR
 sleep 5
 TMPFILE="code.tmp"
 echo 0 > $TMPFILE
+mkdir -p core-dumps
 ls  build/bin/ | grep test | grep -v grep | while read line 
 do 
     ./build/bin/$line --gtest_output=xml:./reports/$line.xml
@@ -18,8 +19,13 @@ do
     echo "$line result code is: $RET"
     if [ $RET -ne 0 ];then 
         echo $RET > $TMPFILE
+        mv core.* core-dumps/ -f
     fi 
 done
+tar zcf core-dumps.tar.gz core-dumps
+echo "core-dumps saved"
+curl -T core-dumps.tar.gz ftp://172.27.12.154/pub/team_hpc/pz_rtidb/
+echo "core-dumps uploaded"
 code=`cat $TMPFILE`
 echo "code result: $code"
 rm $TMPFILE
