8f2ba795c3dc471cba55610a9d6918779824a1ba
imotai <wangtaize@4paradigm.com>
2020-05-16 09:46:57 +0800

    fix: fix ns duplicate bug

--- python/test/test_dbms.py
+++ python/test/test_dbms.py
@@ -9,6 +9,7 @@
 """
 
 from test_base import *
+import time
 import fesql
 
 def run_db_check(dbms, ns, result):
@@ -19,10 +20,9 @@ def run_db_check(dbms, ns, result):
 
 def test_dbms_create_db():
     dbms = fesql.CreateDBMSSdk(dbms_endpoint)
-    cases= [("ns1", 0), ('ns2', 0)]
+    cases= [("ns1" + str(time.time()) , 0), ('ns2' + str(time.time()), 0)]
     for case in cases:
         yield run_db_check, dbms, case[0], case[1]
-
 
 def test_dbms_valid():
     dbms = fesql.CreateDBMSSdk("xxxxx")
