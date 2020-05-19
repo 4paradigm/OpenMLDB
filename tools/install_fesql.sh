89a3b6d40418d951f79db3ed879a8d5d503d0fd1
kongquan <kongquan@4paradigm.com>
2020-05-19 17:54:41 +0800

    remove exit

--- tools/install_fesql.sh
+++ tools/install_fesql.sh
@@ -5,7 +5,6 @@ export JAVA_HOME=${RTIDB_DEV_JAVA_HOME:-/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.25
 export RTIDB_THIRDPARTY=${RTIDB_DEV_THIRDPARTY:-/depends/thirdparty}
 echo $JAVA_HOME
 echo $RTIDB_THIRDPARTY
-exit 1
 cd fesql && ln -sf ${RTIDB_DEV_THIRDPARTY} thirdparty && mkdir -p build
 cd build && cmake -DCMAKE_INSTALL_PREFIX="${RTIDB_THIRDPARTY}" -DTESTING_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DPYSDK_ENABLE=OFF -DJAVASDK_ENABLE=OFF -DEXPRIRMENT_ENABLE=OFF .. && make -j10 install
 
