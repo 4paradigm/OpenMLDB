ccec0206a4b6931a7307769b39e7c8ace3893cd8
Yang Jun01 <yangjun01@4paradigm.com>
2020-05-21 12:20:12 +0800

    update fpga-rte url

--- steps/get_pz_fpga_deps.sh
+++ steps/get_pz_fpga_deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl ftp://172.27.12.154/pub/team_hpc/titanse/pz-titanse.tar.gz -o pz.tar.gz
 tar zxf pz.tar.gz -C ./thirdparty
-curl ftp://172.27.12.154/pub/team_hpc/titanse/rocksdb-titanse-9e5f14ff.tar.gz -o rocksdb.tar.gz
+curl ftp://172.27.12.154/pub/team_hpc/titanse/rocksdb-titanse-28b0a477.tar.gz -o rocksdb.tar.gz
 tar zxf rocksdb.tar.gz -C ./thirdparty
-curl "https://nexus.4pd.io/repository/raw-hosted/fpga-rte/release/v0.2/aclrte-linux64-rte-19.2.0.57-v0.2.tar.gz" -O
-tar zxf aclrte-linux64-rte-19.2.0.57-v0.2.tar.gz -C ./thirdparty
+curl "https://nexus.4pd.io/repository/raw-hosted/fpga-rte/release/v0.2/aclrte-linux64-aclrte-19.2.0.57-v0.2-release.tar.gz" -O
+tar zxf aclrte-linux64-aclrte-19.2.0.57-v0.2-release.tar.gz -C ./thirdparty
 cp -L ./thirdparty/aclrte-linux64/lib/* ./thirdparty/lib/
