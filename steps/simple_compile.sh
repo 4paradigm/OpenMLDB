755826101511ecc5fad0f9aaec8167641c303d41
imotai <wangtaize@4paradigm.com>
2020-06-05 18:42:58 +0800

    fix: add simple compile

--- steps/simple_compile.sh
+++ steps/simple_compile.sh
@@ -3,16 +3,9 @@
 # compile.sh
 
 WORK_DIR=`pwd`
-if [ "$1" = "DEBUG" ]
-then
-    sed -i '/set(CMAKE_BUILD_TYPE/c\set(CMAKE_BUILD_TYPE Debug)' CMakeLists.txt 
-else
-    sed -i '/set(CMAKE_BUILD_TYPE/c\set(CMAKE_BUILD_TYPE RelWithDebInfo)' CMakeLists.txt 
-fi
-
 sh steps/gen_code.sh
 mkdir -p $WORK_DIR/build 
-cd $WORK_DIR/build && cmake .. && make -j10 rtidb sql_pysdk_package sql_javasdk_package
+cd $WORK_DIR/build && cmake .. && make -j4 rtidb sql_pysdk_package sql_javasdk_package
 code=$?
 cd $WORK_DIR
 exit $code
