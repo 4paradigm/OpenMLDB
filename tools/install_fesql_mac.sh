106ab447dc9348ae77d27cb548d4ea82a6811fc9
imotai <imotai@imotaideMacBook-Pro.local>
2020-06-05 18:19:45 +0800

    feat: add mac build cicd

---
+++ tools/install_fesql_mac.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+# install_fesql_mac.sh
+export RTIDB_THIRDPARTY=/opt/depends/thirdparty
+cd fesql && ln -sf ${RTIDB_THIRDPARTY} thirdparty && mkdir -p build
+cd build && cmake -DCMAKE_INSTALL_PREFIX="${RTIDB_THIRDPARTY}" -DTESTING_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DPYSDK_ENABLE=OFF -DJAVASDK_ENABLE=OFF -DEXPRIRMENT_ENABLE=OFF .. && make fesql_proto fesql_parser &&make -j4 install
