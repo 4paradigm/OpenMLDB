cfffd4d2f4900cfd03692cad70384d637d151add
tobe <tobeg3oogle@gmail.com>
2020-06-09 16:33:43 +0800

    Rename for output schema with different column name

--- java/fesql-spark/src/main/scala/com/_4paradigm/fesql/offline/nodes/SimpleProjectPlan.scala
+++ java/fesql-spark/src/main/scala/com/_4paradigm/fesql/offline/nodes/SimpleProjectPlan.scala
@@ -1,7 +1,12 @@
 package com._4paradigm.fesql.offline.nodes
 
 import com._4paradigm.fesql.offline._
+import com._4paradigm.fesql.offline.utils.FesqlUtil
+import com._4paradigm.fesql.offline.utils.FesqlUtil.getSparkType
 import com._4paradigm.fesql.vm.PhysicalSimpleProjectNode
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.functions.col
+import scala.collection.JavaConverters._
 
 object SimpleProjectPlan {
 
@@ -15,12 +20,22 @@ object SimpleProjectPlan {
     val inputInstance = inputs.head
     val inputDf = inputInstance.getDf(ctx.getSparkSession)
 
+    // Get the output column names from output schema
+    val outputColNameList = node.GetOutputSchema().asScala.map(col =>
+      col.getName
+    ).toList
+
+    // Get the select column names from node
     val columnSourceList = node.getProject_().getColumn_sources_()
-    val selectColNameList = (0 until columnSourceList.size()).map(i => {
-      columnSourceList.get(i).column_name()
+
+    val selectColList = (0 until columnSourceList.size()).map(i => {
+      // Take the select column and rename with output schema
+      col(columnSourceList.get(i).column_name()).as(outputColNameList(i))
     }).toList
 
-    val result = inputDf.select(selectColNameList.head, selectColNameList.tail: _*)
+    // Use Spark DataFrame to select
+    val result = inputDf.select(selectColList:_*)
+
     SparkInstance.fromDataFrame(result)
   }
 
