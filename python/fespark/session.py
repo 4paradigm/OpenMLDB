816f1accce1b47017ee4001c101632bfe90ac8ec
tobe <tobeg3oogle@gmail.com>
2020-06-18 16:29:43 +0800

    Remove unused api for python session api

--- python/fespark/session.py
+++ python/fespark/session.py
@@ -6,7 +6,7 @@ from .backend import GlobalBackend
 
 class FesqlSession(object):
 
-    def __init__(self, pySparkSession=None, jsession=None):
+    def __init__(self, pySparkSession=None):
         self.jsession = None
 
         # Init the py4j jvm from local or spark-submit
@@ -14,11 +14,6 @@ class FesqlSession(object):
 
         if pySparkSession is not None:
             self.jsession = GlobalBackend.get().fesql_api.FesqlSession(pySparkSession._jsparkSession)
-
-        # Use the jsession if given, which may be used when Java creates the session and pass to Python
-        if jsession is not None:
-            self.jsession = jsession
-            return
 
         if "PYSPARK_GATEWAY_PORT" not in os.environ: # Run with spark-submit
             from pyspark.sql import SparkSession
