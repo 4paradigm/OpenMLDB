2c2367a80c0f469c20c664f6e46e35bdb5f4d7b3
imotai <wangtaize@4paradigm.com>
2020-07-01 18:33:21 +0800

    fix:update docker file

--- docker/cluster/build_docker.sh
+++ docker/cluster/build_docker.sh
@@ -8,9 +8,16 @@ cp zookeeper-3.4.14/conf/zoo_sample.cfg zookeeper-3.4.14/conf/zoo.cfg
 wget http://pkg.4paradigm.com/rtidb/dev/jdk-8u121-linux-x64.tar.gz
 tar -zxvf jdk-8u121-linux-x64.tar.gz
 
+wget http://pkg.4paradigm.com/fedb/spark/spark-2.3.4-bin-hadoop2.7.tar.gz
+tar -zxvf spark-2.3.4-bin-hadoop2.7.tar.gz
+
 cp -r ../../release/conf ./
 cp -r ../../release/bin ./
-cp ../../build/bin/rtidb bin/
+cp ../../build/bin/rtidb bin/fedb
+cp -r ../../build/sql_pysdk sql_pysdk
+cp -r ../../fesql/build/python fe_python
+sed -i 's/rtidb/fedb/g' bin/boot.sh
+sed -i 's/rtidb/fedb/g' bin/boot_ns.sh
 
-docker build -t develop-registry.4pd.io/fedb:0.0.0 .
-docker push develop-registry.4pd.io/fedb:0.0.0
+docker build -t develop-registry.4pd.io/fedb:2.0.0 .
+
