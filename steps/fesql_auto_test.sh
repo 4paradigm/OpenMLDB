a6cf6705311ef12051f3b4552a7608b441aa55fd
zhaowei <zw1020688@163.com>
2020-07-01 20:34:46 +0800

    测试CICD

--- steps/fesql_auto_test.sh
+++ steps/fesql_auto_test.sh
@@ -2,6 +2,14 @@
 
 ROOT_DIR=`pwd`
 
+PROTO_BIN=$ROOT_DIR/thirdparty/bin/protoc
+ulimit -c unlimited
+sed -i "/protocExecutable/c\<protocExecutable>${PROTO_BIN}<\/protocExecutable>" java/pom.xml
+mkdir -p java/src/main/proto/
+cp -rf src/proto/tablet.proto java/src/main/proto/
+cp -rf src/proto/name_server.proto java/src/main/proto/
+cp -rf src/proto/common.proto java/src/main/proto/
+
 echo "ROOT_DIR:${ROOT_DIR}"
 
 case_xml=test_v1.xml
