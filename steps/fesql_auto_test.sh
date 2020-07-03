28fc7628ce0ad1b25cf70640967b12266a601b4e
imotai <wangtaize@4paradigm.com>
2020-07-03 13:06:14 +0800

    fix:update compile steps

--- steps/fesql_auto_test.sh
+++ steps/fesql_auto_test.sh
@@ -11,9 +11,8 @@ cp -rf src/proto/name_server.proto java/src/main/proto/
 cp -rf src/proto/common.proto java/src/main/proto/
 
 echo "ROOT_DIR:${ROOT_DIR}"
-
+sh steps/gen_code.sh
+mkdir -p build  && cd build && cmake .. && make sql_javasdk_package
 case_xml=test_v1.xml
-
-cd src/sdk/feql-auto-test-java
-
+cd ${ROOT_DIR}/src/sdk/feql-auto-test-java
 mvn clean test -DsuiteXmlFile=test_suite/${case_xml}
