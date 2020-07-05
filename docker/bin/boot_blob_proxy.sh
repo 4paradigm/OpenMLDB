8b92931320582f229563b410e30dbd70589bd0e2
kongquan <kongquan@4paradigm.com>
2020-07-05 14:25:25 +0800

    feat: output mon log to stderr, and chnage script abs path get method
    Change-Id: I7747bfc90ba6c7f98f05c504bdab9b0e5339b39b

--- docker/bin/boot_blob_proxy.sh
+++ docker/bin/boot_blob_proxy.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 #
 # boot_blob_proxy.sh
-[ -z "${zkservers}" ] && { bash ./bin/getzkconf.sh || exit 1; }
+source ./bin/getzkconf.sh
+[ -z "${zkservers}" ] && { getzk; }
 export zk_cluster=${zkservers}
 export zk_root_path=${zkpath}
 export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
 export blobproxy_port
-Endpoint=$MY_NODE_NAME:$blobproxy_port
-SELFDIR=$(dirname $0)
+Endpoint="$(hostname -i):${blobproxy_port}"
+SELFDIR=$(dirname $(readlink -f $0))
 WORKDIR=$(dirname ${SELFDIR})
 cd ${WORKDIR}
 ./bin/rtidb --flagfile=./conf/blob_proxy.flags --endpoint=$Endpoint --zk_cluster=$zk_cluster --zk_root_path=$zk_root_path
