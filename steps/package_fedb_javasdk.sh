7391ead1ee54aab790c0076e4ed0810d64ef0260
imotai <wangtaize@4paradigm.com>
2020-07-10 18:31:46 +0800

    fix: update java package

--- steps/package_fedb_javasdk.sh
+++ steps/package_fedb_javasdk.sh
@@ -2,13 +2,13 @@
 #
 # package.sh
 #
-
 WORKDIR=`pwd`
-
 set -e
-
 sdk_vesion=$1-SNAPSHOT
-mkdir -p  src/sdk/java/sql-native/src/main/resources/
+mkdir -p src/sdk/java/sql-native/src/main/resources/
+sh tools/install_fesql.sh
+mkdir -p build && cd build &&  cmake .. && make sql_jsdk
+cd ${WORKDIR}
 cp build/src/sdk/libsql_jsdk.so  src/sdk/java/sql-native/src/main/resources/
 cp build/src/sdk/libsql_jsdk.dylib  src/sdk/java/sql-native/src/main/resources/
 cd src/sdk/java/  &&  mvn versions:set -DnewVersion=${sdk_vesion} && mvn deploy -Dmaven.test.skip=true
