edeb209875a5736074f797901d1c92d3d66a6f08
wangbao <wangbao@4paradigm.com>
2020-07-10 10:56:54 +0000

    fix: add parse log

--- steps/simple_compile.sh
+++ steps/simple_compile.sh
@@ -5,7 +5,7 @@
 WORK_DIR=`pwd`
 sh steps/gen_code.sh
 mkdir -p $WORK_DIR/build 
-cd $WORK_DIR/build && cmake .. && make -j4 rtidb sql_pysdk_package sql_javasdk_package
+cd $WORK_DIR/build && cmake .. && make -j4 rtidb sql_pysdk_package sql_javasdk_package parse_log
 code=$?
 cd $WORK_DIR
 exit $code
