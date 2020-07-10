67c447b9223ce3cf1284d4ce087bfc628b029784
imotai <wangtaize@4paradigm.com>
2020-07-10 18:17:33 +0800

    fix: add resource dir

--- steps/package_fedb_javasdk.sh
+++ steps/package_fedb_javasdk.sh
@@ -8,6 +8,7 @@ WORKDIR=`pwd`
 set -e
 
 sdk_vesion=$1-SNAPSHOT
+mkdir -p  src/sdk/java/sql-native/src/main/resources/
 cp build/src/sdk/libsql_jsdk.so  src/sdk/java/sql-native/src/main/resources/
 cp build/src/sdk/libsql_jsdk.dylib  src/sdk/java/sql-native/src/main/resources/
 cd src/sdk/java/  &&  mvn versions:set -DnewVersion=${sdk_vesion} && mvn deploy -Dmaven.test.skip=true
