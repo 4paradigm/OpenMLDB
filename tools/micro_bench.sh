0b6ef655968e30a64ccdc49bef765832919b2b8f
baoxinqi <baoxinqi@4paradigm.com>
2020-07-20 21:13:29 +0800

    feat: Refactor cmake build scripts

--- tools/micro_bench.sh
+++ tools/micro_bench.sh
@@ -6,10 +6,8 @@ export JAVA_HOME=${PWD}/thirdparty/jdk1.8.0_141
 export PATH=${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH
 
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=ON
-make fesql_proto
-make fesql_parser
-make -j16 storage_bm udf_bm runner_bm engine_bm fesql_client_batch_run_bm
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=ON
+make -j16 fesql_bm
 
 echo "storage benchmark:"
 src/bm/storage_bm 2>/dev/null
