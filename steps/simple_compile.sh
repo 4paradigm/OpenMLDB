d0406f8346599cd21775eca3ab42a29e72e02cc0
chenjing <chenjing@4paradigm.com>
2020-07-29 18:11:36 +0800

    compile fix

--- steps/simple_compile.sh
+++ steps/simple_compile.sh
@@ -4,9 +4,7 @@
 
 WORK_DIR=`pwd`
 sh steps/gen_code.sh
-cd ${WORK_DIR}/fesql/java/fesql-common
-mvn clean install
-mkdir -p $WORK_DIR/build 
+mkdir -p $WORK_DIR/build
 cd $WORK_DIR/build && cmake .. && make -j16 rtidb sql_pysdk_package sql_jsdk parse_log
 code=$?
 cd $WORK_DIR
--- tools/install_fesql.sh
+++ tools/install_fesql.sh
@@ -6,4 +6,7 @@ export RTIDB_THIRDPARTY=${RTIDB_DEV_THIRDPARTY:-/depends/thirdparty}
 cd fesql && ln -sf ${RTIDB_THIRDPARTY} thirdparty && mkdir -p build
 cd build && cmake -DCMAKE_INSTALL_PREFIX="${RTIDB_THIRDPARTY}" -DTESTING_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DPYSDK_ENABLE=OFF -DJAVASDK_ENABLE=OFF -DEXPRIRMENT_ENABLE=OFF .. && make fesql_proto fesql_parser &&make -j16 install
 
+export JAVA_HOME=/depends/thirdparty/jdk1.8.0_141
+cd ${WORK_DIR}/fesql/java/fesql-common
+mvn clean install
 
