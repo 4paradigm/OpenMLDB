5fa1c9c6418b8e36b9e2306bbf3b85fa9cc27def
kongquan <kongquan@4paradigm.com>
2020-08-18 06:01:33 -0400

    feat fe auto test progress

--- steps/fesql_auto_test.sh
+++ steps/fesql_auto_test.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 ROOT_DIR=`pwd`
+ulimit -c unlimited
+
+echo "ROOT_DIR:${ROOT_DIR}"
+sh steps/gen_code.sh
+sh tools/install_fesql.sh
+mkdir -p ${ROOT_DIR}/build  && cd ${ROOT_DIR}/build && cmake .. 
+if [ -z "${FEDEV}" ]; then
+    make -j5 sql_javasdk_package || { echo "compile error"; exit 1; }
+else
+    make -j16 || { echo "compile error"; exit 1; }
+fi
 test -d /rambuild/ut_zookeeper && rm -rf /rambuild/ut_zookeeper/*
 cp steps/zoo.cfg thirdsrc/zookeeper-3.4.14/conf
 cd thirdsrc/zookeeper-3.4.14
@@ -9,10 +20,6 @@ netstat -anp | grep 6181 | awk '{print $NF}' | awk -F '/' '{print $1}'| xargs ki
 sleep 5
 cd onebox && sh start_onebox_on_rambuild.sh && cd $ROOT_DIR
 sleep 5
-echo "ROOT_DIR:${ROOT_DIR}"
-sh steps/gen_code.sh
-sh tools/install_fesql.sh
-mkdir -p ${ROOT_DIR}/build  && cd ${ROOT_DIR}/build && cmake .. && make -j16
 
 case_xml=test_v1.xml
 cd ${ROOT_DIR}/src/sdk/java/
