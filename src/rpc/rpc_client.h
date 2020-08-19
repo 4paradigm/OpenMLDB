f800dbf3e2c837f315c7bb8407ab12ebd7683734
denglong <denglong@4paradigm.com>
2020-08-19 21:13:04 +0800

    test: add debug

--- src/rpc/rpc_client.h
+++ src/rpc/rpc_client.h
@@ -135,6 +135,34 @@ class RpcClient {
     }
 
     template <class Request, class Response, class Callback>
+    bool SendRequestMsg(void (T::*func)(google::protobuf::RpcController*,
+                                     const Request*, Response*, Callback*),
+                     const Request* request, Response* response,
+                     uint64_t rpc_timeout, int retry_times, std::string* msg) {
+        brpc::Controller cntl;
+        cntl.set_log_id(log_id_++);
+        if (rpc_timeout > 0) {
+            cntl.set_timeout_ms(rpc_timeout);
+        }
+        if (retry_times > 0) {
+            cntl.set_max_retry(retry_times);
+        }
+        if (stub_ == NULL) {
+            PDLOG(WARNING,
+                  "stub is null. client must be init before send request");
+            *msg = "stub is null";
+            return false;
+        }
+        (stub_->*func)(&cntl, request, response, NULL);
+        if (!cntl.Failed()) {
+            return true;
+        }
+        PDLOG(WARNING, "request error. %s", cntl.ErrorText().c_str());
+        *msg = cntl.ErrorText().c_str();
+        return false;
+    }
+
+    template <class Request, class Response, class Callback>
     bool SendRequestGetAttachment(
         void (T::*func)(google::protobuf::RpcController*, const Request*,
                         Response*, Callback*),
