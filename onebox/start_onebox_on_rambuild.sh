6d0b5cf4a163efff35b1722983620f61d451c76b
denglong <denglong@4paradigm.com>
2020-08-19 21:34:14 +0800

    test: update request_timeout_ms

--- onebox/start_onebox_on_rambuild.sh
+++ onebox/start_onebox_on_rambuild.sh
@@ -58,6 +58,7 @@ BLOB1=$IP:9720
                    --zk_cluster=${ZK_CLUSTER}\
                    --tablet_offline_check_interval=1\
                    --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=200000\
                    --zk_root_path=/onebox > ns1.log 2>&1 &
 sleep 2
 
@@ -66,6 +67,7 @@ sleep 2
                    --zk_cluster=${ZK_CLUSTER}\
                    --tablet_offline_check_interval=1\
                    --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=200000\
                    --zk_root_path=/onebox > ns2.log 2>&1 &
 sleep 2
 
@@ -73,6 +75,7 @@ sleep 2
 ../build/bin/rtidb --endpoint=${NS3} --role=nameserver \
                    --tablet_offline_check_interval=1\
                    --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=200000\
                    --zk_cluster=${ZK_CLUSTER}\
                    --zk_root_path=/onebox > ns3.log 2>&1 &
 
