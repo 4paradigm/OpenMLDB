7e60e64d2c48b7e646f1e57e00adaa8f8c05c354
tobe <tobeg3oogle@gmail.com>
2020-08-20 16:27:23 +0800

    Add comment and format the spark groupby agg planner

--- java/fesql-spark/src/main/scala/com/_4paradigm/fesql/offline/nodes/GroupByAggregationPlan.scala
+++ java/fesql-spark/src/main/scala/com/_4paradigm/fesql/offline/nodes/GroupByAggregationPlan.scala
@@ -15,17 +15,18 @@ object GroupByAggregationPlan {
   def gen(ctx: PlanContext, node: PhysicalGroupAggrerationNode, input: SparkInstance): SparkInstance = {
     val inputDf = input.getDf(ctx.getSparkSession)
 
-    // Sort by partition keys
+    // Get parition keys
     val groupByExprs = node.getGroup_.keys()
     val groupByCols = mutable.ArrayBuffer[Column]()
     val groupIdxs = mutable.ArrayBuffer[Int]()
     for (i <- 0 until groupByExprs.GetChildNum()) {
       val expr = groupByExprs.GetChild(i)
-
       val colIdx = SparkColumnUtil.resolveColumnIndex(expr, node.GetProducer(0))
       groupIdxs += colIdx
       groupByCols += SparkColumnUtil.getCol(inputDf, colIdx)
     }
+
+    // Sort by partition keys
     val sortedInputDf = inputDf.sortWithinPartitions(groupByCols:_*)
 
     // Get schema info
@@ -34,9 +35,7 @@ object GroupByAggregationPlan {
     val outputSchemaSlices = FesqlUtil.getOutputSchemaSlices(node)
     val outputSchema = FesqlUtil.getSparkSchema(node.GetOutputSchema())
 
-    val groupKeyComparator = FesqlUtil.createGroupKeyComparator(groupIdxs.toArray, inputSchema)
-
-    // spark closure
+    // Wrap spark closure
     val projectConfig = ProjectConfig(
       functionName = node.project().fn_name(),
       moduleTag = ctx.getTag,
@@ -45,30 +44,28 @@ object GroupByAggregationPlan {
       outputSchemaSlices = outputSchemaSlices,
       inputSchema = inputSchema
     )
+    val groupKeyComparator = FesqlUtil.createGroupKeyComparator(groupIdxs.toArray, inputSchema)
 
-    // project implementation
+    // Map partition
     val resultRDD = sortedInputDf.rdd.mapPartitions(iter => {
-      // ensure worker native
-      val tag = projectConfig.moduleTag
-      val buffer = projectConfig.moduleBroadcast.value.getBuffer
-      JITManager.initJITModule(tag, buffer)
-      val jit = JITManager.getJIT(tag)
-
-      // reusable output row inst
-      val outputFields = projectConfig.outputSchemaSlices.map(_.size).sum
-
-      val fn = jit.FindFunction(projectConfig.functionName)
-
-      val encoder = new SparkRowCodec(projectConfig.inputSchemaSlices)
-      val decoder = new SparkRowCodec(projectConfig.outputSchemaSlices)
-
-      val inputSchema = FesqlUtil.getFeSQLSchema(projectConfig.inputSchema)
-
-      val resultList =  mutable.ArrayBuffer[Row]()
+      val resultRowList =  mutable.ArrayBuffer[Row]()
 
       if (!iter.isEmpty) { // Ignore the empty partition
-        // Create memory table and insert encoded rows
-        var groupbyInterface = new GroupbyInterface(inputSchema)
+        // Init JIT
+        val tag = projectConfig.moduleTag
+        val buffer = projectConfig.moduleBroadcast.value.getBuffer
+        JITManager.initJITModule(tag, buffer)
+        val jit = JITManager.getJIT(tag)
+        val fn = jit.FindFunction(projectConfig.functionName)
+
+        val encoder = new SparkRowCodec(projectConfig.inputSchemaSlices)
+        val decoder = new SparkRowCodec(projectConfig.outputSchemaSlices)
+
+        val inputFesqlSchema = FesqlUtil.getFeSQLSchema(projectConfig.inputSchema)
+        val outputFields = projectConfig.outputSchemaSlices.map(_.size).sum
+
+        // Init first groupby interface
+        var groupbyInterface = new GroupbyInterface(inputFesqlSchema)
         var lastRow: Row = null
         val grouopNativeRows =  mutable.ArrayBuffer[NativeRow]()
 
@@ -81,12 +78,11 @@ object GroupByAggregationPlan {
               val outputFesqlRow = CoreAPI.GroupbyProject(fn, groupbyInterface)
               val outputArr = Array.fill[Any](outputFields)(null)
               decoder.decode(outputFesqlRow, outputArr)
-              resultList += Row.fromSeq(outputArr)
+              resultRowList += Row.fromSeq(outputArr)
 
-              // Reset group
+              // Reset group interface and release native rows
               groupbyInterface.delete()
-              groupbyInterface = new GroupbyInterface(inputSchema)
-              // Release native rows
+              groupbyInterface = new GroupbyInterface(inputFesqlSchema)
               grouopNativeRows.map(nativeRow => nativeRow.delete())
               grouopNativeRows.clear()
             }
@@ -103,14 +99,14 @@ object GroupByAggregationPlan {
         val outputFesqlRow = CoreAPI.GroupbyProject(fn, groupbyInterface)
         val outputArr = Array.fill[Any](outputFields)(null)
         decoder.decode(outputFesqlRow, outputArr)
-        resultList += Row.fromSeq(outputArr)
+        resultRowList += Row.fromSeq(outputArr)
 
         groupbyInterface.delete()
         grouopNativeRows.map(nativeRow => nativeRow.delete())
         grouopNativeRows.clear()
       }
 
-      resultList.toIterator
+      resultRowList.toIterator
     })
 
     SparkInstance.fromRDD(outputSchema, resultRDD)
