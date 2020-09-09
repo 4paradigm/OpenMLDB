38a95fdde3676993e419713dee549f6908ea862a
baoxinqi <baoxinqi@4paradigm.com>
2020-09-09 10:25:14 +0800

    fix: Fix cicd artifact size

--- tools/cicd/extract_intermediate_java_cicd_artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ROOT_DIR=$(cd $(dirname $0); pwd)/../..
-SUFFIX=$1
-TAR_NAME="java_intermediate_cicd_artifact_${SUFFIX}.tar.gz"
-
-# extract into java/fesql-proto java/fesql-native
-cd ${ROOT_DIR}; tar xf ${TAR_NAME}
