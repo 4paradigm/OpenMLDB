c11be20f486ff2d76de1ee30a8fe16cdd13ef7e3
zhaowei <zw1020688@163.com>
2020-09-21 17:08:06 +0800

    返回结果的数据类型支持了int32 int16 int64 创建表支持了replicate

--- steps/fesql_auto_test_python.sh
+++ steps/fesql_auto_test_python.sh
@@ -10,6 +10,7 @@ sleep 5
 cd onebox && sh start_onebox_on_rambuild.sh && cd $ROOT_DIR
 sleep 5
 echo "ROOT_DIR:${ROOT_DIR}"
+
 #sh steps/gen_code.sh
 #sh tools/install_fesql.sh
 #mkdir -p ${ROOT_DIR}/build  && cd ${ROOT_DIR}/build && cmake .. && make -j16 python_package
@@ -28,6 +29,11 @@ cd ${ROOT_DIR}
 cd src/sdk/python/fesql-auto-test-python
 python3 -m pip install -r requirements.txt
 sed -i "s/env=.*/env=cicd/" fesql.conf
+IP=`hostname -i`
+echo "cicd_tb_endpoint_0=$IP:9520" >> fesql.conf
+echo "cicd_tb_endpoint_1=$IP:9521" >> fesql.conf
+echo "cicd_tb_endpoint_2=$IP:9522" >> fesql.conf
+
 cat fesql.conf
 rm -rf report
 mkdir report
