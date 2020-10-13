6031a3c15c6f35c1b0be2d9f795d13ec53cf8509
zhaowei <zw1020688@163.com>
2020-10-13 14:06:06 +0800

    CICD测试

--- steps/fesql_auto_gen_test.sh
+++ steps/fesql_auto_gen_test.sh
@@ -11,27 +11,37 @@ echo "BBB"
 ls -al fesql/tools/autotest/
 
 sh steps/gen_code.sh
-sh tools/install_fesql.sh
-mkdir -p ${ROOT_DIR}/build  && cd ${ROOT_DIR}/build && cmake .. 
-if [ -z "${FEDEV}" ]; then
-    make -j5 sql_javasdk_package || { echo "compile error"; exit 1; }
-else
-    make -j16 || { echo "compile error"; exit 1; }
-fi
+#sh tools/install_fesql.sh
+
+#CMAKE_TYPE=$1
+#
+#if [[ "${CMAKE_TYPE}" != "Debug" ]]; then
+#        CMAKE_TYPE="Release"
+#fi
+#echo "CMake Type "${CMAKE_TYPE}
+
+export JAVA_HOME=${RTIDB_DEV_JAVA_HOME:-/depends/thirdparty/jdk1.8.0_141}
+export RTIDB_THIRDPARTY=${RTIDB_DEV_THIRDPARTY:-/depends/thirdparty}
+WORK_DIR=`pwd`
+
+cd fesql && ln -sf ${RTIDB_THIRDPARTY} thirdparty && mkdir -p build
+cd build && cmake .. && make fesql_proto && make fesql_parser && make -j5
+
+#cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} -DCMAKE_INSTALL_PREFIX="${RTIDB_THIRDPARTY}" -DTESTING_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DPYSDK_ENABLE=OFF -DJAVASDK_ENABLE=OFF -DEXPRIRMENT_ENABLE=OFF ..  && make -j5 install
+
+
+#mkdir -p ${ROOT_DIR}/build  && cd ${ROOT_DIR}/build && cmake ..
+#if [ -z "${FEDEV}" ]; then
+#    make -j5 sql_javasdk_package || { echo "compile error"; exit 1; }
+#else
+#    make -j16 || { echo "compile error"; exit 1; }
+#fi
 cd ${ROOT_DIR}
 echo "CCC"
 ls -al fesql/build
 echo "DDDD"
 ls -al fesql/build/src/
-test -d /rambuild/ut_zookeeper && rm -rf /rambuild/ut_zookeeper/*
-cp steps/zoo.cfg thirdsrc/zookeeper-3.4.14/conf
-cd thirdsrc/zookeeper-3.4.14
-netstat -atnp | grep 6181 | awk '{print $NF}' | awk -F '/' '{print $1}'| xargs kill -9
-./bin/zkServer.sh start && cd $ROOT_DIR
-sleep 5
-cd onebox && sh start_onebox_on_rambuild.sh && cd $ROOT_DIR
-sleep 5
-case_xml=test_v1.xml
+
 cd ${ROOT_DIR}/src/sdk/java/
 mvn install -Dmaven.test.skip=true
 cd ${ROOT_DIR}/src/sdk/java/fesql-auto-test-java
