09d88cf669b9d4b77f88aeb169be3b449197b4a9
Chen Jing <chenjing@4paradigm.com>
2020-10-20 04:26:34 +0000

    Update udf.h

--- src/udf/udf.h
+++ src/udf/udf.h
@@ -256,11 +256,9 @@ void string_to(codec::StringRef *str, V *v, bool *is_null_ptr) {
     } catch (boost::bad_lexical_cast const &e) {
         *is_null_ptr = true;
         *v = V();
-       
         return;
     } catch (...) {
         *is_null_ptr = true;
-        
         *v = V();
         return;
     }
