ad443a927ceb35afc8840e11cb6301bd3c66e9e9
wangzixian <wangzixian@4paradigm.com>
2020-10-27 11:28:16 +0800

    feat:exec sqlEngine

--- java/fesql-common/src/main/java/com/_4paradigm/fesql/common/DDLEngine.java
+++ java/fesql-common/src/main/java/com/_4paradigm/fesql/common/DDLEngine.java
@@ -1,5 +1,8 @@
 package com._4paradigm.fesql.common;
+import com._4paradigm.fesql.FeSqlLibrary;
 import com._4paradigm.fesql.type.TypeOuterClass;
+import com._4paradigm.fesql.vm.Engine;
+import com._4paradigm.fesql.vm.PhysicalOpNode;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -7,32 +10,90 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class DDLEngine {
+    static {
+        // Ensure native initialized
+        FeSqlLibrary.initCore();
+        Engine.InitializeGlobalLLVM();
+    }
 
-
-    public String genDDL(String sql, String schema) {
+    /**
+     *
+     * @param sql
+     * @param schema json format
+     * @return
+     */
+    public static String genDDL(String sql, String schema) {
         String tempDB = "temp_" + System.currentTimeMillis();
-//        try {
-////            SQLEngine engine = new SQLEngine(sql, tempDB);
-//        } catch (UnsupportedFesqlException e) {
-//            e.printStackTrace();
-//        }
+        TypeOuterClass.Database.Builder db = TypeOuterClass.Database.newBuilder();
+        db.setName(tempDB);
+        for (TypeOuterClass.TableDef e : getTableDefs(schema)) {
+            db.addTables(e);
+        }
+        try {
+            SQLEngine engine = new SQLEngine(sql, db.build());
+            PhysicalOpNode plan = engine.getPlan();
+            plan.Print();
+            System.out.println("plan info");
+            System.out.println(plan.GetProducerCnt());
+        } catch (UnsupportedFesqlException e) {
+            e.printStackTrace();
+        }
         return "xx";
     }
 
-    public static TypeOuterClass.TableDef getTableSchema(String jsonObject) {
-        Gson gson = new Gson();
+    public static TypeOuterClass.Type getFesqlType(String type) {
+        if (type.equalsIgnoreCase("bigint") || type.equalsIgnoreCase("long")) {
+            return TypeOuterClass.Type.kInt64;
+        }
+        if (type.equalsIgnoreCase("smallint") || type.equalsIgnoreCase("small")) {
+            return TypeOuterClass.Type.kInt16;
+        }
+        if (type.equalsIgnoreCase("int")) {
+            return TypeOuterClass.Type.kInt32;
+        }
+        if (type.equalsIgnoreCase("float")) {
+            return TypeOuterClass.Type.kFloat;
+        }
+        if (type.equalsIgnoreCase("double")) {
+            return TypeOuterClass.Type.kDouble;
+        }
+        if (type.equalsIgnoreCase("string")) {
+            return TypeOuterClass.Type.kVarchar;
+        }
+        if (type.equalsIgnoreCase("boolean") || type.equalsIgnoreCase("bool")) {
+            return TypeOuterClass.Type.kBool;
+        }
+        if (type.equalsIgnoreCase("timestamp")) {
+            return TypeOuterClass.Type.kTimestamp;
+        }
+        if (type.equalsIgnoreCase("date")) {
+            return TypeOuterClass.Type.kDate;
+        }
+        return null;
+    }
+
+    public static List<TypeOuterClass.TableDef> getTableDefs(String jsonObject) {
+        List<TypeOuterClass.TableDef> tableDefs = new ArrayList<>();
+//        Gson gson = new Gson();
         JsonParser jsonParser = new JsonParser();
         JsonElement tableJson = jsonParser.parse(jsonObject);
-        for (JsonElement e : tableJson.getAsJsonObject().get("tableInfo").getAsJsonArray()) {
-            System.out.println(e);
+        for (Map.Entry<String, JsonElement> e : tableJson.getAsJsonObject().get("tableInfo").getAsJsonObject().entrySet()) {
+            TypeOuterClass.TableDef.Builder table = TypeOuterClass.TableDef.newBuilder();
+            table.setName(e.getKey());
+            for (JsonElement element : e.getValue().getAsJsonArray()) {
+                table.addColumns(TypeOuterClass.ColumnDef.newBuilder()
+                        .setName(element.getAsJsonObject().get("name").getAsString())
+                        .setIsNotNull(false)
+                        .setType(getFesqlType(element.getAsJsonObject().get("type").getAsString())));
+            }
+            tableDefs.add(table.build());
         }
-
-        TypeOuterClass.TableDef.Builder table = TypeOuterClass.TableDef.newBuilder();
-        table.addColumns(TypeOuterClass.ColumnDef.newBuilder().setName("string").build());
-        return table.build();
-
+        return tableDefs;
     }
 
 
@@ -40,8 +101,10 @@ public class DDLEngine {
         String schemaPath = "/home/wangzixian/ferrari/idea/docker-code/fesql/java/fesql-common/src/test/resources/ddl/homecredit.json";
         String sqlPath = "/home/wangzixian/ferrari/idea/docker-code/fesql/java/fesql-common/src/test/resources/ddl/homecredit.txt";
         File file = new File(schemaPath);
+        File sql = new File(sqlPath);
         try {
-            getTableSchema(FileUtils.readFileToString(file, "UTF-8"));
+            genDDL(FileUtils.readFileToString(sql, "UTF-8"), FileUtils.readFileToString(file, "UTF-8"));
+//            getTableDefs(FileUtils.readFileToString(file, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
