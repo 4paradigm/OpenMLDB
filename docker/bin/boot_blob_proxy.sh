0e53575344676cb69c73084728aa988ac5dea91f
kongquan <kongquan@4paradigm.com>
2020-11-09 02:29:14 -0500

    feat: add zk conf change detect

--- docker/bin/boot_blob_proxy.sh
+++ docker/bin/boot_blob_proxy.sh
@@ -7,8 +7,24 @@ export zk_cluster=${zkservers}
 export zk_root_path=${zkpath}
 export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
 export blobproxy_port
+
+function detect_zk_change() {
+  while :
+  do
+    sleep 30
+    getzk
+    if [[ ${zk_cluster} != ${zkservers} ]] || [[ ${zk_root_path} != ${zkpath} ]]; then
+      ps -ef | grep ${Endpoint} | grep -v grep | awk '{print "kill "$1}' | bash
+      break
+    fi
+  done
+}
+
 Endpoint="$(hostname -i):${blobproxy_port}"
 SELFDIR=$(dirname $(readlink -f $0))
 WORKDIR=$(dirname ${SELFDIR})
 cd ${WORKDIR}
+tmpfile=$(mktemp)
+[ -n "${mimedb}" ] && { echo ${mimedb} | base64 -d >> ${tmpfile} && cat ${tmpfile} >> conf/mime.conf; }
+detect_zk_change &
 ./bin/rtidb --flagfile=./conf/blob_proxy.flags --endpoint=$Endpoint --zk_cluster=$zk_cluster --zk_root_path=$zk_root_path --enable_status_service=true
