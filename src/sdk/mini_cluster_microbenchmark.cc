6b6d01ad782bfc9f318d10bedc1e18891dfd8232
Chenjing <chenjing@4paradigm.com>
2020-11-17 19:43:26 -0800

    ttl

--- src/sdk/mini_cluster_microbenchmark.cc
+++ src/sdk/mini_cluster_microbenchmark.cc
@@ -283,7 +283,7 @@ static void BM_SimpleRowWindow(benchmark::State& state) {  // NOLINT
     ::fesql::sdk::Status status;
     router->CreateDB(db, &status);
     std::string create =
-        "create table " + name + "(id int, c1 string, c6 double, c7 timestamp, index(key=(c1), ts=c7, ttl=30d)) partitionnum=8;";
+        "create table " + name + "(id int, c1 string, c6 double, c7 timestamp, index(key=(c1), ts=c7, ttl=3650d)) partitionnum=8;";
     router->ExecuteDDL(db, create, &status);
     if (status.msg != "ok") {
         std::cout << "fail to create table" << std::endl;
