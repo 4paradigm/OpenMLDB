0fc8cfa2dcf2a4854905f4fe4a04a4057b244c7f
zhaowei <zw1020688@163.com>
2020-12-28 17:42:08 +0800

    增加了一个注释

--- tools/autotest/gen_case_yaml_main.py
+++ tools/autotest/gen_case_yaml_main.py
@@ -4,6 +4,9 @@ import sys
 from auto_cases import gen_case_yaml
 
 if __name__ == "__main__":
+    '''
+    生成yaml的入口
+    '''
     currentPath = os.getcwd()
     index = currentPath.rfind('fesql')
     prePath = currentPath[0:index]
