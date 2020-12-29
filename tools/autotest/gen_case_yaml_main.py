94d5327dcc7cdd67cdbf5ae26870fea956e151f8
zhaowei <zw1020688@163.com>
2020-12-29 12:51:22 +0800

    修改了gen_case_yaml_main.py

--- tools/autotest/gen_case_yaml_main.py
+++ tools/autotest/gen_case_yaml_main.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     生成yaml的入口
     '''
     currentPath = os.getcwd()
-    print("currentPath:"+currentPath)
     index = currentPath.rfind('fesql')
-    print("index:"+index)
-    prePath = currentPath[0:index]
+    if index == -1:
+        prePath = currentPath+"/"
+    else:
+        prePath = currentPath[0:index]
     print("prePath:"+prePath)
     casePath = prePath+"fesql/cases/auto_gen_cases"
     print("casePath:"+casePath)
