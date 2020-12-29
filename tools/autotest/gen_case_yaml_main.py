ca5c534a8387f10266c6efac8c554592b1018d46
zhaowei <zw1020688@163.com>
2020-12-29 12:41:44 +0800

    增加了udf_defs.yaml

--- tools/autotest/gen_case_yaml_main.py
+++ tools/autotest/gen_case_yaml_main.py
@@ -8,8 +8,11 @@ if __name__ == "__main__":
     生成yaml的入口
     '''
     currentPath = os.getcwd()
+    print("currentPath:"+currentPath)
     index = currentPath.rfind('fesql')
+    print("index:"+index)
     prePath = currentPath[0:index]
+    print("prePath:"+prePath)
     casePath = prePath+"fesql/cases/auto_gen_cases"
     print("casePath:"+casePath)
     gen_case_yaml(casePath)
