1b399fdc1e5d6e4149e6145574fc0439cec05c0e
Chenjing <chenjing@4paradigm.com>
2021-01-04 18:17:48 -0800

    FailSQLCase when dataprovider load yaml fail

--- src/sdk/java/fesql-auto-test-java/src/main/java/com/_4paradigm/fesql_auto_test/entity/FesqlDataProvider.java
+++ src/sdk/java/fesql-auto-test-java/src/main/java/com/_4paradigm/fesql_auto_test/entity/FesqlDataProvider.java
@@ -2,18 +2,14 @@ package com._4paradigm.fesql_auto_test.entity;
 
 import com._4paradigm.fesql.sqlcase.model.CaseFile;
 import com._4paradigm.fesql.sqlcase.model.SQLCase;
-import com._4paradigm.fesql_auto_test.util.Tool;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
+import org.testng.collections.Lists;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author zhaowei
@@ -22,16 +18,22 @@ import java.util.List;
 @Data
 public class FesqlDataProvider extends CaseFile {
     private static Logger logger = LoggerFactory.getLogger(FesqlDataProvider.class);
+    public static final String FAIL_SQL_CASE= "FailSQLCase";
 
     public static FesqlDataProvider dataProviderGenerator(String caseFile) throws FileNotFoundException {
-        Yaml yaml = new Yaml();
-//        String rtidbDir = Tool.rtidbDir().getAbsolutePath();
-//        Assert.assertNotNull(rtidbDir);
-//        String caseAbsPath = rtidbDir + "/fesql/cases/" + caseFile;
-//        logger.debug("fesql case absolute path: {}", caseAbsPath);
-        FileInputStream testDataStream = new FileInputStream(caseFile);
-        FesqlDataProvider testDateProvider = yaml.loadAs(testDataStream, FesqlDataProvider.class);
-        return testDateProvider;
+        try {
+            Yaml yaml = new Yaml();
+            FileInputStream testDataStream = new FileInputStream(caseFile);
+            FesqlDataProvider testDateProvider = yaml.loadAs(testDataStream, FesqlDataProvider.class);
+            return testDateProvider;
+        } catch (Exception e) {
+            logger.error("fail to load yaml: ", e);
+            FesqlDataProvider nullDataProvider = new FesqlDataProvider();
+            SQLCase failCase = new SQLCase();
+            failCase.setDesc(FAIL_SQL_CASE);
+            nullDataProvider.setCases(Lists.newArrayList(failCase));
+            return nullDataProvider;
+        }
     }
 
 
