5c5dd3b9b54d0285e2ad6fa036bb1bad1bf1ec09
zhaowei <zw1020688@163.com>
2021-01-05 15:06:37 +0800

    测试 cicd

---
+++ src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/FunctionTest.java
@@ -0,0 +1,45 @@
+package com._4paradigm.fesql_auto_test.v1;
+
+import com._4paradigm.fesql.sqlcase.model.SQLCase;
+import com._4paradigm.fesql_auto_test.common.FesqlTest;
+import com._4paradigm.fesql_auto_test.entity.FesqlDataProviderList;
+import com._4paradigm.fesql_auto_test.executor.ExecutorFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author zhaowei
+ * @date 2020/6/11 2:53 PM
+ */
+@Slf4j
+public class FunctionTest extends FesqlTest {
+
+    @DataProvider
+    public Object[] testExpressCase() throws FileNotFoundException {
+        FesqlDataProviderList dp = FesqlDataProviderList.dataProviderGenerator(
+                new String[]{
+                        "/integration/v1/function/"
+                });
+        return dp.getCases().toArray();
+    }
+
+    @Test(dataProvider = "testExpressCase")
+    public void testArithmetic(SQLCase testCase) throws Exception {
+        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kBatch).run();
+    }
+    @Test(dataProvider = "testSelectCase")
+    public void testSelectRequestMode(SQLCase testCase) throws Exception {
+        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequest).run();
+    }
+    @Test(dataProvider = "testSelectCase")
+    public void testSelectRequestModeWithSp(SQLCase testCase) throws Exception {
+        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSp).run();
+    }
+    @Test(dataProvider = "testSelectCase")
+    public void testSelectRequestModeWithSpAysn(SQLCase testCase) throws Exception {
+        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSpAsync).run();
+    }
+}
