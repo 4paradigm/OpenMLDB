6a783125271a7986d0c8a796f1fc543e3efc7638
imotai <wangtaize@4paradigm.com>
2021-01-07 19:06:57 +0800

    fix:add debug

--- tools/install_fesql.sh
+++ tools/install_fesql.sh
@@ -15,5 +15,3 @@ WORK_DIR=`pwd`
 cd fesql && ln -sf ${RTIDB_THIRDPARTY} thirdparty && mkdir -p build
 cd build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} -DCMAKE_INSTALL_PREFIX="${RTIDB_THIRDPARTY}" -DTESTING_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DPYSDK_ENABLE=OFF -DJAVASDK_ENABLE=ON -DEXPRIRMENT_ENABLE=OFF ..  && make -j5 install
 
-cd ${WORK_DIR}/fesql/java/fesql-native && mvn install
-cd ${WORK_DIR}/fesql/java/fesql-common && mvn install
