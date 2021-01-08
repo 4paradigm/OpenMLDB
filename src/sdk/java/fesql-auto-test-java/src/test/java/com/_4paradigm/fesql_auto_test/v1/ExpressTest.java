64be5ec3584bb726ba5bba9b7f02561c0d8e6a8f
zhaowei <zw1020688@163.com>
2021-01-08 14:57:12 +0800

    测试 cicd

--- src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/ExpressTest.java
+++ src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/ExpressTest.java
@@ -26,18 +26,18 @@ public class ExpressTest extends FesqlTest {
         return dp.getCases().toArray();
     }
 
-    @Test(dataProvider = "testExpressCase")
-    public void testExpress(SQLCase testCase) throws Exception {
-        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kBatch).run();
-    }
-    @Test(dataProvider = "testExpressCase")
-    public void testExpressRequestMode(SQLCase testCase) throws Exception {
-        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequest).run();
-    }
-    @Test(dataProvider = "testExpressCase")
-    public void testExpressRequestModeWithSp(SQLCase testCase) throws Exception {
-        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSp).run();
-    }
+//    @Test(dataProvider = "testExpressCase")
+//    public void testExpress(SQLCase testCase) throws Exception {
+//        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kBatch).run();
+//    }
+//    @Test(dataProvider = "testExpressCase")
+//    public void testExpressRequestMode(SQLCase testCase) throws Exception {
+//        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequest).run();
+//    }
+//    @Test(dataProvider = "testExpressCase")
+//    public void testExpressRequestModeWithSp(SQLCase testCase) throws Exception {
+//        ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSp).run();
+//    }
     @Test(dataProvider = "testExpressCase")
     public void testExpressRequestModeWithSpAysn(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSpAsync).run();
