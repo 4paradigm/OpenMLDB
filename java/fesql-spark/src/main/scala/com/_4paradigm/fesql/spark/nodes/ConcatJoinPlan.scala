5008f22ea83c0d242829a16709dd910b3614fffa
tobe <tobeg3oogle@gmail.com>
2021-01-22 17:59:24 +0800

    Keep the only one index column for concat cast join plan

--- java/fesql-spark/src/main/scala/com/_4paradigm/fesql/spark/nodes/ConcatJoinPlan.scala
+++ java/fesql-spark/src/main/scala/com/_4paradigm/fesql/spark/nodes/ConcatJoinPlan.scala
@@ -3,7 +3,7 @@ package com._4paradigm.fesql.spark.nodes
 import com._4paradigm.fesql.common.FesqlException
 import com._4paradigm.fesql.node.JoinType
 import com._4paradigm.fesql.spark._
-import com._4paradigm.fesql.spark.utils.{SparkColumnUtil, SparkUtil}
+import com._4paradigm.fesql.spark.utils.{NodeIndexType, SparkColumnUtil, SparkUtil}
 import com._4paradigm.fesql.vm.PhysicalJoinNode
 import org.apache.spark.sql.DataFrame
 import org.slf4j.LoggerFactory
@@ -20,27 +20,38 @@ object ConcatJoinPlan {
       throw new FesqlException(s"Concat join type $joinType not supported")
     }
 
-    // TODO: Get index column name from child node instead of current node's index info
-    val indexName = ctx.getIndexInfo(node.GetProducer(0).GetNodeId()).indexColumnName
+    val indexName = ctx.getIndexInfo(node.GetNodeId()).indexColumnName
+    val newRightTableIndexName = "New_" + indexName
 
     // Note that this is exception to use "getDfWithIndex" instead of "getSparkDfConsideringIndex" because ConcatJoin has not index flag but request input dataframe with index
     val leftDf: DataFrame = left.getDfWithIndex
-    val rightDf: DataFrame = right.getDfWithIndex
+    val rightDf: DataFrame = right.getDfWithIndex.withColumnRenamed(indexName, newRightTableIndexName)
+
+    val nodeIndexType = ctx.getIndexInfo(node.GetNodeId()).nodeIndexType
 
     // Check if we can use native last join
     val supportNativeLastJoin = SparkUtil.supportNativeLastJoin(joinType, false)
 
-    // Use the last column to join which are always index column in concat join node
     // Use left join or native last join
     val resultDf = if (supportNativeLastJoin) {
-      leftDf.join(rightDf, SparkColumnUtil.getColumnFromIndex(leftDf, -1) === SparkColumnUtil.getColumnFromIndex(rightDf, -1), "last")
+      leftDf.join(rightDf, leftDf(indexName) === rightDf(newRightTableIndexName), "last")
     } else {
-      leftDf.join(rightDf, SparkColumnUtil.getColumnFromIndex(leftDf, leftDf.schema.size-1) === SparkColumnUtil.getColumnFromIndex(rightDf, rightDf.schema.size-1), "left")
+      leftDf.join(rightDf, leftDf(indexName) === rightDf(newRightTableIndexName), "left")
     }
 
     // Drop the index column, this will drop two columns with the same index name
-    logger.info("Drop the index column %s for output dataframe".format(indexName))
-    val outputDf = resultDf.drop(indexName)
+    val outputDf = nodeIndexType match {
+      case NodeIndexType.SourceConcatJoinNode => {
+        logger.info("Drop the index column %s and %s for output dataframe".format(indexName, newRightTableIndexName))
+        resultDf.drop(indexName).drop(newRightTableIndexName)
+      }
+      case NodeIndexType.InternalConcatJoinNode => {
+        logger.info("Drop the index column %s for output dataframe".format(indexName))
+        resultDf.drop(newRightTableIndexName)
+      }
+      case _ => throw new FesqlException("Handle unsupported concat join node index type: %s".format(nodeIndexType))
+    }
+
 
     SparkInstance.createConsideringIndex(ctx, node.GetNodeId(), outputDf)
   }
