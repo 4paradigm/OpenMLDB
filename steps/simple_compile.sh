b93c4202a6cc1058871aa2357f2679d8d7f5f749
wangbao <wangbao@4paradigm.com>
2021-01-26 09:16:26 +0000

    fix: rm redundant modify

--- steps/simple_compile.sh
+++ steps/simple_compile.sh
@@ -9,7 +9,7 @@ WORK_DIR=`pwd`
 
 sh steps/gen_code.sh
 mkdir -p $WORK_DIR/build
-cd $WORK_DIR/build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} .. && make -j36 rtidb sql_pysdk_package sql_jsdk parse_log sqlalchemy_fedb
+cd $WORK_DIR/build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} .. && make -j16 rtidb sql_pysdk_package sql_jsdk parse_log sqlalchemy_fedb
 code=$?
 cd $WORK_DIR
 exit $code
