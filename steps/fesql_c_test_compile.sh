6c9e30073fb19c8822da05d0d0392ab8163aede9
Chenjing <chenjing@4paradigm.com>
2021-01-27 20:57:12 -0800

    compile fix

--- steps/fesql_c_test_compile.sh
+++ steps/fesql_c_test_compile.sh
@@ -9,7 +9,7 @@ WORK_DIR=`pwd`
 
 sh steps/gen_code.sh
 mkdir -p $WORK_DIR/build
-cd $WORK_DIR/build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} .. && make -j16 rtidb sql_sdk_test sql_cluster_test tabelt_engine_test
+cd $WORK_DIR/build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} .. && make -j16 rtidb sql_sdk_test sql_cluster_test tablet_engine_test
 code=$?
 cd $WORK_DIR
 exit $code
