6811b57a1a6d22c7f229beb8255cd78cb6a833de
Yang Jun01 <yangjun01@4paradigm.com>
2021-02-02 17:05:56 +0800

    upgrade pz-titanse and rocksdb-titanse

--- steps/fpga_compile.sh
+++ steps/fpga_compile.sh
@@ -20,10 +20,10 @@ if [ -n "${VERSION}" ]; then
     fi
     sh ./steps/release.sh ${VERSION}
 fi
-curl ftp://ftp.4pd.io/pub/team_hpc/titanse/pz-titanse-v0.1.0-release.tar.gz -o pz.tar.gz
+curl ftp://ftp.4pd.io/pub/team_hpc/titanse/pz-titanse-v0.1.1-release.tar.gz -o pz.tar.gz
 tar zxf pz.tar.gz -C ./thirdparty
 
-curl ftp://ftp.4pd.io/pub/team_hpc/titanse/rocksdb-titanse-v0.0.1-release.tar.gz -o rocksdb.tar.gz
+curl ftp://ftp.4pd.io/pub/team_hpc/titanse/rocksdb-titanse-v0.1.1-release.tar.gz -o rocksdb.tar.gz
 tar zxf rocksdb.tar.gz -C ./thirdparty
 curl "https://nexus.4pd.io/repository/raw-hosted/fpga-rte/release/v0.2.1/aclrte-linux64-aclrte-19.2.0.57-v0.2.1-release.tar.gz" -O
 tar zxf aclrte-linux64-aclrte-19.2.0.57-v0.2.1-release.tar.gz -C ./thirdparty
