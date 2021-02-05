76a10eb8a7f3c0a77d18799ce1bf13a5233f6b73
Chenjing <chenjing@4paradigm.com>
2021-02-04 17:59:35 -0800

    c++ fesql update

--- steps/fesql_c_test_compile.sh
+++ steps/fesql_c_test_compile.sh
@@ -7,13 +7,10 @@ if [[ "${CMAKE_TYPE}" != "Debug" ]]; then
 fi
 WORK_DIR=`pwd`
 
-sh steps/gen_code.sh
 mkdir -p $WORK_DIR/build
-if [ -z "${FEDEV}" ]; then
-  cd $WORK_DIR/build && make -j16 sql_sdk_test sql_cluster_test tablet_engine_test
-  code=$?
-  cd $WORK_DIR
-else
+if [ ! -f "sql_sdk_test" ]; then
+  echo "sql_sdk_test not exist, make rtidb and sql_sdk_test..."
+  sh steps/gen_code.sh
   cd $WORK_DIR/build && cmake -DCMAKE_BUILD_TYPE=${CMAKE_TYPE} .. && make -j16 rtidb sql_sdk_test sql_cluster_test tablet_engine_test
   code=$?
   cd $WORK_DIR
