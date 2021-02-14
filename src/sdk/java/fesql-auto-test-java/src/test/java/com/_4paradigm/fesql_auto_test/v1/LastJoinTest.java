dd3ecb9c5219cefaf041e4cbbbdcef82522e3820
zhaowei <zw1020688@163.com>
2021-02-14 08:19:06 +0800

    测试case

--- src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/LastJoinTest.java
+++ src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/LastJoinTest.java
@@ -2,9 +2,10 @@ package com._4paradigm.fesql_auto_test.v1;
 
 import com._4paradigm.fesql.sqlcase.model.SQLCase;
 import com._4paradigm.fesql_auto_test.common.FesqlTest;
-import com._4paradigm.fesql_auto_test.entity.FesqlDataProvider;
 import com._4paradigm.fesql_auto_test.entity.FesqlDataProviderList;
 import com._4paradigm.fesql_auto_test.executor.ExecutorFactory;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -16,30 +17,34 @@ import java.io.FileNotFoundException;
  * @date 2020/6/11 2:53 PM
  */
 @Slf4j
+@Feature("lastjoin")
 public class LastJoinTest extends FesqlTest {
 
     @DataProvider
     public Object[] testLastJoinData() throws FileNotFoundException {
         FesqlDataProviderList dp = FesqlDataProviderList
                 .dataProviderGenerator(new String[]{
-                        "/integration/v1/join/",
-                       "/integration/cluster/window_and_lastjoin.yaml"
+                        "/integration/v1/join/test_last_join.yaml",
+                       // "/integration/cluster/window_and_lastjoin.yaml"
                 });
         return dp.getCases().toArray();
     }
-
+    @Story("batch")
     @Test(dataProvider = "testLastJoinData")
     public void testLastJoin(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor,testCase, ExecutorFactory.ExecutorType.kBatch).run();
     }
+    @Story("request")
     @Test(dataProvider = "testLastJoinData")
     public void testLastJoinRequestMode(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor,testCase, ExecutorFactory.ExecutorType.kRequest).run();
     }
+    @Story("requestWithSp")
     @Test(dataProvider = "testLastJoinData")
     public void testLastJoinRequestModeWithSp(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor,testCase, ExecutorFactory.ExecutorType.kRequestWithSp).run();
     }
+    @Story("requestWithSpAysn")
     @Test(dataProvider = "testLastJoinData")
     public void testLastJoinRequestModeWithSpAsync(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor,testCase, ExecutorFactory.ExecutorType.kRequestWithSpAsync).run();
--- src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/SelectTest.java
+++ src/sdk/java/fesql-auto-test-java/src/test/java/com/_4paradigm/fesql_auto_test/v1/SelectTest.java
@@ -4,6 +4,9 @@ import com._4paradigm.fesql.sqlcase.model.SQLCase;
 import com._4paradigm.fesql_auto_test.common.FesqlTest;
 import com._4paradigm.fesql_auto_test.entity.FesqlDataProviderList;
 import com._4paradigm.fesql_auto_test.executor.ExecutorFactory;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -15,30 +18,35 @@ import java.io.FileNotFoundException;
  * @date 2020/6/11 2:53 PM
  */
 @Slf4j
+@Feature("SelectTest")
 public class SelectTest extends FesqlTest {
 
     @DataProvider
     public Object[] testSelectCase() throws FileNotFoundException {
         FesqlDataProviderList dp = FesqlDataProviderList.dataProviderGenerator(
                 new String[]{
-                        "/query/const_query.yaml",
-                        "/integration/v1/select/"
+                        // "/query/const_query.yaml",
+                        "/integration/v1/select/test_select_sample.yaml"
                 });
         return dp.getCases().toArray();
     }
-
+    @Story("batch")
     @Test(dataProvider = "testSelectCase")
+    @Step("{testCase.desc}")
     public void testSelect(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kBatch).run();
     }
+    @Story("request")
     @Test(dataProvider = "testSelectCase")
     public void testSelectRequestMode(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequest).run();
     }
+    @Story("requestWithSp")
     @Test(dataProvider = "testSelectCase")
     public void testSelectRequestModeWithSp(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSp).run();
     }
+    @Story("requestWithSpAysn")
     @Test(dataProvider = "testSelectCase")
     public void testSelectRequestModeWithSpAysn(SQLCase testCase) throws Exception {
         ExecutorFactory.build(executor, testCase, ExecutorFactory.ExecutorType.kRequestWithSpAsync).run();
