69c3d5bcf5dd7c4bff0e102568abcc30372e0d90
wangbao <wangbao@4paradigm.com>
2021-02-26 10:32:02 +0000

    feat: rm rtidb java client and python client

--- java/src/main/java/com/_4paradigm/rtidb/client/Version.java
+++
@@ -1,9 +0,0 @@
-package com._4paradigm.rtidb.client;
-
-public class Version {
-    public static int MAJOR = 1;
-    public static int MEDIUM = 6;
-    public static int MINOR = 2;
-    public static int BUG = 0;
-    public static String VERSION = String.valueOf(MAJOR) + "." + String.valueOf(MEDIUM) + "." + String.valueOf(MINOR) + "." + String.valueOf(BUG);
-}
--- java/src/main/java/rtidb/blobserver/BlobServer.java
+++
@@ -1,9 +0,0 @@
-package rtidb.blobserver;
-
-import com._4paradigm.rtidb.blobserver.OSS;
-
-public interface BlobServer {
-    OSS.PutResponse put(OSS.PutRequest request);
-    OSS.GetResponse get(OSS.GetRequest reuqest);
-    OSS.DeleteResponse delete(OSS.DeleteRequest request);
-}
