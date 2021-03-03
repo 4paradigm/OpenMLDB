1d1e263cdd76f195e1415cd041e7ca1a6c615b80
$User <qiliguo@4paradigm.com>
2021-03-03 09:16:51 +0000

    update

--- src/base/fe_linenoise.cc
+++ src/base/fe_linenoise.cc
@@ -1,20 +1,3 @@
-/*
- * fe_linenoise.cc
- * Copyright 2021 4Paradigm
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /* linenoise.c -- guerrilla line editing library against the idea that a
  * Copyright (c) 2010-2016, Salvatore Sanfilippo <antirez at gmail dot com>
  * Copyright (c) 2010-2013, Pieter Noordhuis <pcnoordhuis at gmail dot com>
