374cfb5a7478b01c1d20336fed928a824dd74614
$User <qiliguo@4paradigm.com>
2021-03-03 03:48:24 +0000

    批量license 添加

--- benchmark/offline/multi_column_sum/create_data.sh
+++ benchmark/offline/multi_column_sum/create_data.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# benchmark/offline/multi_column_sum/create_data.sh
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 YARN_JARS="hdfs:///spark_benchmark/dependency/spark230_jars/*.jar"
 
 OUTPUT_PATH="hdfs:///baoxinqi/test_multi_column_sum/data"
--- tools/init_env.profile.sh
+++ tools/init_env.profile.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 echo "CICD environment tag: ${CICD_RUNNER_TAG}"
+# tools/init_env.profile.sh
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 echo "Third party packages path: ${CICD_RUNNER_THIRDPARTY_PATH}"
 if [[ "$OSTYPE" == "linux-gnu"* ]]
 then
