71817bc324b59cb864d4480dec31e4ef8b90f3f3
denglong <denglong@4paradigm.com>
2021-03-10 16:33:57 +0800

    feat: add license to src file

--- src/catalog/sdk_catalog.cc
+++ src/catalog/sdk_catalog.cc
@@ -1,12 +1,11 @@
 /*
- * sdk_catalog.cc
- * Copyright (C) 4paradigm.com 2020 wangtaize <wangtaize@4paradigm.com>
+ * Copyright 2021 4Paradigm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 #include "catalog/sdk_catalog.h"
 
