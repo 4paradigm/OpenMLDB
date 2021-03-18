a36d37d537e89c358c9f6bdf9d8c80f2a373aa5a
aceforeverd <teapot@aceforeverd.com>
2021-03-18 16:15:18 +0800

    hello

--- tools/init_env.profile.sh
+++ tools/init_env.profile.sh
@@ -21,10 +21,7 @@ echo "Third party packages path: ${CICD_RUNNER_THIRDPARTY_PATH}"
 if [[ "$OSTYPE" == "linux-gnu"* ]]
 then
     ln -sf /depends/thirdparty thirdparty
-    source /opt/rh/python27/enable
-    source /opt/rh/devtoolset-7/enable
-    export JAVA_HOME=${PWD}/thirdparty/jdk1.8.0_141
-    export PATH=${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH
+    source /etc/bashrc
 else
     source ~/.bash_profile
     ln -sf ${CICD_RUNNER_THIRDPARTY_PATH} thirdparty
