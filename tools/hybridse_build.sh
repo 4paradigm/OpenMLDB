1d759176262df1bf06610a9ed1fd4b11d5633894
Chenjing <chenjing@4paradigm.com>
2021-03-22 14:29:03 -0700

    Fix bug in hybridse build script

--- tools/hybridse_build.sh
+++ tools/hybridse_build.sh
@@ -28,7 +28,7 @@ fi
 
 HYRBIDSE_VERSION="release-0.1.0"
 rm -rf build
-mkdir -p build && cd build$
+mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="hybridse" -DBENCHMARK_ENABLE=OFF -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=OFF -DPYSDK_ENABLE=OFF -DEXAMPLES_ENABLE=OFF
 make -j"$(nproc)" hybridse_proto && make -j"$(nproc)" hybridse_parser
 make -j"$(nproc)"
