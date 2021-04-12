c24e31a55ea4e0edbad7b341fe0b833a9d0c5d8f
aceforeverd <teapot@aceforeverd.com>
2021-04-12 12:18:09 +0800

    feat: add hybridse_deploy.sh

--- tools/hybridse_deploy.sh
+++ tools/hybridse_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2021 4Paradigm
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,20 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# hybridse_deploy.sh create compiled archive from make install
+
 set -eE
 
 # goto toplevel directory
-cd "$(dirname "$0")/.."
+pushd "$(dirname "$0")/.."
 
-source tools/init_env.profile.sh
-
+if [[ -n $VERSION ]]; then
+    HYBRIDSE_VERSION=$VERSION
+else
+    HYBRIDSE_VERSION="SNAPSHOT-$(date +%Y-%m-%d)"
+fi
 
 if uname -a | grep -q Darwin; then
     # in case coreutils not install on mac
     alias nproc='sysctl -n hw.logicalcpu'
 fi
 
-rm -rf build
-mkdir -p build && cd build
+mkdir -p build
+pushd build/
+
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="hybridse"
-make -j"$(nproc)"
+make -j "$(nproc)" install
+mv hybridse "hybridse-$HYBRIDSE_VERSION"
+tar czf ../hybridse-"$HYBRIDSE_VERSION.tar.gz" "hybridse-$HYBRIDSE_VERSION"
+
+popd
+
+popd
