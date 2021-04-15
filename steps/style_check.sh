b45b2b988f386343cc8dd1dc593d57389e9e4ac9
deng <deng@dengdeMacBook-Pro.local>
2021-04-15 16:04:22 +0800

    fix: use new hybridse interface

--- steps/style_check.sh
+++ steps/style_check.sh
@@ -17,4 +17,4 @@
 # style_check.sh
 
 mkdir -p ./reports
-python3 tools/cpplint.py --linelength=120 --output=junit --root=. --exclude='src/proto/*' --exclude='src/storage/beans/*' --exclude='src/version.h' --exclude='src/config.h' --exclude='src/sdk/java/*' --recursive src/* 2> ./reports/style.xml
+python3 steps/cpplint.py --linelength=120 --output=junit --root=. --exclude='src/proto/*' --exclude='src/storage/beans/*' --exclude='src/version.h' --exclude='src/config.h' --exclude='src/sdk/java/*' --recursive src/* 2> ./reports/style.xml
