c1aba41de7a8c928068248508db707c16ed866c8
deng <deng@dengdeMacBook-Pro.local>
2021-04-16 11:02:38 +0800

    fix: update curl parameter

--- steps/init_env.sh
+++ steps/init_env.sh
@@ -21,7 +21,7 @@ cd /depends && tar -zxf thirdparty.tar.gz
 rm -rf thirdparty/hybridse
 mkdir -p thirdparty/hybridse
 PACKAGE_NAME=hybridse-0.1.1-linux-x86_64
-curl -O  https://github.com/4paradigm/HybridSE/releases/download/v0.1.1/${PACKAGE_NAME}.tar.gz
+curl -LJO  https://github.com/4paradigm/HybridSE/releases/download/v0.1.1/${PACKAGE_NAME}.tar.gz
 tar zxf ${PACKAGE_NAME}.tar.gz > /dev/null
 mv ${PACKAGE_NAME}/* thirdparty/hybridse
 
