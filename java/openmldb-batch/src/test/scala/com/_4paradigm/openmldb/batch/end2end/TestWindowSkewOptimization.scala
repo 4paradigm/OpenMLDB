15f8df6e62b14357797e206e42f3b8969c16c3e8
tobe <tobeg3oogle@gmail.com>
2021-06-23 14:45:50 +0800

    Upgrade version and dependency to 0.1.14

--- java/openmldb-batch/src/test/scala/com/_4paradigm/openmldb/batch/end2end/TestWindowSkewOptimization.scala
+++ java/openmldb-batch/src/test/scala/com/_4paradigm/openmldb/batch/end2end/TestWindowSkewOptimization.scala
@@ -57,7 +57,7 @@ class TestWindowSkewOptimization extends FunSuite {
     val outputDf = sess.sql(sqlText)
 
     val sparksqlOutputDf = sess.sparksql(sqlText)
-    // Notice that the sum column type is different for SparkSQL and SparkFE
+    // Notice that the sum column type is different for SparkSQL
     assert(SparkUtil.approximateDfEqual(outputDf.getSparkDf(), sparksqlOutputDf, false))
   }
 
