dca89c79f8633cfb9effda9ea1637547489129b7
Chen22 <jingchen2222@gmail.com>
2021-06-24 10:08:27 -0700

    fix: zetasql package name

--- steps/init_env.sh
+++ steps/init_env.sh
@@ -25,7 +25,7 @@ rm -rf thirdparty/hybridse
 mkdir -p thirdparty/hybridse
 #PACKAGE_NAME=hybridse-0.1.1-linux-x86_64
 #curl -LO  https://github.com/4paradigm/HybridSE/releases/download/v0.1.1/${PACKAGE_NAME}.tar.gz
-PACKAGE_NAME=hybridse-0.1.5-pre-1-linux-x86_64.tar.gz
+PACKAGE_NAME=hybridse-0.1.5-pre-1-linux-x86_64
 curl -LO https://github.com/jingchen2222/HybridSE/releases/download/v0.1.5-pre-1/${PACKAGE_NAME}.tar.gz
 tar zxf ${PACKAGE_NAME}.tar.gz > /dev/null
 mv ${PACKAGE_NAME}/* thirdparty/hybridse
