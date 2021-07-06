2ff2bbd733842e16a4b43d6ab4f101c037e38815
dl239 <dl239@126.com>
2021-07-06 14:17:25 +0800

    test: add sql_cluster_test

--- src/sdk/sql_sdk_test.cc
+++ src/sdk/sql_sdk_test.cc
@@ -49,9 +49,7 @@ static std::shared_ptr<SQLRouter> GetNewSQLRouter() {
 }
 
 static bool IsSupportMode(const std::string& mode) {
-    if (mode.find("rtidb-unsupport") != std::string::npos ||
-            mode.find("standalone-unsupport") != std::string::npos ||
-            mode.find("request-unsupport") != std::string::npos) {
+    if (mode.find("rtidb-unsupport") != std::string::npos || mode.find("request-unsupport") != std::string::npos) {
         return false;
     }
     return true;
