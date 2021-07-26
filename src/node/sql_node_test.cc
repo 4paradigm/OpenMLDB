c2797bf45073233127bf754a772f7b4e113b8a2d
Chen22 <jingchen2222@gmail.com>
2021-07-25 22:25:19 -0700

    feat: add unit test

--- src/node/sql_node_test.cc
+++ src/node/sql_node_test.cc
@@ -923,8 +923,9 @@ TEST_F(SqlNodeTest, QueryTypeNameTest) {
 TEST_F(SqlNodeTest, OrderByNodeTest) {
     // expr list
     ExprListNode *expr_list1 = node_manager_->MakeExprList();
-    expr_list1->AddChild(node_manager_->MakeOrderExpression(node_manager_->MakeColumnRefNode("col1", ""),
-                                                            true));
+    OrderExpression* order_expression = node_manager_->MakeOrderExpression(node_manager_->MakeColumnRefNode("col1", ""),
+                                                                           true);
+    expr_list1->AddChild(order_expression);
     expr_list1->AddChild(node_manager_->MakeOrderExpression(node_manager_->MakeColumnRefNode("col2", ""), true));
 
     ExprListNode *expr_list2 = node_manager_->MakeExprList();
@@ -957,6 +958,45 @@ TEST_F(SqlNodeTest, OrderByNodeTest) {
     ASSERT_EQ("(col1 ASC, col2 ASC)", order2->GetExprString());
     ASSERT_EQ("(c1 ASC, col2 ASC)", order3->GetExprString());
     ASSERT_EQ("(col1 ASC, col2 DESC)", order4->GetExprString());
+
+    {
+        std::ostringstream oss;
+        order1->Print(oss, "");
+        ASSERT_EQ(
+            "+-node[kExpr]\n"
+            "  +-order_expressions: (col1 ASC, col2 ASC)",
+            oss.str());
+    }
+    {
+        std::ostringstream oss;
+        order_expression->Print(oss, "");
+        ASSERT_EQ("col1 ASC", order_expression->GetExprString());
+        ASSERT_EQ("+-node[kExpr]\n"
+            "  +-order_expression: col1 ASC", oss.str());
+    }
+}
+TEST_F(SqlNodeTest, ParameterExprTest) {
+    ParameterExpr* parameter_expr1 = node_manager_->MakeParameterExpr(1);
+    ParameterExpr* parameter_expr2 = node_manager_->MakeParameterExpr(1);
+    ParameterExpr* parameter_expr3 = node_manager_->MakeParameterExpr(2);
+    ASSERT_TRUE(node::ExprEquals(parameter_expr1, parameter_expr1));
+    ASSERT_TRUE(node::ExprEquals(parameter_expr1, parameter_expr2));
+    ASSERT_FALSE(node::ExprEquals(parameter_expr1, parameter_expr3));
+
+    ASSERT_EQ("(?1)", parameter_expr1->GetExprString());
+}
+TEST_F(SqlNodeTest, LimitNodeTest) {
+    SqlNode *node1 = node_manager_->MakeLimitNode(100);
+    SqlNode *node2 = node_manager_->MakeLimitNode(100);
+    SqlNode *node3 = node_manager_->MakeLimitNode(200);
+    ASSERT_TRUE(node::SqlEquals(node1, node1));
+    ASSERT_TRUE(node::SqlEquals(node1, node2));
+    ASSERT_FALSE(node::SqlEquals(node1, node3));
+    std::ostringstream oss;
+    node1->Print(oss, "");
+    ASSERT_EQ("+-node[kLimit]\n"
+        "  +-limit_cnt: 100", oss.str());
+
 }
 }  // namespace node
 }  // namespace hybridse
