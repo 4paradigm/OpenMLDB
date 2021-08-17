412df5ed23d2cc682c0069216deae2aaaad32b20
Chen22 <jingchen2222@gmail.com>
2021-08-17 10:03:25 -0700

    move setup_thirdparty to openmldb/steps/...

--- hybridse/tools/hybridse_core_test.sh
+++ hybridse/tools/hybridse_core_test.sh
@@ -21,7 +21,9 @@ popd
 # goto hybridse directory
 pushd "$(dirname "$0")/.."
 
-./tools/setup_thirdparty.sh
+# install thirdparty hybrise
+HYBRIDSE_THIRDPARTY="$(pwd)/thirdparty"
+../steps/setup_thirdparty.sh ${HYBRIDSE_THIRDPARTY}
 
 if uname -a | grep -q Darwin; then
   # in case coreutils not install on mac
--- steps/init_env.sh
+++ steps/init_env.sh
@@ -21,51 +21,30 @@ pushd "$(dirname "$0")/.."
 
 ROOT=$(pwd)
 ARCH=$(arch)
-
-echo "Install thirdparty ... for $(uname -a)"
+# hybridse is required downloaded or install locally
+HYBRIDSE_SOURCE=$1
 
 # on local machine, one can tweak thirdparty path by passing extra argument
-THIRDPARTY_PATH=${1:-"$ROOT/thirdparty"}
+THIRDPARTY_PATH=${2:-"$ROOT/thirdparty"}
 THIRDSRC_PATH="$ROOT/thirdsrc"
 
-if [ -d "$THIRDPARTY_PATH" ]; then
+echo "THIRDPARTY_PATH: "${THIRDPARTY_PATH}
+echo "Install thirdparty ... for $(uname -a)"
+
+if [[ -d "$THIRDPARTY_PATH" ]]; then
     echo "thirdparty path: $THIRDPARTY_PATH already exist, skip download deps"
+else
+    ./steps/setup_thirdparty.sh ${THIRDPARTY_PATH}
+fi
+
+echo "HYBRIDSE_SOURCE: "${HYBRIDSE_SOURCE}
+if [ -d "$THIRDPARTY_PATH/hybridse" ]; then
+    echo "thirdparty path: $THIRDPARTY_PATH/hybridse already exist, skip download/install deps"
     exit 0
 fi
-
-mkdir -p "$THIRDPARTY_PATH"
-mkdir -p "$THIRDPARTY_PATH/hybridse"
-mkdir -p "$THIRDSRC_PATH"
-
-pushd "${THIRDSRC_PATH}"
-
-if [[ "$OSTYPE" = "darwin"* ]]; then
-    curl -SLo thirdparty.tar.gz https://github.com/jingchen2222/hybridsql-asserts/releases/download/v0.4.0/thirdparty-2021-08-03-darwin-x86_64.tar.gz
-    curl -SLo libzetasql.tar.gz https://github.com/jingchen2222/zetasql/releases/download/v0.2.0/libzetasql-0.2.0-darwin-x86_64.tar.gz
-    curl -SLo hybridse.tar.gz https://github.com/4paradigm/HybridSE/releases/download/v0.2.1/hybridse-0.2.1-darwin-x86_64.tar.gz
-elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
-    if [[ $ARCH = 'x86_64' ]]; then
-        curl -SLo thirdparty.tar.gz https://github.com/jingchen2222/hybridsql-asserts/releases/download/v0.4.0/thirdparty-2021-08-03-linux-gnu-x86_64.tar.gz
-        curl -SLo libzetasql.tar.gz https://github.com/jingchen2222/zetasql/releases/download/v0.2.0/libzetasql-0.2.0-linux-x86_64.tar.gz
-        curl -SLo hybridse.tar.gz https://github.com/4paradigm/HybridSE/releases/download/v0.2.1/hybridse-0.2.1-linux-x86_64.tar.gz
-    elif [[ $ARCH = 'aarch64' ]]; then
-        curl -SLo thirdparty.tar.gz https://github.com/jingchen2222/hybridsql-asserts/releases/download/v0.4.0/thirdparty-2021-08-03-linux-gnu-aarch64.tar.gz
-        curl -SLo libzetasql.tar.gz https://github.com/aceforeverd/zetasql/releases/download/v0.2.1-beta5/libzetasql-0.2.1-beta5-linux-gnu-aarch64.tar.gz
-        # NOTE: missing hybridse-aarch64
-    fi
-fi
-
-curl -SLo thirdsrc.tar.gz https://github.com/jingchen2222/hybridsql-asserts/releases/download/v0.4.0/thirdsrc-2021-08-03.tar.gz
-
-tar xzf thirdparty.tar.gz -C "$THIRDPARTY_PATH" --strip-components 1
-tar xzf libzetasql.tar.gz -C "$THIRDPARTY_PATH" --strip-components 1
-tar xzf thirdsrc.tar.gz -C "$THIRDSRC_PATH" --strip-components 1
-
-popd
-
-HYBRIDSE_SOURCE=$1
 if [[ ${HYBRIDSE_SOURCE} = "local" ]]; then
-  cd ${ROOT}/hybridse
+  echo "Install hybridse locally"
+  cd "${ROOT}/hybridse"
   ln -sf $THIRDPARTY_PATH thirdparty
   ln -sf $THIRDSRC_PATH thirdsrc
   if uname -a | grep -q Darwin; then
@@ -78,7 +57,20 @@ if [[ ${HYBRIDSE_SOURCE} = "local" ]]; then
   make -j"$(nproc)" install
   mv hybridse ${THIRDPARTY_PATH}/hybridse
 else
+  echo "Download hybridse package"
   pushd "${THIRDSRC_PATH}"
+
+  if [[ "$OSTYPE" = "darwin"* ]]; then
+    curl -SLo hybridse.tar.gz https://github.com/4paradigm/HybridSE/releases/download/v0.2.1/hybridse-0.2.1-darwin-x86_64.tar.gz
+  elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+    if [[ $ARCH = 'x86_64' ]]; then
+        curl -SLo hybridse.tar.gz https://github.com/4paradigm/HybridSE/releases/download/v0.2.1/hybridse-0.2.1-linux-x86_64.tar.gz
+    elif [[ $ARCH = 'aarch64' ]]; then
+        # NOTE: missing hybridse-aarch64
+        echo "missing hybridse-aarch64"
+    fi
+  fi
+  mkdir -p "$THIRDPARTY_PATH/hybridse"
   tar xzf hybridse.tar.gz -C "${THIRDPARTY_PATH}/hybridse" --strip-components 1
   popd
 fi
