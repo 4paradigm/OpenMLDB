9731933f5e0dc967d4a001f68357af294880610b
kane <1290561498@qq.com>
2021-08-23 17:45:38 +0800

    Add UnitTest and Fix a bug

--- java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/utils/SparkUtil.scala
+++ java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/utils/SparkUtil.scala
@@ -139,7 +139,7 @@ object SparkUtil {
       }
     }
 
-    df1.except(df1).isEmpty && df2.except(df2).isEmpty
+    df1.except(df2).isEmpty && df2.except(df1).isEmpty
   }
 
   /** Use Java reflect to call private method to convert RDD[InternalRow] to DataFrame.
---
+++ java/openmldb-batch/src/test/scala/com/_4paradigm/openmldb/batch/end2end/TestOrderBy.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021 4Paradigm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com._4paradigm.openmldb.batch.end2end
+
+;
+
+import com._4paradigm.openmldb.batch.api.OpenmldbSession
+import com._4paradigm.openmldb.batch.utils.SparkUtil
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.scalatest.FunSuite;
+
+class TestOrderBy extends FunSuite {
+
+  test("Test end2end Order by") {
+
+    val spark = SparkSession.builder().master("local[*]")
+      .getOrCreate()
+    val sess = new OpenmldbSession(spark)
+
+    val data = Seq(
+      Row(5, 111, 500, 5),
+      Row(6, 222, 600, 6),
+      Row(8, 222, 800, 8),
+      Row(9, 111, 900, 9),
+      Row(1, 111, 100, 1),
+      Row(2, 222, 200, 2),
+      Row(3, 111, 300, 3),
+      Row(4, 222, 400, 4),
+      Row(7, 111, 700, 7),
+      Row(10, 222, 1000, 10))
+    val schema = StructType(List(
+      StructField("id", IntegerType),
+      StructField("user", IntegerType),
+      StructField("trans_amount", IntegerType),
+      StructField("trans_time", IntegerType)))
+    val df = spark.createDataFrame(spark.sparkContext.makeRDD(data), schema)
+
+    sess.registerTable("t1", df)
+    df.createOrReplaceTempView("t1")
+
+    val sqlText1 =
+      """
+        | SELECT * FROM t1 ORDER BY trans_amount
+     """.stripMargin
+
+    val outputDfAsc = sess.sql(sqlText1).getSparkDf()
+    val sparksqlOutputDfAsc = sess.sparksql(sqlText1)
+
+    val outputDfAscList = outputDfAsc.collectAsList()
+    val sparksqlOutputDfAscList = sparksqlOutputDfAsc.collectAsList()
+
+    assert(outputDfAscList.equals(sparksqlOutputDfAscList) && outputDfAsc.schema == sparksqlOutputDfAsc.schema)
+
+    val sqlText2 =
+      """
+        | SELECT * FROM t1 ORDER BY trans_amount DESC
+     """.stripMargin
+
+    val outputDfDesc = sess.sql(sqlText2).getSparkDf()
+    val sparksqlOutputDfDesc = sess.sparksql(sqlText2)
+
+    val outputDfDescList = outputDfAsc.collectAsList()
+    val sparksqlOutputDfDescList = sparksqlOutputDfAsc.collectAsList()
+
+    assert(outputDfDescList.equals(sparksqlOutputDfDescList) && outputDfDesc.schema == sparksqlOutputDfDesc.schema)
+  }
+
+}
