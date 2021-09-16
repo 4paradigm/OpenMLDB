bd279c235d918c3dba095c7931b0f00c08295a0e
zhaowei <zw1020688@163.com>
2021-09-16 15:28:04 +0800

    test java cicd

--- test/steps/deploy_fedb.sh
+++ test/steps/deploy_fedb.sh
@@ -38,7 +38,7 @@ echo "TABLET2:${TABLET2}"
 echo "TABLET3:${TABLET3}"
 echo "API_SERVER:${API_SERVER}"
 # deploy zk
-wget http://pkg.4paradigm.com:81/rtidb/test/zookeeper-3.4.14.tar.gz
+wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.14/zookeeper-3.4.14.tar.gz
 tar -zxvf zookeeper-3.4.14.tar.gz
 cd zookeeper-3.4.14 || exit
 cp conf/zoo_sample.cfg conf/zoo.cfg || exit
@@ -51,7 +51,8 @@ sleep 5
 function deploy_ns() {
   local ns_name=$1
   local ns_endpoint=$2
-  cp -r fedb-cluster-"${SERVER_VERSION}" "${ns_name}"
+  local pkg_name=$3
+  cp -r "${pkg_name}" "${ns_name}"
   cd "${ns_name}" || exit
   sed -i "s#--zk_cluster=.*#--zk_cluster=${ZK_CLUSTER}#" conf/nameserver.flags
   echo '--request_timeout_ms=60000' >> conf/nameserver.flags
@@ -65,21 +66,22 @@ function deploy_ns() {
   cd .. || exit
   sleep 5
 }
-wget http://pkg.4paradigm.com:81/rtidb/test/fedb-"${SERVER_VERSION}"-linux.tar.gz
-tar -zxvf fedb-"${SERVER_VERSION}"-linux.tar.gz
+wget https://github.com/4paradigm/OpenMLDB/releases/download/v"${SERVER_VERSION}"/openmldb-"${SERVER_VERSION}"-linux.tar.gz
+tar -zxvf openmldb-"${SERVER_VERSION}"-linux.tar.gz
 # shellcheck disable=SC2010
-pkg_name=$(ls | grep fedb-cluster)
+pkg_name=$(ls | grep openmldb)
 echo "pkg_name:${pkg_name}"
-deploy_ns fedb-ns-1 "${NS1}"
-deploy_ns fedb-ns-2 "${NS2}"
+deploy_ns openmldb-ns-1 "${NS1}" "${pkg_name}"
+deploy_ns openmldb-ns-2 "${NS2}" "${pkg_name}"
 # deploy tablet
 function deploy_tablet() {
   local tablet_name=$1
   local tablet_endpoint=$2
-  cp -r fedb-cluster-"${SERVER_VERSION}" "${tablet_name}"
+  local pkg_name=$3
+  cp -r "${pkg_name}" "${tablet_name}"
   cd "${tablet_name}" || exit
   sed -i "s#--zk_cluster=.*#--zk_cluster=${ZK_CLUSTER}#" conf/tablet.flags
-  sed -i 's@--zk_root_path=.*@--zk_root_path=/fedb@' conf/tablet.flags
+  sed -i 's@--zk_root_path=.*@--zk_root_path=/openmldb@' conf/tablet.flags
   sed -i "s#--endpoint=.*#--endpoint=${tablet_endpoint}#" conf/tablet.flags
   if [[ "${DEPLOY_MODE}" == "cluster" ]]; then
     sed -i 's#--enable_distsql=.*#--enable_distsql=true#' conf/tablet.flags
@@ -90,17 +92,18 @@ function deploy_tablet() {
   cd ..
   sleep 5
 }
-deploy_tablet fedb-tablet-1 "${TABLET1}"
-deploy_tablet fedb-tablet-2 "${TABLET2}"
-deploy_tablet fedb-tablet-3 "${TABLET3}"
+deploy_tablet openmldb-tablet-1 "${TABLET1}" "${pkg_name}"
+deploy_tablet openmldb-tablet-2 "${TABLET2}" "${pkg_name}"
+deploy_tablet openmldb-tablet-3 "${TABLET3}" "${pkg_name}"
 # deploy api server
 function deploy_api_server() {
   local api_serve_name=$1
   local api_serve_endpoint=$API_SERVER
-  cp -r fedb-cluster-"${SERVER_VERSION}" "${api_serve_name}"
+  local pkg_name=$3
+  cp -r "${pkg_name}" "${api_serve_name}"
   cd "${api_serve_name}" || exit
   sed -i "s#--zk_cluster=.*#--zk_cluster=${ZK_CLUSTER}#" conf/apiserver.flags
-  sed -i 's@--zk_root_path=.*@--zk_root_path=/fedb@' conf/apiserver.flags
+  sed -i 's@--zk_root_path=.*@--zk_root_path=/openmldb@' conf/apiserver.flags
   sed -i "s#--endpoint=.*#--endpoint=${api_serve_endpoint}#" conf/apiserver.flags
   sed -i "s#--role=.*#--role=apiserver#" conf/apiserver.flags
 #  if [[ "${DEPLOY_MODE}" == "cluster" ]]; then
@@ -112,4 +115,4 @@ function deploy_api_server() {
   cd ..
   sleep 5
 }
-deploy_api_server fedb-apiserver-1
+deploy_api_server openmldb-apiserver-1
