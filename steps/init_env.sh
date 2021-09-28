a1ac0df7d0a80e84bb77cdfa056b04ac317eb76b
Chen22 <jingchen2222@gmail.com>
2021-09-27 17:14:16 -0700

    feat: update changelog and cicd and version

--- steps/init_env.sh
+++ steps/init_env.sh
@@ -68,10 +68,10 @@ else
     pushd "${THIRDSRC_PATH}"
 
     if [[ "$OSTYPE" = "darwin"* ]]; then
-        curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-test2/hybridse-0.2.4-test2-darwin-x86_64.tar.gz
+        curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-0927/hybridse-0.2.4-0927-darwin-x86_64.tar.gz
     elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
         if [[ $ARCH = 'x86_64' ]]; then
-            curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-test2/hybridse-0.2.4-test2-linux-x86_64.tar.gz
+            curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-0927/hybridse-0.2.4-0927-linux-x86_64.tar.gz
         elif [[ $ARCH = 'aarch64' ]]; then
             # NOTE: missing hybridse-aarch64
             echo "missing hybridse-aarch64"
