f6b4ac98f3d46580cb1f25c03bd33d9e46535f99
Chen22 <jingchen2222@gmail.com>
2021-09-29 11:38:12 -0700

    fix: shell style

--- hybridse/examples/toydb/tools/micro_bench.sh
+++ hybridse/examples/toydb/tools/micro_bench.sh
@@ -15,9 +15,9 @@
 
 #
 # micro_bench.sh
-PWD=`pwd`
-export JAVA_HOME=${PWD}/thirdparty/jdk1.8.0_141
-export PATH=${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH
+PWD=$(pwd)
+export JAVA_HOME="${PWD}/thirdparty/jdk1.8.0_141"
+export PATH="${PWD}/thirdparty/bin:$JAVA_HOME/bin:${PWD}/thirdparty/apache-maven-3.6.3/bin:$PATH"
 
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCOVERAGE_ENABLE=OFF -DTESTING_ENABLE=OFF -DBENCHMARK_ENABLE=ON
