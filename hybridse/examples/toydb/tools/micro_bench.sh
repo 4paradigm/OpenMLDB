f24951b8bac82748e91216789e3d0f65d49545af
Chen22 <jingchen2222@gmail.com>
2021-09-29 11:25:51 -0700

    cicd: deploy allinone always

--- hybridse/examples/toydb/tools/micro_bench.sh
+++ hybridse/examples/toydb/tools/micro_bench.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 # Copyright 2021 4Paradigm
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#! /bin/sh
 #
 # micro_bench.sh
 PWD=`pwd`
