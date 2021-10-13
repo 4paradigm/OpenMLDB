425ce874a4241e665805f618c1e97b4d11087ac6
heiyan1shengdun <32361605+heiyan1shengdun@users.noreply.github.com>
2021-10-13 12:04:02 +0800

    fix shellcheck error

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 if ! [ -x "$(command -v java)" ]; then
   echo 'Error: java is not installed.' >&2
   exit 1
@@ -11,24 +12,24 @@ fi
 
 echo 'install zookeeper'
 FILE=zookeeper-3.4.14.tar.gz
-if [ -e "$FILE" ]; then
-    echo "$FILE exist"
+ZK=zookeeper-3.4.14
+if [ -e "${FILE}" ]; then
+    echo "${FILE} exist"
 else 
-  ZK=zookeeper-3.4.14
   wget "https://archive.apache.org/dist/zookeeper/${ZK}/${ZK}.tar.gz"
-  tar -zxvf zookeeper-3.4.14.tar.gz
-  cd zookeeper-3.4.14
+  tar -zxvf ${FILE}
+  cd ${ZK}
   cp conf/zoo_sample.cfg conf/zoo.cfg
   cd ..
 fi
 
 echo 'install nameserver and tabnet'
 FILE=openmldb-0.2.2-linux.tar.gz
-if [ -e "$FILE" ]; then
-  echo "$FILE exist"
+if [ -e "${FILE}" ]; then
+  echo "${FILE} exist"
 else
   wget https://github.com/4paradigm/OpenMLDB/releases/download/0.2.2/openmldb-0.2.2-linux.tar.gz
-  if [ -e "$FILE" ]; then
+  if [ -e "${FILE}" ]; then
     tar -zxvf openmldb-0.2.2-linux.tar.gz
     mv openmldb-0.2.2-linux openmldb-ns-0.2.2
     echo 'install tablet'
@@ -43,15 +44,15 @@ fi
 echo 'install complete'
 
 echo 'starting zookeeper'
-cd zookeeper-3.4.14
+cd ${ZK}
 dataDir="./data"
 clientPort="6181"
 #modify zookeeper config
 sed -i "s:dataDir=[a-zA-Z0-9//]*:dataDir=${dataDir}:" conf/zoo.cfg
 sed -i "s:clientPort=[0-9]*:clientPort=${clientPort}:" conf/zoo.cfg
 PID=$(lsof -i :${clientPort}|grep -v "PID" | awk '{print $2}')
-echo $PID
-if [ "$PID" != "" ]; then
+echo "${PID}"
+if [ "${PID}" != "" ]; then
    echo "zk port is unavailable"
    exit 1
 fi
@@ -65,20 +66,19 @@ IP=$(hostname -i)
 #get available port
 ns_port=6527
 PID=$(lsof -i :${ns_port}|grep -v "PID" | awk '{print $2}')
-if [ "$PID" != "" ]; then
+if [ "${PID}" != "" ]; then
    echo "ns port is unavailable; try to get another port!"
-   templ=0
    ns_port=0
-   while [ "$ns_port" -eq 0 ]; do
+   while [ "${ns_port}" -eq 0 ]; do
       temp1=$(shuf -i 1024-10000 -n1)
-      PID=$(lsof -i :${temp1}|grep -v "PID" | awk '{print $2}')
-      if [ "$PID" != "" ] ; then
+      PID=$(lsof -i :"${temp1}"|grep -v "PID" | awk '{print $2}')
+      if [ "${PID}" != "" ] ; then
          echo "try to get another port!"
       else
-         ns_port=$temp1
+         ns_port=${temp1}
       fi
    done
-   echo $ns_port
+   echo "${ns_port}"
 fi
 #modify nameserver config
 sed -i "s:--endpoint=[a-zA-Z0-9//.:]*:--endpoint=${IP}\:${ns_port}:" conf/nameserver.flags
@@ -96,20 +96,19 @@ IP=$(hostname -i)
 #get available port
 ns_port=9527
 PID=$(lsof -i :${ns_port}|grep -v "PID" | awk '{print $2}')
-if [ "$PID" != "" ]; then
+if [ "${PID}" != "" ]; then
    echo "ns port is unavailable; try to get another port!"
-   templ=0
    ns_port=0
-   while [ "$ns_port" -eq 0 ]; do
+   while [ "${ns_port}" -eq 0 ]; do
       temp1=$(shuf -i 1024-10000 -n1)
-      PID=$(lsof -i :${temp1}|grep -v "PID" | awk '{print $2}')
-      if [ "$PID" != "" ] ; then
+      PID=$(lsof -i :"${temp1}"|grep -v "PID" | awk '{print $2}')
+      if [ "${PID}" != "" ] ; then
          echo "try to get another port!"
       else
-         ns_port=$temp1
+         ns_port=${temp1}
       fi
    done
-   echo $ns_port
+   echo "${ns_port}"
 fi
 #modify tabnet config
 sed -i "s:--endpoint=[a-zA-Z0-9//.:]*:--endpoint=${IP}\:${ns_port}:" conf/tablet.flags
