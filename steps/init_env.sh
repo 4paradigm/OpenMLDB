b34c302367f42d659fa87b88d45037a4c67e314e
Chen22 <jingchen2222@gmail.com>
2021-10-18 17:12:54 -0700

    feat: update hybridse package

--- steps/init_env.sh
+++ steps/init_env.sh
@@ -68,10 +68,10 @@ else
     pushd "${THIRDSRC_PATH}"
 
     if [[ "$OSTYPE" = "darwin"* ]]; then
-        curl -SLo hybridse.tar.gz https://github.com/vagetablechicken/fedb/releases/download/hybridse-v0.2.4.20210930/hybridse-0.2.4.20210930-darwin-x86_64.tar.gz
+        curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-20211018/hybridse-0.2.4-20211018-darwin-x86_64.tar.gz
     elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
         if [[ $ARCH = 'x86_64' ]]; then
-            curl -SLo hybridse.tar.gz https://github.com/vagetablechicken/fedb/releases/download/hybridse-v0.2.4.20210930/hybridse-0.2.4.20210930-linux-x86_64.tar.gz
+            curl -SLo hybridse.tar.gz https://github.com/jingchen2222/OpenMLDB/releases/download/hybridse-v0.2.4-20211018/hybridse-0.2.4-20211018-linux-x86_64.tar.gz
         elif [[ $ARCH = 'aarch64' ]]; then
             # NOTE: missing hybridse-aarch64
             echo "missing hybridse-aarch64"
