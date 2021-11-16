6b75ca2aec2f65dc4564f35d60b600d5726a966d
zhaowei <zw1020688@163.com>
2021-11-16 18:29:24 +0800

    modify out case

--- test/integration-test/openmldb-test-java/openmldb-sdk-test/src/main/java/com/_4paradigm/openmldb/java_sdk_test/util/CommandResultUtil.java
+++ test/integration-test/openmldb-test-java/openmldb-sdk-test/src/main/java/com/_4paradigm/openmldb/java_sdk_test/util/CommandResultUtil.java
@@ -26,7 +26,8 @@ public class CommandResultUtil {
         String tmp = s.toLowerCase();
         return tmp.contains("fail")||tmp.contains("error")||tmp.contains("unknow")||tmp.contains("unsupported")||
                 tmp.contains("not an astint")||tmp.contains("already exists")||tmp.contains("not supported")
-                ||tmp.contains("not found")||tmp.contains("un-support")||tmp.contains("invalid")||tmp.contains("distribution element is not");
+                ||tmp.contains("not found")||tmp.contains("un-support")||tmp.contains("invalid")
+                ||tmp.contains("distribution element is not")||tmp.contains("is not currently supported");
     }
     public static OpenMLDBSchema parseSchema(List<String> lines){
         OpenMLDBSchema schema = new OpenMLDBSchema();
