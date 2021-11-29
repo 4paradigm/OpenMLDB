2b58c2bcc088436d02a4b3eef295894715796b0c
tobe <tobeg3oogle@gmail.com>
2021-11-29 18:04:52 +0800

    Revert ut zk port

--- src/sdk/sql_cluster_test.cc
+++ src/sdk/sql_cluster_test.cc
@@ -632,7 +632,7 @@ TEST_F(SQLClusterTest, GetTableSchema) {
 int main(int argc, char** argv) {
     ::hybridse::vm::Engine::InitializeGlobalLLVM();
     FLAGS_zk_session_timeout = 100000;
-    ::openmldb::sdk::MiniCluster mc(2181);
+    ::openmldb::sdk::MiniCluster mc(6181);
     ::openmldb::sdk::mc_ = &mc;
     FLAGS_enable_distsql = true;
     int ok = ::openmldb::sdk::mc_->SetUp(3);
