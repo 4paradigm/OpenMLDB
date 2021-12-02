d963920bd1f39d30b20a5a396a960b97133e7fab
Nicholas-SR <nicholasrobertson.sw@gmail.com>
2021-12-02 03:13:34 -0500

    Revert "refactor: delete unused scripts"
    This reverts commit 88ed2dca4ff5c2cf29a1e86b2b379e3afc0c8cc0.

---
+++ onebox/start_onebox_on_rambuild_cluster.sh
@@ -0,0 +1,90 @@
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#! /bin/sh
+#
+ulimit -c unlimited
+# start_onebox.sh
+
+# first start zookeeper
+IP=127.0.0.1
+
+ZK_CLUSTER=$IP:6181
+NS1=$IP:9622
+NS2=$IP:9623
+NS3=$IP:9624
+TABLET1=$IP:9520
+TABLET2=$IP:9521
+TABLET3=$IP:9522
+
+RAMBUILD_PREFIX=/tmp/rambuild
+../build/bin/openmldb --db_root_path=${RAMBUILD_PREFIX}/tablet0-binlogs \
+                   --recycle_bin_root_path=${RAMBUILD_PREFIX}/recycle_bin0 \
+                   --endpoint=${TABLET1} --role=tablet \
+                   --binlog_notify_on_put=true\
+                   --enable_distsql=true\
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --zk_keep_alive_check_interval=100000000\
+                   --zk_root_path=/cluster> tablet0.log 2>&1 &
+
+# start tablet1
+../build/bin/openmldb --db_root_path=${RAMBUILD_PREFIX}/tablet1-binlogs \
+                   --recycle_bin_root_path=${RAMBUILD_PREFIX}/recycle_bin1 \
+                   --endpoint=${TABLET2} --role=tablet \
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --binlog_notify_on_put=true\
+                   --enable_distsql=true\
+                   --zk_keep_alive_check_interval=100000000\
+                   --zk_root_path=/cluster > tablet1.log 2>&1 &
+
+# start tablet2
+../build/bin/openmldb --db_root_path=${RAMBUILD_PREFIX}/tablet2-binlogs \
+                   --recycle_bin_root_path=${RAMBUILD_PREFIX}/recycle_bin2 \
+                   --endpoint=${TABLET3} --role=tablet \
+                   --binlog_notify_on_put=true\
+                   --enable_distsql=true\
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --zk_keep_alive_check_interval=100000000\
+                   --zk_root_path=/cluster > tablet2.log 2>&1 &
+
+# start ns1
+../build/bin/openmldb --endpoint=${NS1} --role=nameserver \
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --tablet_offline_check_interval=1\
+                   --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=100000\
+                   --zk_root_path=/cluster > ns1.log 2>&1 &
+sleep 2
+
+# start ns2
+../build/bin/openmldb --endpoint=${NS2} --role=nameserver \
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --tablet_offline_check_interval=1\
+                   --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=100000\
+                   --zk_root_path=/cluster > ns2.log 2>&1 &
+sleep 2
+
+# start ns3
+../build/bin/openmldb --endpoint=${NS3} --role=nameserver \
+                   --tablet_offline_check_interval=1\
+                   --tablet_heartbeat_timeout=1\
+                   --request_timeout_ms=100000\
+                   --zk_cluster=${ZK_CLUSTER}\
+                   --zk_root_path=/cluster > ns3.log 2>&1 &
+
+sleep 5
+
+echo "start all ok"
+
---
+++ steps/fesql_test_python.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+ROOT_DIR=$(pwd)
+test -d /rambuild/ut_zookeeper && rm -rf /rambuild/ut_zookeeper/*
+cp steps/zoo.cfg thirdsrc/zookeeper-3.4.14/conf
+cd thirdsrc/zookeeper-3.4.14
+# TODO(hw): macos no -p
+netstat -anp | grep 6181 | awk '{print $NF}' | awk -F '/' '{print $1}'| xargs -I{} kill -9 {}
+./bin/zkServer.sh start && cd "$ROOT_DIR"
+echo "zk started"
+sleep 5
+cd onebox && sh start_onebox.sh && cd "$ROOT_DIR"
+echo "onebox started, check"
+sleep 5
+pgrep -f openmldb
+echo "ROOT_DIR:${ROOT_DIR}"
+
+cd "${ROOT_DIR}"/build/python/dist/
+whl_name=$(ls openmldb*.whl)
+echo "whl_name:${whl_name}"
+python3 -m pip install "${whl_name}" -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# needs: easy_install nose (sqlalchemy is openmldb required)
+cd "${ROOT_DIR}"/python/test
+nosetests --with-xunit
+
+cd "${ROOT_DIR}"/onebox && sh stop_all.sh && cd "$ROOT_DIR"
+cd thirdsrc/zookeeper-3.4.14 && ./bin/zkServer.sh stop && cd "$ROOT_DIR"
