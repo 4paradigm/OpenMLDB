1a585765b864b18bc015939c9a7f45a97c939a3f
aceforeverd <teapot@aceforeverd.com>
2021-12-19 16:07:04 +0800

    refactor(cicd): java sdk jobs

--- java/prepare_release.sh
+++ java/prepare_release.sh
@@ -18,11 +18,17 @@
 #  - release version: '0.1.4'
 #  - snapshot version: '0.1.4-SNAPSHOT'
 #  - beta/alpha: '0.12.2.beta1', '0.12.2.alpha2', will convert released as '0.12.2-SNAPSHOT'
+#  - a optional debug number come after base version, 0.12.2.33.alpha -> 0.12.2.33-SNAPSHOT
+#  - string literal 'main': it is treated as a push to main, no version number will tweaked
 #
 # in case the version number passed in (extracted from tag) is in wrong style, the script will
 # replace any string after 'x.x.x' with '-SNAPSHOT', to avoid publish directly into maven central
 
 set -eE
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 
 if [ -z "$1" ]; then
     echo -e "Usage: $0 \$VERSION.\n\terror: version number required"
@@ -32,6 +38,11 @@ fi
 cd "$(dirname "$0")"
 
 VERSION=$1
+
+if [[ $VERSION = 'main' ]]; then
+  echo -e "${GREEN}quiting... not release from a tag push, $0 will not tweak the java versions but instead respect versions already set in code${NC}"
+  exit 0;
+fi
 # rm semVer number from VERSION of 3 numbers MAJOR.MINOR.PATCH
 #  0.1.2 -> ''
 #  0.1.2-SNAPSHOT -> '-SNAPSHOT'
@@ -50,6 +61,12 @@ BASE_VERSION=${VERSION%"$SUFFIX_VERSION"}
 if [[ -n $SUFFIX_VERSION ]] ; then
     SUFFIX_VERSION=-SNAPSHOT
 fi
+
+if [[ -z $BASE_VERSION ]]; then
+  echo -e "${RED}invalid version number inputed: $VERSION${NC}"
+  exit 1
+fi
+
 # VERSION             -> BASE_VERSION    DEBUG_NO    SUFFIX_VERSION
 # 0.1.2               -> 0.1.2
 # 0.1.2-SNAPSHOT      -> 0.1.2                       SNAPSHOT
@@ -60,8 +77,6 @@ fi
 # 0.1.2-0928          -> 0.2.1                       SNAPSHOT
 echo "BASE_VERSION: ${BASE_VERSION}, DEBUG_NO: ${DEBUG_NO}, SUFFIX_VERSION: ${SUFFIX_VERSION}"
 JAVA_VERSION="$BASE_VERSION${DEBUG_NO}$SUFFIX_VERSION"
+echo -e "${GREEN}setting release version to: $JAVA_VERSION${NC}"
 
 mvn versions:set -DnewVersion="$JAVA_VERSION"
-
-mvn versions:set-property -Dproperty="project.version.base" -DnewVersion="$BASE_VERSION${DEBUG_NO}"
-mvn versions:set-property -Dproperty="project.version.suffix" -DnewVersion="$SUFFIX_VERSION"
