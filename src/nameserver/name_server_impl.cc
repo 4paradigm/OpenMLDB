7ad6d9f6ff972e63a3fd49d1482a992374d45eff
“keyu” <1121848065@qq.com>
2021-12-24 15:38:20 +0800

    issue810

--- src/nameserver/name_server_impl.cc
+++ src/nameserver/name_server_impl.cc
@@ -1066,24 +1066,7 @@ void NameServerImpl::UpdateTablets(const std::vector<std::string>& endpoints) {
         }
     }
 
-    if (nearline_tablet_endpoint.empty()) {
-        if (nearline_tablet_.state_ == ::openmldb::type::EndpointState::kHealthy) {
-            nearline_tablet_.state_ = ::openmldb::type::EndpointState::kOffline;
-            PDLOG(WARNING, "nearline endpoint %s is offline", nearline_tablet_.client_->GetEndpoint().c_str());
-        }
-    } else if (nearline_tablet_.state_ == ::openmldb::type::EndpointState::kOffline) {
-        auto it = real_ep_map_.find(nearline_tablet_endpoint);
-        if (it != real_ep_map_.end()) {
-            nearline_tablet_.client_ =
-                std::make_shared<::openmldb::client::NearLineTabletClient>(nearline_tablet_endpoint, it->second, true);
-            if (nearline_tablet_.client_->Init() == 0) {
-                nearline_tablet_.state_ = ::openmldb::type::EndpointState::kHealthy;
-                PDLOG(INFO, "nearline endpoint %s is online", it->second.c_str());
-            } else {
-                PDLOG(WARNING, "fail to init nearline endpoint %s", it->second.c_str());
-            }
-        }
-    }
+
 
     auto it = tablet_endpoints.begin();
     for (; it != tablet_endpoints.end(); ++it) {
@@ -3656,26 +3639,8 @@ void NameServerImpl::CreateTable(RpcController* controller, const CreateTableReq
         }
     }
 
-    // if `create table xx (xx, index()) xx;` or `create table xx (xx, index(ts=xx)) xx;`
-    if (table_info->column_key_size() == 1 && table_info->column_key(0).index_name().empty() &&
-        table_info->column_key(0).col_name_size() == 0) {
-        if (IsClusterMode()) {
-            auto ret = CreateOfflineTable(table_info->db(), table_info->name(), table_info->column_key(0).ts_name(),
-                                          table_info->column_desc());
-            base::SetResponseStatus(ret, response);
-            if (ret.code != 0) {
-                response->set_code(::openmldb::base::ReturnCode::kCreateTableFailed);
-            }
-            return;
-        } else {
-            schema::IndexUtil::AddDefaultIndex(table_info.get());
-        }
-    } else if (table_info->column_key_size() == 0) {
-        // if no column_key, add one which key is the first column which is not float or double
-        // the logic should be the same as 'create table xx(xx,index(key=<auto_selected_col>)) xx;'
-        // Ref NsClient::TransformToTableDef
-        schema::IndexUtil::AddDefaultIndex(table_info.get());
-    }
+
+    schema::IndexUtil::AddDefaultIndex(table_info.get());
     if (!IsClusterMode()) {
         table_info->set_partition_num(1);
         table_info->set_replica_num(1);
