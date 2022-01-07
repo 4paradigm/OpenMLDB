5574e8a7db846b72f42da89123dc8583fd8400ef
zhaowei <zw1020688@163.com>
2022-01-07 17:42:31 +0800

    add offline select into case

--- test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/temp/TestFEDBDeploy.java
+++ test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/temp/TestFEDBDeploy.java
@@ -51,7 +51,7 @@ public class TestFEDBDeploy {
     @Test
     public void testTmp(){
         FEDBDeploy deploy = new FEDBDeploy("tmp");
-        deploy.setCluster(true);
+        deploy.setCluster(false);
         deploy.setSparkMaster("yarn");
         deploy.setBatchJobJarPath("hdfs://172.27.128.215:8020/Users/tobe/openmldb-batchjob-0.4.0-SNAPSHOT.jar");
         deploy.setSparkYarnJars("hdfs://172.27.128.215:8020/Users/tobe/openmldb_040_jars/*");
