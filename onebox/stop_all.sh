5a25dd247970eb79dfbe8f81e8728ae908d9b7da
tobe <tobeg3oogle@gmail.com>
2022-01-10 11:24:32 +0800

    Update stop all script

--- onebox/stop_all.sh
+++ onebox/stop_all.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # Copyright 2021 4Paradigm
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,10 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#! /bin/sh
-#
-# stop_all.sh
 
 set -x -e
 
