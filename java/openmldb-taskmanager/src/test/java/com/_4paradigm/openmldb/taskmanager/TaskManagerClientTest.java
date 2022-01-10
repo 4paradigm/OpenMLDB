02b25758e5ef0e13fe121d6395aa3cece2db2204
yabg-shuai666 <59086688+yabg-shuai666@users.noreply.github.com>
2022-01-10 18:05:52 +0800

    Update TaskManagerClientTest.java

--- java/openmldb-taskmanager/src/test/java/com/_4paradigm/openmldb/taskmanager/TaskManagerClientTest.java
+++ java/openmldb-taskmanager/src/test/java/com/_4paradigm/openmldb/taskmanager/TaskManagerClientTest.java
@@ -1,19 +1,17 @@
 package com._4paradigm.openmldb.taskmanager;
 
-import com._4paradigm.openmldb.proto.TaskManager;
 import com._4paradigm.openmldb.taskmanager.client.TaskManagerClient;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 
 public abstract class TaskManagerClientTest {
-
-    public static void main(String[] args) throws Exception{
+    @Ignore
+    @Test
+    public void TestManagerClient() throws Exception{
         TaskManagerClient client = new TaskManagerClient("127.0.0.1:9995"); //要与taskmanager.properties里的配置一致
 //        client.stopJob(0);传入job id
-        client.showJobs();
+        client.printJobs();
         client.showJobs();
         client.importOnlineData("load data infile '/Users/4paradigm/IdeaProjects/OpenMLDB/java/openmldb-taskmanager/src/test/java/com/_4paradigm/openmldb/taskmanager/test.csv' " +
                 "into table test_taskmanager.t3 options(format='csv', foo='bar', header=false, mode='append');");//插入需要大概半分钟左右的时间，注意最好不要在t1表上插入，否则会失败。
