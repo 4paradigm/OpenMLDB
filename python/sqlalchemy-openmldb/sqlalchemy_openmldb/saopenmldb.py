42a9ed73c4b7380c33d8b9f2287744f424fe1773
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-17 15:59:53 +0800

    Update saopenmldb.py

--- python/sqlalchemy-openmldb/sqlalchemy_openmldb/saopenmldb.py
+++ python/sqlalchemy-openmldb/sqlalchemy_openmldb/saopenmldb.py
@@ -104,6 +104,8 @@ class OpenMLDBDialect(default.DefaultDialect):
         return module
 
     def has_table(self, connection, table_name, schema=None):
+        if schema is not None:
+            raise Exception("schema unsupported in OpenMLDB")
         return table_name in connection.connection.cursor().get_all_tables()
     
     def create_connect_args(self, url, **kwargs):
