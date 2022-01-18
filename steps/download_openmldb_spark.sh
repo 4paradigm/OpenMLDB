11a0f8348bba1002219e2786fc611cd5704bfcfa
tobe <tobeg3oogle@gmail.com>
2022-01-18 12:54:35 +0800

    Update curl to download spark file

--- steps/download_openmldb_spark.sh
+++ steps/download_openmldb_spark.sh
@@ -5,7 +5,7 @@ set -xe
 SPARK_HOME_PATH=$1
 
 # Download OpenMLDB Spark distribution
-curl https://github.com/4paradigm/spark/releases/download/v3.0.0-openmldb0.4.0/spark-3.0.0-bin-openmldbspark.tgz > spark-3.0.0-bin-openmldbspark.tgz
+curl -L -o spark-3.0.0-bin-openmldbspark.tgz https://github.com/4paradigm/spark/releases/download/v3.0.0-openmldb0.4.0/spark-3.0.0-bin-openmldbspark.tgz
 tar xzf ./spark-3.0.0-bin-openmldbspark.tgz
 
 # Move Spark files to $SPARK_HOME
