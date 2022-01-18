e158fe630aa7243fc381fc3a29a2d31b4355e91f
tobe <tobeg3oogle@gmail.com>
2022-01-18 12:10:18 +0800

    Use curl instead of wget

--- steps/download_openmldb_spark.sh
+++ steps/download_openmldb_spark.sh
@@ -5,10 +5,10 @@ set -xe
 SPARK_HOME_PATH=$1
 
 # Download OpenMLDB Spark distribution
-wget https://github.com/4paradigm/spark/releases/download/v3.0.0-openmldb0.4.0/spark-3.0.0-bin-openmldbspark.tgz
+curl https://github.com/4paradigm/spark/releases/download/v3.0.0-openmldb0.4.0/spark-3.0.0-bin-openmldbspark.tgz > spark-3.0.0-bin-openmldbspark.tgz
 tar xzf ./spark-3.0.0-bin-openmldbspark.tgz
 
 # Move Spark files to $SPARK_HOME
-mv ./spark-3.0.0-bin-openmldbspark/ $SPARK_HOME_PATH
+mv ./spark-3.0.0-bin-openmldbspark/ "$SPARK_HOME_PATH"
 
 rm ./spark-3.0.0-bin-openmldbspark.tgz
