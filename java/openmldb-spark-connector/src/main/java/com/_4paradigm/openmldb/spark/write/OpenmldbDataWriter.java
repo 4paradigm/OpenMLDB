8a3421b4323a533f971b3f711525d9eae0d4b81e
yabg-shuai666 <59086688+yabg-shuai666@users.noreply.github.com>
2022-01-19 18:50:33 +0800

    Update OpenmldbDataWriter.java

--- java/openmldb-spark-connector/src/main/java/com/_4paradigm/openmldb/spark/write/OpenmldbDataWriter.java
+++ java/openmldb-spark-connector/src/main/java/com/_4paradigm/openmldb/spark/write/OpenmldbDataWriter.java
@@ -22,7 +22,6 @@ import com._4paradigm.openmldb.sdk.SdkOption;
 import com._4paradigm.openmldb.sdk.SqlException;
 import com._4paradigm.openmldb.sdk.impl.SqlClusterExecutor;
 import com.google.common.base.Preconditions;
-import jdk.nashorn.internal.objects.annotations.Where;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.write.DataWriter;
 import org.apache.spark.sql.connector.write.WriterCommitMessage;
@@ -111,9 +110,7 @@ public class OpenmldbDataWriter implements DataWriter<InternalRow> {
                     preparedStatement.setString(i + 1, record.getString(i));
                     break;
                 case Types.DATE:
-                    //The new Data() parameter is the number of milliseconds elapsed since January 1, 1970 00:00:00 GMT
-                    // where record.getInt(i) gets the number of days elapsed, and 86400000 is the number of milliseconds in a day.
-                    preparedStatement.setDate(i + 1, new Date(record.getInt(i) * 86400000l));
+                    preparedStatement.setDate(i + 1, new Date(record.getInt(i)));
                     break;
                 case Types.TIMESTAMP:
                     // record.getLong(spark sql TimestampType) gets us timestamp, and sql timestamp unit is ms
