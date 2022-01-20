f3c78963e4c0cf6efdb61d31c28a390620cd3f05
zhaowei <zw1020688@163.com>
2022-01-20 15:39:25 +0800

    test cicd

--- test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/cluster/v230/LastJoinTest.java
+++ test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/cluster/v230/LastJoinTest.java
@@ -40,18 +40,18 @@ public class LastJoinTest extends FedbTest {
     // public void testLastJoin(SQLCase testCase) throws Exception {
     //     ExecutorFactory.build(executor,testCase, SQLCaseType.kBatch).run();
     // }
-    // @Story("request")
-    // @Test(dataProvider = "getCase")
-    // @Yaml(filePaths = {"function/join/","function/cluster/window_and_lastjoin.yaml"})
-    // public void testLastJoinRequestMode(SQLCase testCase) throws Exception {
-    //     ExecutorFactory.build(executor,testCase, SQLCaseType.kRequest).run();
-    // }
-    @Story("requestWithSp")
+    @Story("request")
     @Test(dataProvider = "getCase")
     @Yaml(filePaths = {"function/join/","function/cluster/window_and_lastjoin.yaml"})
-    public void testLastJoinRequestModeWithSp(SQLCase testCase) throws Exception {
-        ExecutorFactory.build(executor,testCase, SQLCaseType.kRequestWithSp).run();
+    public void testLastJoinRequestMode(SQLCase testCase) throws Exception {
+        ExecutorFactory.build(executor,testCase, SQLCaseType.kRequest).run();
     }
+    // @Story("requestWithSp")
+    // @Test(dataProvider = "getCase")
+    // @Yaml(filePaths = {"function/join/","function/cluster/window_and_lastjoin.yaml"})
+    // public void testLastJoinRequestModeWithSp(SQLCase testCase) throws Exception {
+    //     ExecutorFactory.build(executor,testCase, SQLCaseType.kRequestWithSp).run();
+    // }
     // @Story("requestWithSpAysn")
     // @Test(dataProvider = "getCase")
     // @Yaml(filePaths = {"function/join/","function/cluster/window_and_lastjoin.yaml"})
