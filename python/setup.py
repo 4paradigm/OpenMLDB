d2ce2a37dbe79dee203bc38897eef5045ddb238c
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-24 16:28:54 +0800

    Add files via upload

---
+++ python/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='openmldb',
+    version='0.5.0a0',
+    author='OpenMLDB Team',
+    author_email=' ',
+    url='https://github.com/4paradigm/OpenMLDB',
+    description='OpenMLDB Python SDK',
+    license="copyright 4paradigm.com",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        ],
+    install_requires=[
+        "sqlalchemy < 1.4.0"
+    ],
+    include_package_data=True,
+    package_data = {'':['*.so']},
+    packages=find_packages(),
+    entry_points={
+        'sqlalchemy.dialects': [
+            'openmldb = sqlalchemy_openmldb.openmldb_dialect:OpenmldbDialect',
+        ],
+    },
+    zip_safe=False,
+)
