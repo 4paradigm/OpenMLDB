d349efec08d853b5a76145d3c92d4db1b3a17f64
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-26 17:14:14 +0800

    Update dbapi.py

--- python/openmldb/dbapi/dbapi.py
+++ python/openmldb/dbapi/dbapi.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+sys.path.append("../")
+
 import logging
 from ..sdk import sdk as sdk_module
 from ..native import sql_router_sdk
