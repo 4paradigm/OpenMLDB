d6ccbebc60df8f4d3dd18da350af041f8279d1af
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-27 10:17:02 +0800

    Update sql_smoke_test.py

--- python/openmldb/test/sql_smoke_test.py
+++ python/openmldb/test/sql_smoke_test.py
@@ -13,8 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 import sys
-sys.path.append("../")
+sys.path.append(os.path.dirname(__file__) + "/..")
 
 from sdk import sdk as sdk_module
 import case_conf
