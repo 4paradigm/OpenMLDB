6c1ce1e922cac39b36bc1f3fbc7a7727414371b1
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-27 14:50:19 +0800

    Update openmldb_dialect.py

--- python/openmldb/sqlalchemy_openmldb/openmldb_dialect.py
+++ python/openmldb/sqlalchemy_openmldb/openmldb_dialect.py
@@ -13,13 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# fmt:off
 import os
 import sys
 sys.path.append(os.path.dirname(__file__) + "/..")
-
 from sqlalchemy.engine import default
 from sqlalchemy import pool
 from sqlalchemy.sql import compiler
+# fmt:on
 
 try:
     from sqlalchemy.sql.compiler import SQLCompiler
