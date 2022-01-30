af2693ee28e433572352fcb7ce25746f7e144a93
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-01-30 11:21:16 +0800

    Update openmldb_client_test.py

--- python/openmldb/test/openmldb_client_test.py
+++ python/openmldb/test/openmldb_client_test.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import openmldb
+import pytest
 import unittest
 import logging
 import time
