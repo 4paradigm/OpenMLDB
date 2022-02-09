dd3ab9c848da212be281af482d052badcc385772
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2022-02-09 17:41:11 +0800

    Update sql_magic.py

--- python/openmldb/sql_magic/sql_magic.py
+++ python/openmldb/sql_magic/sql_magic.py
@@ -16,14 +16,13 @@
 from __future__ import print_function
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
-import openmldb
 import re
 @magics_class
 class SqlMagic(Magics):
 
-    def __init__(self, shell):
+    def __init__(self, shell, db):
         super(SqlMagic, self).__init__(shell)
-        db = openmldb.dbapi.connect('db_test','127.0.0.1:6181','/onebox')
+        self.db = db
         self.cursor = db.cursor()
         self.selectRE = re.compile("^select", re.I)
         self.createTableRE = re.compile("^create\s+table", re.I)
@@ -42,18 +41,8 @@ class SqlMagic(Magics):
             print(self.cursor.execute(sqlText).fetchone())
         else:
             self.cursor.execute(sqlText)
-            
-        if self.createDBRE.match(sqlText):
-            DB = sqlText.split()[-1].rstrip(";")
-            print(f'Sucessfully create database {DB}.') 
-        elif self.createTableRE.match(sqlText):
-            print('Sucessfully create table.')
-        elif self.insertRE.match(sqlText):
-            print('Insert successfully')
-        elif self.dropTable.match(sqlText):
-            print('Sucessfully drop table')
 
 def register():
     ip = get_ipython()
-    magics = SqlMagic(ip)
+    magics = SqlMagic(ip,db)
     ip.register_magics(magics)
