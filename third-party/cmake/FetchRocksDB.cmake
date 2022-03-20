aa444f5bdf93c0db8c9bc0fa04bb486a8d465198
aceforeverd <teapot@aceforeverd.com>
2022-03-20 17:59:04 +0800

    only apply rocksdb patch for latest mac

--- third-party/cmake/FetchRocksDB.cmake
+++ third-party/cmake/FetchRocksDB.cmake
@@ -15,6 +15,11 @@
 set(SRC_URL https://github.com/facebook/rocksdb/archive/v6.27.3.tar.gz)
 message(STATUS "build rocksdb from ${SRC_URL}")
 
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang" AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL "13.1.6")
+  message(STATUS "apply rocksdb patch for apple clang 13.1.6 or later")
+  set(PATCH_CMD patch -p 1 -N -i ${PROJECT_SOURCE_DIR}/patches/rocksdb.patch)
+endif()
+
 find_program(MAKE_EXE NAMES gmake nmake make REQUIRED)
 ExternalProject_Add(
   rocksdb
@@ -23,7 +28,7 @@ ExternalProject_Add(
   PREFIX ${DEPS_BUILD_DIR}
   DOWNLOAD_DIR ${DEPS_DOWNLOAD_DIR}/rocksdb
   INSTALL_DIR ${DEPS_INSTALL_DIR}
-  PATCH_COMMAND patch -p 1 -N -i ${PROJECT_SOURCE_DIR}/patches/rocksdb.patch
+  PATCH_COMMAND ${PATCH_CMD}
   CONFIGURE_COMMAND
     ${CMAKE_COMMAND} -H<SOURCE_DIR> -B<BINARY_DIR> -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     -DCMAKE_BUILD_TYPE=Release -DUSE_RTTI=ON -DROCKSDB_BUILD_SHARED=OFF -DWITH_TESTS=OFF -DWITH_TOOLS=OFF
