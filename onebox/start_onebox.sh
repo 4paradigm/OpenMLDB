4aa052e4c0d12c3b425d1a5bebef0cf42b15e143
“keyu813” <1121848065@qq.com>
2022-03-21 19:16:03 +0800

    onebox support standalone and add jave sdk test in standalone

--- onebox/start_onebox.sh
+++ onebox/start_onebox.sh
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+if [ $# -ne 1 ]; then
+    echo "./start_onebox.sh [cluster|standalone]"
+    exit 1
+fi
+
 set -x -e
 
 cd "$(dirname "$0")/../"
@@ -31,65 +36,88 @@ TABLET1=$IP:9520
 TABLET2=$IP:9521
 TABLET3=$IP:9522
 
-# start tablet0
-test -d tablet0-binlogs && rm -rf tablet0-binlogs
-test -d recycle_bin0 && rm -rf recycle_bin0
-./build/bin/openmldb --db_root_path=tablet0-binlogs \
-                   --recycle_bin_root_path=recycle_bin0 \
-                   --endpoint=${TABLET1} --role=tablet \
-                   --binlog_notify_on_put=true\
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --zk_keep_alive_check_interval=100000000\
-                   --zk_root_path=/onebox > tablet0.log 2>&1 &
-test -d tablet1-binlogs && rm -rf tablet1-binlogs
-test -d recycle_bin1 && rm -rf recycle_bin1
+OP=$1
+
+case $OP in
+    cluster)
+        # start tablet0
+        test -d tablet0-binlogs && rm -rf tablet0-binlogs
+        test -d recycle_bin0 && rm -rf recycle_bin0
+        ./build/bin/openmldb --db_root_path=tablet0-binlogs \
+                           --recycle_bin_root_path=recycle_bin0 \
+                           --endpoint=${TABLET1} --role=tablet \
+                           --binlog_notify_on_put=true\
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --zk_keep_alive_check_interval=100000000\
+                           --zk_root_path=/onebox > tablet0.log 2>&1 &
+        test -d tablet1-binlogs && rm -rf tablet1-binlogs
+        test -d recycle_bin1 && rm -rf recycle_bin1
 
 
-# start tablet1
-./build/bin/openmldb --db_root_path=tablet1-binlogs \
-                   --recycle_bin_root_path=recycle_bin1 \
-                   --endpoint=${TABLET2} --role=tablet \
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --binlog_notify_on_put=true\
-                   --zk_keep_alive_check_interval=100000000\
-                   --zk_root_path=/onebox > tablet1.log 2>&1 &
-test -d tablet2-binlogs && rm -rf tablet2-binlogs
-test -d recycle_bin2 && rm -rf recycle_bin2
+        # start tablet1
+        ./build/bin/openmldb --db_root_path=tablet1-binlogs \
+                           --recycle_bin_root_path=recycle_bin1 \
+                           --endpoint=${TABLET2} --role=tablet \
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --binlog_notify_on_put=true\
+                           --zk_keep_alive_check_interval=100000000\
+                           --zk_root_path=/onebox > tablet1.log 2>&1 &
+        test -d tablet2-binlogs && rm -rf tablet2-binlogs
+        test -d recycle_bin2 && rm -rf recycle_bin2
 
 
-# start tablet2
-./build/bin/openmldb --db_root_path=tablet2-binlogs \
-                   --recycle_bin_root_path=recycle_bin2 \
-                   --endpoint=${TABLET3} --role=tablet \
-                   --binlog_notify_on_put=true\
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --zk_keep_alive_check_interval=100000000\
-                   --zk_root_path=/onebox > tablet2.log 2>&1 &
+        # start tablet2
+        ./build/bin/openmldb --db_root_path=tablet2-binlogs \
+                           --recycle_bin_root_path=recycle_bin2 \
+                           --endpoint=${TABLET3} --role=tablet \
+                           --binlog_notify_on_put=true\
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --zk_keep_alive_check_interval=100000000\
+                           --zk_root_path=/onebox > tablet2.log 2>&1 &
 
-test -d recycle_bin3 && rm -rf recycle_bin3
+        test -d recycle_bin3 && rm -rf recycle_bin3
 
-# start ns1 
-./build/bin/openmldb --endpoint=${NS1} --role=nameserver \
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --tablet_offline_check_interval=1\
-                   --tablet_heartbeat_timeout=1\
-                   --zk_root_path=/onebox > ns1.log 2>&1 &
+        # start ns1
+        ./build/bin/openmldb --endpoint=${NS1} --role=nameserver \
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --tablet_offline_check_interval=1\
+                           --tablet_heartbeat_timeout=1\
+                           --zk_root_path=/onebox > ns1.log 2>&1 &
 
-# start ns2 
-./build/bin/openmldb --endpoint=${NS2} --role=nameserver \
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --tablet_offline_check_interval=1\
-                   --tablet_heartbeat_timeout=1\
-                   --zk_root_path=/onebox > ns2.log 2>&1 &
+        # start ns2
+        ./build/bin/openmldb --endpoint=${NS2} --role=nameserver \
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --tablet_offline_check_interval=1\
+                           --tablet_heartbeat_timeout=1\
+                           --zk_root_path=/onebox > ns2.log 2>&1 &
 
-# start ns3 
-./build/bin/openmldb --endpoint=${NS3} --role=nameserver \
-                   --tablet_offline_check_interval=1\
-                   --tablet_heartbeat_timeout=1\
-                   --zk_cluster=${ZK_CLUSTER}\
-                   --zk_root_path=/onebox > ns3.log 2>&1 &
+        # start ns3
+        ./build/bin/openmldb --endpoint=${NS3} --role=nameserver \
+                           --tablet_offline_check_interval=1\
+                           --tablet_heartbeat_timeout=1\
+                           --zk_cluster=${ZK_CLUSTER}\
+                           --zk_root_path=/onebox > ns3.log 2>&1 &
+        sleep 3
+        echo "cluster start ok"
+        ;;
+    standalone)
+        # start tablet
+        test -d tablet0-binlogs && rm -rf tablet0-binlogs
+        test -d recycle_bin0 && rm -rf recycle_bin0
+        ./build/bin/openmldb --db_root_path=tablet0-binlogs \
+                           --recycle_bin_root_path=recycle_bin0 \
+                           --endpoint=${TABLET1} --role=tablet \
+                           --binlog_notify_on_put=true > tablet0.log 2>&1 &
 
-sleep 3
-
-echo "start all ok"
+        # start ns
+        ./build/bin/openmldb --endpoint=${NS1} --role=nameserver \
+                           --tablet=${TABLET1} \
+                           --tablet_offline_check_interval=1\
+                           --tablet_heartbeat_timeout=1 > ns1.log 2>&1 &
+        sleep 3
+        echo "standalone start ok"
+        ;;
+    *)
+        echo "only support standalone|cluster" >&2
+esac
 
