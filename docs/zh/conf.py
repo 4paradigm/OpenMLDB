a2c12442ecb19efc13fa7d12215583b55d6db3ba
Mian Lu <lumianph@gmail.com>
2022-03-31 23:35:11 +0800

    change version name from dev to main

--- docs/zh/conf.py
+++ docs/zh/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, OpenMLDB'
 author = 'OpenMLDB'
 
 # The full version, including alpha/beta/rc tags
-version = 'dev'
+version = 'main'
 
 
 # -- General configuration ---------------------------------------------------
