b7eece62213b1ca5b99b238473c0a078da83992f
dl239 <dl239@126.com>
2022-03-31 17:05:32 +0800

    fix: fix IsClusterMode judgement

--- src/sdk/sql_cluster_router.cc
+++ src/sdk/sql_cluster_router.cc
@@ -2187,7 +2187,7 @@ std::shared_ptr<hybridse::sdk::ResultSet> SQLClusterRouter::ExecuteSQL(const std
         }
         case hybridse::node::kPlanTypeFuncDef:
         case hybridse::node::kPlanTypeQuery: {
-            if (cluster_sdk_->IsClusterMode() || IsOnlineMode()) {
+            if (!cluster_sdk_->IsClusterMode() || IsOnlineMode()) {
                 // Run online query
                 return ExecuteSQLParameterized(db, sql, std::shared_ptr<openmldb::sdk::SQLRequestRow>(), status);
             } else {
@@ -2210,7 +2210,7 @@ std::shared_ptr<hybridse::sdk::ResultSet> SQLClusterRouter::ExecuteSQL(const std
             return {};
         }
         case hybridse::node::kPlanTypeSelectInto: {
-            if (cluster_sdk_->IsClusterMode() || IsOnlineMode()) {
+            if (!cluster_sdk_->IsClusterMode() || IsOnlineMode()) {
                 auto* select_into_plan_node = dynamic_cast<hybridse::node::SelectIntoPlanNode*>(node);
                 const std::string& query_sql = select_into_plan_node->QueryStr();
                 auto rs = ExecuteSQLParameterized(db, query_sql,
