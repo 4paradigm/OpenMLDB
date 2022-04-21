732270ddec7b0e8b63fd062fadd5e8901c8085ab
hezhaozhao <hezz@akulaku.com>
2022-04-21 15:43:13 +0800

    change input format

--- hybridse/src/codegen/udf_ir_builder_test.cc
+++ hybridse/src/codegen/udf_ir_builder_test.cc
@@ -649,7 +649,7 @@ TEST_F(UdfIRBuilderTest, lower_lcase) {
 TEST_F(UdfIRBuilderTest, get_json_object) {
     auto udf_name = "get_json_object";
     CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"("c1")"), StringRef(R"({"a": {"c": "c1"}})"),StringRef("$.a.c"));
-//    CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"(["b2"])"), StringRef(R"([{"b": null}, {"b": "b2"}])"),StringRef("$.[*].b"));
+    CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"(["b2"])"), StringRef(R"([{"b": null}, {"b": "b2"}])"),StringRef("$.[*].b"));
 //    CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"(["b1","b2"])"), StringRef(R"([{"b": "b1"}, {"b": "b2"}])"),StringRef("$.[*].b"));
 //    CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"(["c1","c2"])"), StringRef(R"([{"b": {"c": "c1"}}, {"b": {"c": "c2"}}])"),StringRef("$.[*].b.c"));
 //    CheckUdf<Nullable<StringRef>, Nullable<StringRef>, Nullable<StringRef>>(udf_name, StringRef(R"([{"b":"b1"},{"b":"b2"}])"), StringRef(R"({"ab": [{"b": "b1"}, {"b": "b2"}]})"),StringRef("$.ab[*]"));
