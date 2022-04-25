b7a885dc36a4030befbe775ea95143be2d33ec85
ZHANG Hao <zhanghao@4paradigm.com>
2022-04-25 21:32:12 +0800

    fix t1.col style long window op

--- src/cmd/sql_cmd_test.cc
+++ src/cmd/sql_cmd_test.cc
@@ -565,7 +565,7 @@ TEST_P(DBSDKTest, DeployLongWindowsExecuteSum) {
         " sum(t_col) over w1 as w1_sum_t_col,"
         " sum(col3) over w2 as w2_sum_col3"
         " from " + base_table +
-        " WINDOW w1 AS (PARTITION BY col1,col2 ORDER BY col3"
+        " WINDOW w1 AS (PARTITION BY " + base_table + ".col1," + base_table + ".col2 ORDER BY col3"
         " ROWS_RANGE BETWEEN 5 PRECEDING AND CURRENT ROW), "
         " w2 AS (PARTITION BY col1,col2 ORDER BY i64_col"
         " ROWS BETWEEN 6 PRECEDING AND CURRENT ROW);";
