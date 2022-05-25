0d25e4e206a43a6b40649b6d18be6b2e876bd67c
Wu Wenhan <1020645987@qq.com>
2022-05-26 02:46:53 +0800

    Update WindowAggPlan.scala

--- java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/nodes/WindowAggPlan.scala
+++ java/openmldb-batch/src/main/scala/com/_4paradigm/openmldb/batch/nodes/WindowAggPlan.scala
@@ -733,7 +733,7 @@ object WindowAggPlan {
   def isValidOrder(key: java.lang.Long): Boolean = {
     // TODO: Ignore the null value, maybe handle null in the future
     if (key == null) {
-      false
+      isValodOrder(0)
     } else {
       key >= 0
     }
