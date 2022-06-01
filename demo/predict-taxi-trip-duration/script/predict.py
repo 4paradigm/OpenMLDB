7ab0df994c3424642b4eae1ca21dd039e4fabda7
mango <xu.weiKyrie@foxmail.com>
2022-06-01 13:21:36 +0800

    fix some lint error
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

--- demo/predict-taxi-trip-duration/script/predict.py
+++ demo/predict-taxi-trip-duration/script/predict.py
@@ -17,16 +17,16 @@
 import requests
 
 url = "http://127.0.0.1:8887/predict"
-req ={"id":"id0376262",
-	"vendor_id":1,
-	"pickup_datetime":1467302350000,
-	"dropoff_datetime":1467304896000,
-	"passenger_count":2,
-	"pickup_longitude":-73.873093,
-	"pickup_latitude":40.774097,
-	"dropoff_longitude":-73.926704,
-	"dropoff_latitude":40.856739,
-	"store_and_fwd_flag":"N",
-	"trip_duration":1}
+req = {"id": "id0376262",
+       "vendor_id": 1,
+       "pickup_datetime": 1467302350000,
+       "dropoff_datetime": 1467304896000,
+       "passenger_count": 2,
+       "pickup_longitude": -73.873093,
+       "pickup_latitude": 40.774097,
+       "dropoff_longitude": -73.926704,
+       "dropoff_latitude": 40.856739,
+       "store_and_fwd_flag": "N",
+       "trip_duration": 1}
 r = requests.post(url, json=req)
 print(r.text)
