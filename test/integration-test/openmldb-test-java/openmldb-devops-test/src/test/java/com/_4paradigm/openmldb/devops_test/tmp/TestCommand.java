358a0f7173f40ee701cfe577f6318a82dac3d055
zhaowei <zw1020688@163.com>
2022-07-07 18:56:36 +0800

    add ns operator

--- test/integration-test/openmldb-test-java/openmldb-devops-test/src/test/java/com/_4paradigm/openmldb/devops_test/tmp/TestCommand.java
+++ test/integration-test/openmldb-test-java/openmldb-devops-test/src/test/java/com/_4paradigm/openmldb/devops_test/tmp/TestCommand.java
@@ -11,4 +11,9 @@ public class TestCommand {
         List<String> list = ExecutorUtil.run("/Users/zhaowei/openmldb-auto-test/tmp_mac/openmldb-ns-1/bin/openmldb --zk_cluster=127.0.0.1:30000 --zk_root_path=/openmldb --role=ns_client --interactive=false --database=test_devops4 --cmd='showopstatus'");
         list.forEach(System.out::println);
     }
+    @Test
+    public void test2(){
+        List<String> list = ExecutorUtil.run("/home/zhaowei01/openmldb-auto-test/tmp/openmldb-ns-1/bin/openmldb --zk_cluster=172.24.4.55:30000 --zk_root_path=/openmldb --role=ns_client --interactive=false --database=test_devops4 --cmd='showopstatus'");
+        list.forEach(System.out::println);
+    }
 }
