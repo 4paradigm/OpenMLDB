00e99f9bd436f4f46893f7822db07a7604aba4bd
zhaowei <zw1020688@163.com>
2022-07-13 08:20:55 +0800

    add ns operator

--- test/integration-test/openmldb-test-java/openmldb-devops-test/src/test/java/com/_4paradigm/openmldb/devops_test/tmp/TestTmp.java
+++ test/integration-test/openmldb-test-java/openmldb-devops-test/src/test/java/com/_4paradigm/openmldb/devops_test/tmp/TestTmp.java
@@ -1,24 +1,19 @@
-package com._4paradigm.openmldb.devops_test.high_availability;
+package com._4paradigm.openmldb.devops_test.tmp;
 
 import com._4paradigm.openmldb.devops_test.common.ClusterTest;
 import com._4paradigm.openmldb.test_common.bean.OpenMLDBResult;
 import com._4paradigm.openmldb.test_common.openmldb.*;
-import com._4paradigm.openmldb.test_common.util.SDKByJDBCUtil;
-import com._4paradigm.openmldb.test_common.util.SDKUtil;
 import com._4paradigm.qa.openmldb_deploy.util.Tool;
-import com._4paradigm.test_tool.command_tool.common.ExecutorUtil;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.RandomUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.collections.Lists;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestCluster extends ClusterTest {
+public class TestTmp extends ClusterTest {
     private String dbName;
     private SDKClient sdkClient;
     private NsClient nsClient;
@@ -82,42 +77,42 @@ public class TestCluster extends ClusterTest {
         sdkClient.insertList(ssdTable,dataList);
         sdkClient.insertList(hddTable,dataList);
         // 其中一个tablet stop，leader 内存表和磁盘表可以正常访问，flower 内存表和磁盘表可以正常访问。
-        openMLDBDevops.operateTablet(0,"stop");
-        String oneTabletStopMsg = "tablet1 stop table row count check failed.";
-        Assert.assertEquals(sdkClient.getTableRowCount(memoryTable),dataCount,oneTabletStopMsg);
-        Assert.assertEquals(sdkClient.getTableRowCount(ssdTable),dataCount,oneTabletStopMsg);
-        Assert.assertEquals(sdkClient.getTableRowCount(hddTable),dataCount,oneTabletStopMsg);
-        // tablet start，数据可以回复，要看磁盘表和内存表。
-        openMLDBDevops.operateTablet(0,"start");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,10);
-        //创建磁盘表和内存表，在重启tablet，数据可回复，内存表和磁盘表可以正常访问。
-        openMLDBDevops.operateTablet(0,"restart");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+10,10);
-        //创建磁盘表和内存表，插入一些数据，然后make snapshot，在重启tablet，数据可回复。
-        nsClient.makeSnapshot(dbName,memoryTable);
-        nsClient.makeSnapshot(dbName,ssdTable);
-        nsClient.makeSnapshot(dbName,hddTable);
-        //tablet 依次restart，数据可回复，可以访问。
-        openMLDBDevops.operateTablet("restart");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+20,10);
+//        openMLDBDevops.operateTablet(0,"stop");
+//        String oneTabletStopMsg = "tablet1 stop table row count check failed.";
+//        Assert.assertEquals(sdkClient.getTableRowCount(memoryTable),dataCount,oneTabletStopMsg);
+//        Assert.assertEquals(sdkClient.getTableRowCount(ssdTable),dataCount,oneTabletStopMsg);
+//        Assert.assertEquals(sdkClient.getTableRowCount(hddTable),dataCount,oneTabletStopMsg);
+//        // tablet start，数据可以回复，要看磁盘表和内存表。
+//        openMLDBDevops.operateTablet(0,"start");
+//        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,10);
+//        //创建磁盘表和内存表，在重启tablet，数据可回复，内存表和磁盘表可以正常访问。
+//        openMLDBDevops.operateTablet(0,"restart");
+//        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+10,10);
+//        //创建磁盘表和内存表，插入一些数据，然后make snapshot，在重启tablet，数据可回复。
+//        nsClient.makeSnapshot(dbName,memoryTable);
+//        nsClient.makeSnapshot(dbName,ssdTable);
+//        nsClient.makeSnapshot(dbName,hddTable);
+//        //tablet 依次restart，数据可回复，可以访问。
+//        openMLDBDevops.operateTablet("restart");
+//        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+20,10);
         //1个ns stop，可以正常访问。
         openMLDBDevops.operateNs(0,"stop");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+30,0);
+        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,0);
         // 1个ns start 可以访问。
         openMLDBDevops.operateNs(0,"start");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+30,0);
+        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,0);
         // 1个ns restart 可以访问。
         openMLDBDevops.operateNs(0,"restart");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+30,0);
+        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,0);
         // 单zk stop 在start后 可以访问
         openMLDBDevops.operateZKOne("stop");
         Tool.sleep(3000);
         openMLDBDevops.operateZKOne("start");
         Tool.sleep(3000);
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+30,0);
+        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,0);
         // 单zk restart 后可以访问
         openMLDBDevops.operateZKOne("restart");
-        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount+30,0);
+        addDataCheck(sdkClient,nsClient,dbName,Lists.newArrayList(memoryTable,ssdTable,hddTable),dataCount,0);
         //3个tablet stop，不能访问。
         openMLDBDevops.operateTablet("stop");
         OpenMLDBResult openMLDBResult = sdkClient.execute(String.format("select * from %s",memoryTable));
