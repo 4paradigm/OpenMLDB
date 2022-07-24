2efd575cce43725407a85e7f23c6697345eb264f
qsliu <qsliu2017@outlook.com>
2022-07-25 02:32:37 +0800

    simple impl

--- go/driver.go
+++ go/driver.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"database/sql"
 	interfaces "database/sql/driver"
+	"fmt"
+	"net/url"
+	"strings"
 )
 
 func init() {
@@ -12,29 +15,67 @@ func init() {
 
 var (
 	_ interfaces.Driver        = (*driver)(nil)
-	_ interfaces.Connector     = (*driver)(nil)
 	_ interfaces.DriverContext = (*driver)(nil)
+
+	_ interfaces.Connector = (*connecter)(nil)
 )
 
-type driver struct {
+type driver struct{}
+
+func parseDsn(dsn string) (host string, db string, err error) {
+	u, err := url.Parse(dsn)
+	if err != nil {
+		return "", "", fmt.Errorf("invlaid URL: %w", err)
+	}
+
+	if u.Scheme != "openmldb" && u.Scheme != "" {
+		return "", "", fmt.Errorf("invalid URL: unknown schema '%s'", u.Scheme)
+	}
+
+	p := strings.Split(u.Path, "/")
+	if len(p) == 0 {
+		return "", "", fmt.Errorf("invalid URL: DB name not found")
+	}
+
+	return u.Host, p[0], nil
 }
 
 // Open implements driver.Driver.
-func (d *driver) Open(name string) (interfaces.Conn, error) {
-	return &conn{}, nil
+func (driver) Open(name string) (interfaces.Conn, error) {
+	// name should be the URL of the api server, e.g. openmldb://localhost:6543/db
+	host, db, err := parseDsn(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &conn{host: host, db: db, closed: false}, nil
+}
+
+type connecter struct {
+	host string
+	db   string
 }
 
 // Connect implements driver.Connector.
-func (d *driver) Connect(ctx context.Context) (interfaces.Conn, error) {
-	return &conn{}, nil
+func (c connecter) Connect(ctx context.Context) (interfaces.Conn, error) {
+	conn := &conn{host: c.host, db: c.db, closed: false}
+	if err := conn.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
 
 // Driver implements driver.Connector.
-func (d *driver) Driver() interfaces.Driver {
-	return d
+func (connecter) Driver() interfaces.Driver {
+	return &driver{}
 }
 
 // OpenConnector implements driver.DriverContext.
-func (d *driver) OpenConnector(name string) (interfaces.Connector, error) {
-	return d, nil
+func (driver) OpenConnector(name string) (interfaces.Connector, error) {
+	host, db, err := parseDsn(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &connecter{host, db}, nil
 }
