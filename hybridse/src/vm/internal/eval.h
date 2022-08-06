1ca0bbf0a3fff067500c8968a6909c8f20bc5907
aceforeverd <teapot@aceforeverd.com>
2022-08-06 15:43:28 +0000

    fix: compile on mac

--- hybridse/src/vm/internal/eval.h
+++ hybridse/src/vm/internal/eval.h
@@ -25,8 +25,8 @@
 #ifndef HYBRIDSE_SRC_VM_INTERNAL_EVAL_H_
 #define HYBRIDSE_SRC_VM_INTERNAL_EVAL_H_
 
-#include <string>
 #include <ostream>
+#include <string>
 
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
@@ -102,6 +102,15 @@ absl::StatusOr<std::optional<T>> ExtractValue(const RowParser* parser, const cod
 }
 
 template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::optional<T>& val) {
+    if constexpr (std::is_same_v<std::string, T>) {
+        return os << (val.has_value() ? absl::StrCat("\"", val.value(), "\"") : "NULL");
+    } else {
+        return os << (val.has_value() ? std::to_string(val.value()) : "NULL");
+    }
+}
+
+template <typename T>
 std::optional<bool> EvalSimpleBinaryExpr(node::FnOperator op, const std::optional<T>& lhs,
                                          const std::optional<T>& rhs) {
     DLOG(INFO) << "[EvalSimpleBinaryExpr] " << lhs << " " << node::ExprOpTypeName(op) << " " << rhs;
@@ -128,15 +137,6 @@ std::optional<bool> EvalSimpleBinaryExpr(node::FnOperator op, const std::optiona
     }
 
     return std::nullopt;
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::optional<T>& val) {
-    if constexpr (std::is_same_v<std::string, T>) {
-        return os << (val.has_value() ? absl::StrCat("\"", val.value(), "\"") : "NULL");
-    } else {
-        return os << (val.has_value() ? std::to_string(val.value()) : "NULL");
-    }
 }
 
 template <typename T>
