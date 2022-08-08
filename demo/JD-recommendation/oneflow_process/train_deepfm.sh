e105b77eddf6b36409d030038c757f3e4dd00565
siqi <wangsiqi@4paradigm.com>
2022-08-08 09:54:36 +0800

    Update shell script

--- demo/JD-recommendation/oneflow_process/train_deepfm.sh
+++ demo/JD-recommendation/oneflow_process/train_deepfm.sh
@@ -13,8 +13,8 @@ python3 -m oneflow.distributed.launch \
     --master_addr 127.0.0.1 \
     deepfm_train_eval_JD.py \
       --disable_fusedmlp \
-      --data_dir $DATA_DIR \
-      --persistent_path $PERSISTENT_PATH \
+      --data_dir "$DATA_DIR" \
+      --persistent_path "$PERSISTENT_PATH" \
       --table_size_array "11,42,1105,200,11,1295,1,1,5,3,23,23,7,5042381,3127923,5042381,3649642,28350,105180,7,2,5042381,5,4,4,41,2,2,8,3456,4,5,5042381,10,60,5042381,843,17,1276,101,100" \
       --store_type 'cached_host_mem' \
       --cache_memory_budget_mb 1024 \
@@ -28,8 +28,8 @@ python3 -m oneflow.distributed.launch \
       --num_train_samples 4007924 \
       --num_val_samples 504398 \
       --num_test_samples 530059 \
-      --model_save_dir $MODEL_SAVE_DIR \
+      --model_save_dir "$MODEL_SAVE_DIR" \
       --save_best_model \
       --save_graph_for_serving \
-      --model_serving_path $MODEL_SERVING_PATH \
+      --model_serving_path "$MODEL_SERVING_PATH" \
       --save_model_after_each_eval 
