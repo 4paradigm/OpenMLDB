b9a1aac435cde31f739b4bc1d7ef8ac1123d3d4b
longxu0509 <108991596+longxu0509@users.noreply.github.com>
2022-09-02 11:57:56 +0800

    fix cppLint error
    fix cppLint error

--- hybridse/examples/toydb/src/storage/concurrentlist_test.cpp
+++ hybridse/examples/toydb/src/storage/concurrentlist_test.cpp
@@ -1,7 +1,4 @@
 
-#include <sys/time.h>
-#include <time.h>
-
 #include "gtest/gtest.h"
 #include "skiplist.h"
 #include "ConcurrentList.h"
@@ -10,47 +7,47 @@
 
 
 namespace hybridse {
-    namespace storage {
+namespace storage {
 
-        using ::hybridse::base::DefaultComparator;
-        DefaultComparator cmp;
+using ::hybridse::base::DefaultComparator;
+DefaultComparator cmp;
 
-        TEST(ConcurrentListTest, ShouldPushNewValue) {
-            ConcurrentList<uint64_t, uint64_t, DefaultComparator> list(cmp);
-            std::vector<std::thread> threads_vec(4);
-            for (int i = 0; i < 4; ++i) {
-                int start = 10000 * i, end = 10000 * (i + 1);
-                threads_vec[i] = std::thread([&list = list, start, end]() {
-                    for (int i = start; i < end; ++i) list.Insert(i, 5);
-                });
-            }
-            for (auto &t: threads_vec) {
-                t.join();
-            }
-            std::vector<int> values;
-            for (auto it = list.begin(); it != list.end(); ++it) {
-                values.push_back(it.GetValue());
+TEST(ConcurrentListTest, ShouldPushNewValue) {
+    ConcurrentList<uint64_t, uint64_t, DefaultComparator> list(cmp);
+    std::vector<std::thread> threads_vec(4);
+    for (int i = 0; i < 4; ++i) {
+        int start = 10000 * i, end = 10000 * (i + 1);
+        threads_vec[i] = std::thread([&list = list, start, end]() {
+            for (int i = start; i < end; ++i) list.Insert(i, 5);
+        });
+    }
+    for (auto &t: threads_vec) {
+        t.join();
+    }
+    std::vector<int> values;
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        values.push_back(it.GetValue());
 
-            }
-            std::cout << values.size() << std::endl;
-            ASSERT_EQ(values.size(), 40001);
-        }
+    }
+    std::cout << values.size() << std::endl;
+    ASSERT_EQ(values.size(), 40001);
+}
 
-        TEST(ConcurrentListTest, GC) {
-            ConcurrentList<int, uint64_t, DefaultComparator> list(cmp);
-            for (int i = 0; i < 100000; i++) {
-                list.Insert(i, i+5);
-            }
-            list.GC();
-            for (auto it = list.begin(); it != list.end(); ++it) {
-                std::cout << it.GetKey() << " : " << it.GetValue() << std::endl;
-            }
-            ASSERT_EQ(list.GetSize(), 1001);
-        }
+TEST(ConcurrentListTest, GC) {
+    ConcurrentList<int, uint64_t, DefaultComparator> list(cmp);
+    for (int i = 0; i < 100000; i++) {
+        list.Insert(i, i+5);
+    }
+    list.GC();
+    for (auto it = list.begin(); it != list.end(); ++it) {
+        std::cout << it.GetKey() << " : " << it.GetValue() << std::endl;
+    }
+    ASSERT_EQ(list.GetSize(), 1001);
+}
 
-    }  // namespace storage
-}  // namespace hybridse
-//
+}
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
