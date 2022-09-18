6c8c7ba16c5005c0b0f483053e428f2fef88a903
qsliu <qsliu2017@outlook.com>
2022-09-18 13:25:01 +0800

    simple impl

--- go/conn.go
+++ go/conn.go
@@ -1,8 +1,12 @@
 package openmldb
 
 import (
+	"bytes"
 	"context"
 	interfaces "database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 var (
@@ -23,6 +27,57 @@ var (
 )
 
 type conn struct {
+	host   string // host or host:port
+	db     string // database name
+	closed bool
+}
+
+type GeneralResp struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+type QueryReq struct {
+	Mode string `json:"mode"`
+	SQL  string `json:"sql"`
+}
+
+func (c *conn) query(ctx context.Context, sql string) (err error) {
+	// TODO: implement query entry return result
+	if c.closed {
+		return interfaces.ErrBadConn
+	}
+
+	reqBody, _ := json.Marshal(&QueryReq{
+		Mode: "offsync",
+		SQL:  sql,
+	})
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		fmt.Sprintf("http://%s/dbs/%s", c.host, c.db),
+		bytes.NewBuffer(reqBody),
+	)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var r GeneralResp
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		return fmt.Errorf("unexpected response: %w", err)
+	}
+
+	if r.Code != 0 {
+		return fmt.Errorf("conn error: %s", r.Msg)
+	}
+
+	return nil
 }
 
 // Prepare implements driver.Conn.
@@ -32,6 +87,7 @@ func (c *conn) Prepare(query string) (interfaces.Stmt, error) {
 
 // Close implements driver.Conn.
 func (c *conn) Close() error {
+	c.closed = true
 	return nil
 }
 
@@ -42,7 +98,7 @@ func (c *conn) Begin() (interfaces.Tx, error) {
 
 // Ping implements driver.Pinger.
 func (c *conn) Ping(ctx context.Context) error {
-	return nil
+	return c.query(ctx, "SELECT 1")
 }
 
 // ResetSession implements driver.SessionResetter.
@@ -56,15 +112,15 @@ func (c *conn) ResetSession(ctx context.Context) error {
 //
 // Before a connection is returned to the connection pool after use, IsValid is called.
 func (c *conn) IsValid() bool {
-	return true
+	return !c.closed
 }
 
 // ExecContext implements driver.ExecerContext.
 func (c *conn) ExecContext(ctx context.Context, query string, args []interfaces.NamedValue) (interfaces.Result, error) {
-	return &result{}, nil
+	return &result{}, c.query(ctx, query)
 }
 
 // QueryContext implements driver.QueryerContext.
 func (c *conn) QueryContext(ctx context.Context, query string, args []interfaces.NamedValue) (interfaces.Rows, error) {
-	return &rows{}, nil
+	return &rows{}, c.query(ctx, query)
 }
