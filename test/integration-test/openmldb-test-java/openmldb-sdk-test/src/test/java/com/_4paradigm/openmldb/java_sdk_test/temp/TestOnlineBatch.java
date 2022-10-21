26a1dfc1d1938552082cf0431cf78097c046894c
zhaowei <zw1020688@163.com>
2022-10-21 22:46:41 +0800

    test batch

--- test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/temp/TestOnlineBatch.java
+++ test/integration-test/openmldb-test-java/openmldb-sdk-test/src/test/java/com/_4paradigm/openmldb/java_sdk_test/temp/TestOnlineBatch.java
@@ -11,16 +11,16 @@ public class TestOnlineBatch extends OpenMLDBTest {
         SDKClient sdkClient = SDKClient.of(executor);
         sdkClient.createAndUseDB("test_batch");
         sdkClient.setOnline();
-//        sdkClient.execute("create table t1(\n" +
-//                "id int,\n" +
-//                "c1 string,\n" +
-//                "c3 int,\n" +
-//                "c4 bigint,\n" +
-//                "c5 float,\n" +
-//                "c6 double,\n" +
-//                "c7 timestamp,\n" +
-//                "c8 date,\n" +
-//                "index(key=(c1),ts=c7))options(partitionnum=2,replicanum=1);");
+        sdkClient.execute("create table t1(\n" +
+                "id int,\n" +
+                "c1 string,\n" +
+                "c3 int,\n" +
+                "c4 bigint,\n" +
+                "c5 float,\n" +
+                "c6 double,\n" +
+                "c7 timestamp,\n" +
+                "c8 date,\n" +
+                "index(key=(c1),ts=c7))options(partitionnum=2,replicanum=1);");
         for (int i=1;i<=100000;i++){
             int tmp = i%10;
             sdkClient.execute(String.format("insert into %s values(1,'aa%d',%d,30,1.1,2.1,%d,'2020-05-01');","t1",tmp,i,i));
