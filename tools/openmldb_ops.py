8981ba72385e7af6d837f75f3b27a276886a5319
dl239 <dl239@126.com>
2022-10-28 10:53:44 +0800

    feat: recover data

---
+++ tools/openmldb_ops.py
@@ -0,0 +1,168 @@
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import logging
+log = logging.getLogger(__name__)
+from tool import *
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option("--openmldb_bin_path",
+                  dest="openmldb_bin_path",
+                  help="the openmldb bin path")
+
+parser.add_option("--zk_cluster", 
+                  dest="zk_cluster",
+                  help="the zookeeper cluster")
+
+parser.add_option("--zk_root_path",
+                  dest="zk_root_path",
+                  help="the zookeeper root path")
+
+parser.add_option("--cmd",
+                  dest="cmd",
+                  help="cmd")
+
+
+INTERNAL_DB = ["__INTERNAL_DB", "__PRE_AGG_DB", "INFORMATION_SCHEMA"]
+
+def CheckTable(executor, db, table_name):
+    status, table_partitions = executor.GetTablePartition(db, table_name)
+    if not status.OK():
+        return status
+    endpoints = set()
+    for pid, partitions in table_partitions.items():
+        for partition in partitions:
+            endpoints.add(partition.GetEndpoint())
+            if not partition.IsAlive():
+                return Status(-1, f"partition is not alive. {db} {table_name} {pid} {partition.GetEndpoint()}")
+    endpoint_status = {}
+    for endpoint in endpoints:
+        status, result = executor.GetTableStatus(endpoint)
+        if not status.OK():
+            return status
+        endpoint_status[endpoint] = result
+    for pid, partitions in table_partitions.items():
+        for partition in partitions:
+            endpoint = partition.GetEndpoint()
+            key = "{}_{}".format(partition.GetTid(), pid)
+            if endpoint not in endpoint_status or key not in endpoint_status[endpoint]:
+                return Status(-1, f"not table partition in {endpint}")
+            if partition.IsLeader() and endpoint_status[endpoint][key][3] == "kTableLeader":
+                continue
+            elif not partition.IsLeader() and endpoint_status[endpoint][key][3] != "kTableLeader":
+                continue
+            return Status(-1, f"role is not match")
+    return Status()
+
+def RecoverPartition(executor, db, partitions, endpoint_status):
+    leader_pos = -1
+    max_offset = 0
+    table_name = partitions[0].GetName()
+    pid = partitions[0].GetPid()
+    for pos in range(len(partitions)):
+        partition = partitions[pos]
+        if partition.IsLeader() and partition.GetOffset() >= max_offset:
+            leader_pos = pos
+    if leader_pos < 0:
+        log.error(f"cannot find leader partition. db {db} name {table_name} partition {pid}")
+        return Status(-1, "recover partition failed")
+    tid = partitions[0].GetTid()
+    leader_endpoint = partitions[leader_pos].GetEndpoint()
+    # recover leader
+    if f"{tid}_{pid}" not in endpoint_status[leader_endpoint]:
+        log.info(f"leader partition is not in tablet, db {db} name {table_name} pid {pid} endpoint {leader_endpoint}. start loading data...")
+        if not executor.LoadTable(leader_endpoint, table_name, tid, pid).OK():
+            log.error(f"load table failed. db {db} name {table_name} pid {pid} endpoint {leader_endpoint}")
+            return Status(-1, "recover partition failed")
+    if not partitions[leader_pos].IsAlive():
+        if not executor.UpdateTableAlive(db, table_name, pid, leader_endpoint, True).OK():
+            log.error(f"update leader alive failed. db {db} name {table_name} pid {pid} endpoint {leader_endpoint}")
+            return Status(-1, "recover partition failed")
+    # recover follower
+    for pos in range(len(partitions)):
+        if pos == leader_pos:
+            continue
+        partition = partitions[pos]
+        endpoint = partition.GetEndpoint()
+        if partitions[leader_pos].IsAlive():
+            if not executor.UpdateTableAlive(db, table_name, pid, endpoint, False).OK():
+                log.error(f"update leader alive failed. db {db} name {table_name} pid {pid} endpoint {endpoint}")
+                return Status(-1, "recover partition failed")
+        if not executor.RecoverTablePartition(db, table_name, pid, endpoint).OK():
+            log.error(f"recover table partition failed. db {db} name {table_name} pid {pid} endpoint {endpoint}")
+            return Status(-1, "recover table partition failed")
+
+def RecoverTable(executor, db, table_name):
+    if CheckTable(executor, db, table_name).OK():
+        log.info(f"{table_name} in {db} is healthy")
+        return
+    log.info(f"recover {table_name} in {db}")
+    status, table_info = executor.GetTableInfo(db, table_name)
+    if not status.OK():
+        log.warn(f"get table info failed. msg is {status.GetMsg()}")
+        return
+    partition_dict = executor.ParseTableInfo(table_info)
+    endpoints = set()
+    for record in table_info:
+        endpoints.add(record[3])
+    endpoint_status = {}
+    for endpoint in endpoints:
+        status, result = executor.GetTableStatus(endpoint)
+        if not status.OK():
+            log.warn(f"get table status failed. msg is {status.GetMsg()}")
+            return
+        endpoint_status[endpoint] = result
+    # print(endpoint_status)
+    tid = int(table_info[-1][1])
+    max_pid = int(table_info[-1][2])
+    for pid in range(max_pid + 1):
+        RecoverPartition(executor, db, partition_dict[str(pid)], endpoint_status)
+    # wait op
+    while True:
+        status, result = executor.ShowOpStatus(db, table_name)
+        is_finish = True
+        if status.OK():
+            for record in result:
+                if record[4] == 'kDoing':
+                    is_finish = False
+                    break
+        if not is_finish:
+            log.info(f"waiting task")
+            sleep(2)
+        else:
+            break
+    status = CheckTable(executor, db, table_name)
+    if status.OK():
+        log.info(f"{table_name} in {db} recover success")
+    else:
+        log.warn(status.GetMsg())
+
+def RecoverData(executor):
+    status, dbs = executor.GetAllDatabase()
+    if not status.OK():
+        return
+    alldb = list(INTERNAL_DB)
+    alldb.extend(dbs)
+    for db in alldb:
+        status, tables = executor.GetAllTable(db)
+        if not status.OK():
+            continue
+        for name in tables:
+            RecoverTable(executor, db, name)
+
+if __name__ == "__main__":
+    (options, args) = parser.parse_args()
+    executor = Executor(options.openmldb_bin_path, options.zk_cluster, options.zk_root_path)
+    RecoverData(executor)
---
+++ tools/tool.py
@@ -0,0 +1,271 @@
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import os
+import subprocess
+import sys
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format = '%(levelname)s: %(message)s')
+
+USE_SHELL = sys.platform.startswith("win")
+class Status:
+    def __init__(self, code = 0, msg = "ok"):
+        self.code = code
+        self.msg = msg
+
+    def OK(self):
+        return True if self.code == 0 else False
+
+    def GetMsg(self):
+        return self.msg
+
+    def GetCode(self):
+        return self.code
+
+class Executor:
+    def __init__(self, openmldb_bin_path, zk_cluster, zk_root_path):
+        self.openmldb_bin_path = openmldb_bin_path
+        self.zk_cluster = zk_cluster
+        self.zk_root_path = zk_root_path
+        self.ns_base_cmd = [self.openmldb_bin_path,
+                            "--zk_cluster=" + self.zk_cluster,
+                            "--zk_root_path=" + self.zk_root_path,
+                            "--role=ns_client",
+                            "--interactive=false"]
+        self.tablet_base_cmd = [self.openmldb_bin_path, "--role=client", "--interactive=false"]
+        status, endpoint = self.GetNsLeader()
+        if status.OK():
+            self.ns_leader = endpoint
+            log.info(f"ns leader: {self.ns_leader}")
+            self.ns_base_cmd = [self.openmldb_bin_path,
+                                "--endpoint=" + self.ns_leader,
+                                "--role=ns_client",
+                                "--interactive=false"]
+
+    def RunWithRetuncode(self, command,
+                         universal_newlines = True,
+                         useshell = USE_SHELL,
+                         env = os.environ):
+        try:
+            p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = useshell, universal_newlines = universal_newlines, env = env)
+            output = p.stdout.read()
+            p.wait()
+            errout = p.stderr.read()
+            p.stdout.close()
+            p.stderr.close()
+            return Status(p.returncode, errout), output
+        except Exception as ex:
+            return Status(-1, ex), None
+
+    def GetNsLeader(self):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=showns")
+        status, output = self.RunWithRetuncode(cmd)
+        if status.OK():
+            result = self.ParseResult(output)
+            for record in result:
+                if record[2] == "leader":
+                    return Status(), record[0]
+        return Status(-1, "get ns leader falied"), None
+
+
+    def ParseResult(self, output):
+        result = []
+        lines = output.split("\n")
+        content_is_started = False
+        for line in lines:
+            if line.startswith("---------"):
+                content_is_started = True
+                continue
+            if not content_is_started:
+                continue
+            record = line.split()
+            if len(record) > 0:
+                result.append(record)
+        return result
+
+    def GetAutofailover(self):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=confget auto_failover")
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status, None
+        if stdout.find("true") != -1:
+            return Status(), True
+        return Status(), False;
+
+    def SetAutofailover(self, value):
+        confset_no.append("--cmd=confset auto_failover " + value)
+        status, output = RunWithRetuncode(confset_no)
+        return status
+
+    def GetAllDatabase(self):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=showdb")
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status, None
+        dbs = []
+        for record in self.ParseResult(output):
+            if len(record) < 2:
+                continue
+            dbs.append(record[1])
+        return Status(), dbs
+
+    def GetTableInfo(self, database, table_name = ''):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=showtable " + table_name)
+        cmd.append("--database=" + database)
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status, None
+        result = []
+        for record in self.ParseResult(output):
+            if len(record) < 4:
+                continue
+            result.append(record)
+        return Status(), result
+
+    def ParseTableInfo(self, table_info):
+        result = {}
+        for record in table_info:
+            is_leader = True if record[4] == "leader" else False
+            is_alive = True if record[5] == "yes" else False
+            partition = Partition(record[0], record[1], record[2], record[3], is_leader, is_alive, int(record[6]));
+            result.setdefault(record[2], [])
+            result[record[2]].append(partition)
+        return result
+
+    def GetTablePartition(self, database, table_name):
+        status, result = self.GetTableInfo(database, table_name)
+        if not status.OK:
+            return status, None
+        partition_dict = self.ParseTableInfo(result)
+        return Status(), partition_dict
+
+    def GetAllTable(self, database):
+        status, result = self.GetTableInfo(database)
+        if not status.OK():
+            return status, None
+        tables = []
+        for partition in result:
+            if partition[0] not in tables:
+                tables.append(partition[0])
+        return Status(), tables
+
+    def GetTableStatus(self, endpoint, tid = '', pid = ''):
+        cmd = list(self.tablet_base_cmd)
+        cmd.append("--endpoint=" + endpoint)
+        cmd.append("--cmd=gettablestatus " + tid + " " + pid)
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status, None
+        result = {}
+        for record in self.ParseResult(output):
+            if len(record) < 4:
+                continue
+            key = "{}_{}".format(record[0], record[1])
+            result[key] = record
+        return Status(), result
+
+    def LoadTable(self, endpoint, name, tid, pid, sync = True):
+        cmd = list(self.tablet_base_cmd)
+        cmd.append("--endpoint=" + endpoint)
+        cmd.append("--cmd=loadtable {} {} {} 0 8".format(name, tid, pid))
+        status, output = self.RunWithRetuncode(cmd)
+        if status.OK() and output.find("LoadTable ok") != -1:
+            if not sync:
+                return Status()
+            while True:
+                status, result = self.GetTableStatus(endpoint, tid, pid)
+                key = "{}_{}".format(tid, pid)
+                if status.OK() and key in result:
+                    table_stat = result[key][4]
+                    if table_stat == "kTableNormal":
+                        return Status()
+                    elif table_stat == "kLoading" or table_stat == "kTableUndefined":
+                        log.info(f"table is loading... tid {tid} pid {pid}")
+                        sleep(2)
+                    else:
+                        return Status(-1, "load table failed")
+
+        return Status(-1, "load table failed")
+
+    def GetLeaderFollowerOffset(self, tid, pid):
+        cmd = list(self.tablet_base_cmd)
+        cmd.append("--endpoint=" + endpoint)
+        cmd.append("--cmd=getfollower {} {}".format(tid, pid))
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status
+        return Status(), self.ParseResult(output)
+
+    def RecoverTablePartition(self, database, name, pid, endpoint):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=recovertable {} {} {}".format(name, pid, endpoint))
+        cmd.append("--database=" + database)
+        status, output = self.RunWithRetuncode(cmd)
+        if status.OK() and out.find("recover table ok") != -1:
+            return Status()
+        return Status(-1, "recover table failed")
+
+    def UpdateTableAlive(self, database, name, pid, endpoint, is_alive):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=updatetablealive {} {} {} {}".format(name, pid, endpoint, is_alive))
+        cmd.append("--database=" + database)
+        status, output = self.RunWithRetuncode(cmd)
+        if status.OK() and out.find("update ok") != -1:
+            return Status()
+        return Status(-1, "update table alive failed")
+
+    def ChangeLeader(self, database, name, pid):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=changeleader {} {} auto".format(name, pid))
+        cmd.append("--database=" + database)
+        status, output = self.RunWithRetuncode(cmd)
+        return status
+
+    def ShowOpStatus(self, database, name, pid = ''):
+        cmd = list(self.ns_base_cmd)
+        cmd.append("--cmd=showopstatus {} {} ".format(name, pid))
+        cmd.append("--database=" + database)
+        status, output = self.RunWithRetuncode(cmd)
+        if not status.OK():
+            return status, None
+        return Status(), self.ParseResult(output)
+
+class Partition:
+    def __init__(self, name, tid, pid, endpoint, is_leader, is_alive, offset):
+        self.name = name
+        self.tid = tid
+        self.pid = pid
+        self.endpoint = endpoint
+        self.is_leader = is_leader
+        self.is_alive = is_alive
+        self.offset = offset
+
+    def GetTid(self):
+        return self.tid
+    def GetName(self):
+        return self.name
+    def GetPid(self):
+        return self.pid
+    def GetOffset(self):
+        return self.offset
+    def GetEndpoint(self):
+        return self.endpoint
+    def IsAlive(self):
+        return self.is_alive
+    def IsLeader(self):
+        return self.is_leader
