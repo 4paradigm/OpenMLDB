e1e3cbc0f57120896b3a965968f6802f1dff9baa
aceforeverd <teapot@aceforeverd.com>
2022-11-05 09:29:52 +0800

    fix: hides farmhash in header

--- hybridse/src/udf/udf.h
+++ hybridse/src/udf/udf.h
@@ -21,13 +21,10 @@
 #include <string>
 #include <tuple>
 
-#include "absl/hash/hash.h"
 #include "base/string_ref.h"
 #include "base/type.h"
-#include "boost/lexical_cast.hpp"
 #include "codec/list_iterator_codec.h"
 #include "codec/type_codec.h"
-#include "farmhash.h"  // NOLINT
 #include "node/node_manager.h"
 #include "proto/fe_type.pb.h"
 #include "udf/literal_traits.h"
@@ -208,6 +205,7 @@ struct IncOne {
     V operator()(V i) { return i + 1; }
 };
 
+int64_t FarmFingerprint(absl::string_view input);
 template <typename T>
 struct Hash64 {
     using Args = std::tuple<T>;
@@ -216,10 +214,6 @@ struct Hash64 {
 
     int64_t operator()(ParamType v) {
         return FarmFingerprint(CCallDataTypeTrait<ParamType>::to_bytes_ref(&v));
-    }
-
-    static int64_t FarmFingerprint(absl::string_view input) {
-        return absl::bit_cast<int64_t>(farmhash::Fingerprint64(input));
     }
 };
 
