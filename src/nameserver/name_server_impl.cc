2d8e2622f7da82ddab613617dbfb2aee388412a9
dl239 <dl239@126.com>
2022-11-07 16:56:44 +0800

    feat: skip current op

--- src/nameserver/name_server_impl.cc
+++ src/nameserver/name_server_impl.cc
@@ -3143,6 +3143,9 @@ void NameServerImpl::DropTableInternel(const DropTableRequest& request, GeneralR
                 continue;
             }
             for (auto& op_data : op_list) {
+                if (task_ptr && task_ptr->op_id() == op_data->op_info_.op_id()) {
+                    continue;
+                }
                 if (op_data->op_info_.db() == db && op_data->op_info_.name() != name) {
                     if (op_data->op_info_.task_status() == ::openmldb::api::kInited ||
                          (op_data->op_info_.task_status() == ::openmldb::api::kDoing)) {
