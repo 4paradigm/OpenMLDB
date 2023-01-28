cd90b1f28d0aad67e9d8a7424fcd869c0896a260
dl239 <dl239@126.com>
2023-01-28 14:55:09 +0800

    feat: specify the maximal size of rocksdb log file

--- src/flags.cc
+++ src/flags.cc
@@ -152,16 +152,6 @@ DEFINE_uint32(latest_default_skiplist_height, 1, "the default height of skiplist
 DEFINE_uint32(absolute_default_skiplist_height, 4, "the default height of skiplist for absolute table");
 DEFINE_uint32(max_col_display_length, 256, "config the max length of column display");
 
-// rocksdb
-DEFINE_bool(disable_wal, true, "If true, do not write WAL for write.");
-DEFINE_string(file_compression, "off", "Type of compression, can be off, pz, lz4, zlib");
-DEFINE_uint32(block_cache_mb, 4096,
-              "Memory allocated for caching uncompressed block (OS page cache "
-              "handles the compressed ones)");
-DEFINE_uint32(write_buffer_mb, 128, "Memtable size");
-DEFINE_uint32(block_cache_shardbits, 8, "Divide block cache into 2^8 shards to avoid cache contention");
-DEFINE_bool(verify_compression, false, "For debug");
-
 // load table resouce control
 DEFINE_uint32(load_table_batch, 30, "set laod table batch size");
 DEFINE_uint32(load_table_thread_num, 3, "set load tabale thread pool size");
@@ -173,3 +163,15 @@ DEFINE_uint32(get_replica_status_interval, 10000,
 
 DEFINE_uint32(sync_deploy_stats_timeout, 10000,
               "time interval in milliseconds to sync deploy response time stats into table");
+
+// config for rocksdb
+DEFINE_bool(disable_wal, true, "If true, do not write WAL for write.");
+DEFINE_string(file_compression, "off", "Type of compression, can be off, pz, lz4, zlib");
+DEFINE_uint32(block_cache_mb, 4096,
+              "Memory allocated for caching uncompressed block (OS page cache "
+              "handles the compressed ones)");
+DEFINE_uint32(write_buffer_mb, 128, "Memtable size");
+DEFINE_uint32(block_cache_shardbits, 8, "Divide block cache into 2^8 shards to avoid cache contention");
+DEFINE_bool(verify_compression, false, "For debug");
+DEFINE_uint32(max_log_file_size, 100 * 1024 * 1024, "Specify the maximal size of the rocksdb info log file");
+DEFINE_uint32(keep_log_file_num, 5, "Maximal info log files to be kept");
