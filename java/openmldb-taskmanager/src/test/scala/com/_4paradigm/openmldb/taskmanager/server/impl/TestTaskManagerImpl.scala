9ab70ba410041060e87384d95f37b73d67bce74f
tobe <tobeg3oogle@gmail.com>
2023-03-07 16:45:37 +0800

    Add ut for import online data and import offline data

--- java/openmldb-taskmanager/src/test/scala/com/_4paradigm/openmldb/taskmanager/server/impl/TestTaskManagerImpl.scala
+++ java/openmldb-taskmanager/src/test/scala/com/_4paradigm/openmldb/taskmanager/server/impl/TestTaskManagerImpl.scala
@@ -17,9 +17,15 @@
 package com._4paradigm.openmldb.taskmanager.server.impl
 
 import com._4paradigm.openmldb.proto.TaskManager
+import com._4paradigm.openmldb.sdk.SdkOption
+import com._4paradigm.openmldb.sdk.impl.SqlClusterExecutor
+import com._4paradigm.openmldb.taskmanager.config.TaskManagerConfig
 import com._4paradigm.openmldb.taskmanager.{JobInfoManager, LogManager}
 import com._4paradigm.openmldb.taskmanager.server.StatusCode
 import org.scalatest.FunSuite
+import com._4paradigm.openmldb.proto.NS
+import org.apache.spark.sql.SparkSession
+
 
 class TestTaskManagerImpl extends FunSuite {
 
@@ -39,7 +45,7 @@ class TestTaskManagerImpl extends FunSuite {
     val response = impl.GetVersion(request)
 
     assert(!response.getBatchVersion.equals("unknown"))
-    // Notice that we can not get TaskManager version in unit test
+    // TODO(tobe): Notice that we can not get TaskManager version in unit test
     //assert(response.getTaskmanagerVersion.equals("unknown"))
   }
 
@@ -66,6 +72,120 @@ class TestTaskManagerImpl extends FunSuite {
     assert(response.getCode == StatusCode.SUCCESS)
     assert(finalJobInfo.isSuccess)
     assert(!jobLog.equals(""))
+  }
+
+  // TODO(tobe): RunBatchSql requires TaskManager server to get job log and do not test now
+
+  test("Test RunBatchAndShow") {
+    val impl = new TaskManagerImpl()
+
+    val sql = "SELECT 100"
+    val request = TaskManager.RunBatchAndShowRequest.newBuilder.setSyncJob(true).setSql(sql).build()
+    val response = impl.RunBatchAndShow(request)
+
+    val jobId = response.getJob().getId()
+    val finalJobInfo = JobInfoManager.getJob(jobId).get
+    val jobLog = LogManager.getJobLog(jobId)
+
+    assert(response.getCode == StatusCode.SUCCESS)
+    assert(finalJobInfo.isSuccess)
+
+    val expectedJobLog =
+      """
+        |+---+
+        ||100|
+        |+---+
+        |+---+
+        |""".stripMargin.trim
+    assert(jobLog.trim.equals(expectedJobLog))
+  }
+
+  test("Test ImportOnlineData") {
+    val impl = new TaskManagerImpl()
+
+    // Create test table
+    val testDb = "db1"
+    val testTable = "t1"
+    val option = new SdkOption
+    option.setZkCluster(TaskManagerConfig.ZK_CLUSTER)
+    option.setZkPath(TaskManagerConfig.ZK_ROOT_PATH)
+    val executor = new SqlClusterExecutor(option)
+    executor.createDB(testDb)
+    executor.executeDDL(testDb, s"drop table $testTable")
+    executor.executeDDL(testDb, s"create table $testTable(c1 int)")
+
+    // Run SQL
+    val path = getClass.getResource("/c1_table.csv").getPath
+    val sql = s"LOAD DATA INFILE 'file://$path' INTO TABLE $testTable OPTIONS (header=true, mode='append')"
+
+    val request = TaskManager.ImportOnlineDataRequest.newBuilder
+      .setSyncJob(true)
+      .setSql(sql)
+      .setDefaultDb(testDb)
+      .build()
+    val response = impl.ImportOnlineData(request)
+
+    val jobId = response.getJob().getId()
+    val finalJobInfo = JobInfoManager.getJob(jobId).get
+
+    // Check status
+    assert(response.getCode == StatusCode.SUCCESS)
+    assert(finalJobInfo.isSuccess)
+
+    // Check table result
+    executor.executeSQL(testDb, "SET @@execute_mode='online'")
+    val rs = executor.executeSQL(testDb, s"select count(*) from $testTable")
+    rs.next()
+    val count = rs.getLong(1)
+    assert(count == 5)
+
+    executor.executeDDL(testDb, s"drop table $testTable")
+  }
+
+
+  test("Test ImportOfflineData") {
+    val impl = new TaskManagerImpl()
+
+    // Create test table
+    val testDb = "db1"
+    val testTable = "t1"
+    val option = new SdkOption
+    option.setZkCluster(TaskManagerConfig.ZK_CLUSTER)
+    option.setZkPath(TaskManagerConfig.ZK_ROOT_PATH)
+    val executor = new SqlClusterExecutor(option)
+
+    executor.createDB(testDb)
+    executor.executeDDL(testDb, s"drop table $testTable")
+    executor.executeDDL(testDb, s"create table $testTable(c1 int)")
+
+    // Run SQL
+    val path = getClass.getResource("/c1_table.csv").getPath
+    val sql = s"LOAD DATA INFILE 'file://$path' INTO TABLE $testTable OPTIONS (header=true, mode='append')"
+
+    val request = TaskManager.ImportOfflineDataRequest.newBuilder
+      .setSyncJob(true)
+      .setSql(sql)
+      .setDefaultDb(testDb)
+      .build()
+    val response = impl.ImportOfflineData(request)
+
+    val jobId = response.getJob().getId()
+    val finalJobInfo = JobInfoManager.getJob(jobId).get
+
+    // Check status
+    assert(response.getCode == StatusCode.SUCCESS)
+    assert(finalJobInfo.isSuccess)
+
+    // Check offline table result
+    val tableInfo: NS.TableInfo = executor.getTableInfo(testDb, testTable)
+    val offlinePath: String = tableInfo.getOfflineTableInfo().getPath()
+    assert(offlinePath != null && !offlinePath.equals(""))
+
+    val spark = SparkSession.builder().master("local").getOrCreate()
+    val df = spark.read.parquet(offlinePath)
+    assert(df.count() == 5)
+
+    executor.executeDDL(testDb, s"drop table $testTable")
   }
 
 }
