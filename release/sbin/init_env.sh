3686f209f595eb6e3fbab5e076a898703c3b20ba
dl239 <dl239@126.com>
2023-03-13 19:26:47 +0800

    fix: fix style

--- release/sbin/init_env.sh
+++ release/sbin/init_env.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-user=`whoami`
+user=$(whoami)
 if [[ ${user} != "root" ]]; then
   echo "please switch to 'root' user to run this script"
   exit
