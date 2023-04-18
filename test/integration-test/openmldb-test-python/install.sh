1d739e92fa044a757dfed1f31a7dc6980b313a45
dl239 <dl239@126.com>
2023-04-18 16:27:12 +0800

    fix: fix build

--- test/integration-test/openmldb-test-python/install.sh
+++ test/integration-test/openmldb-test-python/install.sh
@@ -23,6 +23,6 @@ sed -i"" -e "s/OPENMLDB_MODE:=standalone/OPENMLDB_MODE:=cluster/g" openmldb/conf
 sed -i"" -e "s/.*make_snapshot_threshold_offset.*/--make_snapshot_threshold_offset=1/g" openmldb/conf/tablet.flags.template
 rm -rf /tmp/openmldb
 python3 genhost.py
-sh -x openmldb/sbin/deploy-all.sh
-sh -x openmldb/sbin/start-all.sh
+bash openmldb/sbin/deploy-all.sh
+bash openmldb/sbin/start-all.sh
 popd
