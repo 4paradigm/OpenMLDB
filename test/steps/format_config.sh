bb6c304365f812e284cf2ab417c946673edf77f3
liuqiyuan <liuqiyuan@node-4.sg.4pd.io>
2023-05-29 18:19:22 +0800

    []

--- test/steps/format_config.sh
+++ test/steps/format_config.sh
@@ -12,7 +12,7 @@ curTime=$(date "+%m%d%H%M")
 dirName=${jobName}-${version}-${curTime}
 
 #set Deploy Host and Ports
-Hosts=(node-3 node-3 node-1)
+Hosts=(node-3 node-4 node-1)
 
 AvaNode1Ports=$(ssh ${Hosts[0]} "comm -23 <(seq $portFrom $portTo | sort) <(sudo ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 8")
 AvaNode2Ports=$(ssh ${Hosts[1]} "comm -23 <(seq $portFrom $portTo | sort) <(sudo ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1")
