9ea7563b75d7e1b26adbef7e0010f3b667437354
TanZiYen <104113819+TanZiYen@users.noreply.github.com>
2023-05-29 13:02:11 +0800

    Change to only fix warning branch

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -136,7 +136,7 @@ smv_tag_whitelist = None
 
 # Whitelist pattern for branches (set to None to ignore all branches)
 # include branch that is main or v{X}.{Y}
-smv_branch_whitelist = r"^(main|v\d+\.\d+)$"
+smv_branch_whitelist = r"^(fix_warning|v\d+\.\d+)$"
 
 # allow remote origin or upstream
-smv_remote_whitelist = r"^(origin|upstream)$"
+#smv_remote_whitelist = r"^(origin|upstream)$"
