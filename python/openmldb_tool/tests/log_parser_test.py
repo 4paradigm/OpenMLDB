809c9dab64f2f1e963d1379e82a7999f37f4f1cf
张子恒 <zhangziheng00001@163.com>
2023-05-30 11:21:04 +0800

    fix log_parser_test OpenMLDB_ZK_CLUSTER

--- python/openmldb_tool/tests/log_parser_test.py
+++ python/openmldb_tool/tests/log_parser_test.py
@@ -4,13 +4,14 @@ from absl import flags
 
 import pytest
 from diagnostic_tool.parser import LogParser
+from .case_conf import OpenMLDB_ZK_CLUSTER
 
 err_log_list = [os.path.join("off_err_logs", err_log) for err_log in os.listdir("off_err_logs")]
 
 
 @pytest.mark.parametrize("err_log", err_log_list)
 def test_pattern_logs(err_log):
-    flags.FLAGS['cluster'].parse('127.0.0.1:2181/openmldb')
+    flags.FLAGS['cluster'].parse(OpenMLDB_ZK_CLUSTER)
     flags.FLAGS['sdk_log'].parse(False)
     print("in", err_log)
     with open(err_log, "r") as f:
