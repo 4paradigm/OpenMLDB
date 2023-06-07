f70be9b3c423eedb0dec0982c123bf20e6080869
张子恒 <zhangziheng00001@163.com>
2023-06-07 12:01:54 +0800

    back and check

--- python/openmldb_sdk/setup.py
+++ python/openmldb_sdk/setup.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='openmldb',
@@ -38,7 +38,7 @@ setup(
     ]},
     include_package_data=True,
     package_data={'': ['*.so']},
-    packages=['openmldb'],
+    packages=find_packages(),
     entry_points={
         'sqlalchemy.dialects': [
             'openmldb = openmldb.sqlalchemy_openmldb.openmldb_dialect:OpenmldbDialect',
