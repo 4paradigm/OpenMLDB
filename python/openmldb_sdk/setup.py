2ed4c5f757da233c16fc73cdb31cc5e4cb3621a4
aceforeverd <teapot@aceforeverd.com>
2023-07-05 02:17:28 +0000

    bump version: 0.7.3.p1

--- python/openmldb_sdk/setup.py
+++ python/openmldb_sdk/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openmldb',
-    version='0.7.0a0',
+    version='0.7.3p1',
     author='OpenMLDB Team',
     author_email=' ',
     url='https://github.com/4paradigm/OpenMLDB',
