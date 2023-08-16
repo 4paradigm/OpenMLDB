4e568b0bd921f95055b1039be0d6b45c506609f1
dl239 <dl239@126.com>
2023-08-16 18:55:36 +0800

    refact: rm unused code

--- tools/tool.py
+++ tools/tool.py
@@ -16,7 +16,6 @@ import os
 import subprocess
 import sys
 import time
-#from typing import List, Dict, Tuple
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format = '%(levelname)s: %(message)s')
 
