63a4b7e69a150d0b5bb6dfc487891e61141cf97a
tobe <tobeg3oogle@gmail.com>
2023-09-14 10:24:27 +0800

    Reset install spark script

--- release/sbin/deploy-all.sh
+++ release/sbin/deploy-all.sh
@@ -132,8 +132,6 @@ function download_spark {
       if [[ -e "${spark_tar}" ]]; then
         echo "Skip downloading openmldbspark as ${spark_tar} already exists"
       else
-        # TODO: Change spark version
-        #url="https://github.com/4paradigm/spark/releases/download/v3.2.1-openmldb${OPENMLDB_VERSION}/${spark_tar}"
         url="https://github.com/4paradigm/spark/releases/download/v3.2.1-openmldb0.7.1/${spark_tar}"
         echo "Download spark from $url"
         curl -SLo ${spark_tar} "$url"
