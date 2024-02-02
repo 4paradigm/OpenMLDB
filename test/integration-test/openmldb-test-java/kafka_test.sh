c86a70f2a7eba702b5132ce6992ffbd129614a4f
liuqiyuan <liuqiyuan@node-4.sg.4pd.io>
2024-02-02 17:41:58 +0800

    fix shell issue

--- test/integration-test/openmldb-test-java/kafka_test.sh
+++ test/integration-test/openmldb-test-java/kafka_test.sh
@@ -10,8 +10,8 @@ zk="${zkc}""${zkpath}"
 docker exec -it kafka_test /start.sh "${zk}"
 sed -i "s#\"bootstrap.servers\":.*#\"bootstrap.servers\":node-4:9092,#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
 sed -i "s#\"connect.listeners\":.*#\"connect.listeners\":http://node-4:8083,#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
-sed -i "s#apiserver.address:.*#apiserver.address: "${apiserver}"#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
-sed -i "s#kafka_test?.*#kafka_test?"${zk}"\"#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
+sed -i "s#apiserver.address:.*#apiserver.address: ${apiserver}#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
+sed -i "s#kafka_test?.*#kafka_test?${zk}\"#" openmldb-ecosystem/src/test/resources/kafka_test_cases.yml
 
 mvn test -pl openmldb-ecosystem
 
