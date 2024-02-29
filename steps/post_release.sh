a091d043d2b9883dbc71a65be4b6da777f59f050
dl239 <dl239@126.com>
2024-02-29 17:55:17 +0800

    fix: update version

--- steps/post_release.sh
+++ steps/post_release.sh
@@ -21,8 +21,20 @@ then
     echo "invalid version ${VERSION}"
     exit 0
 fi
+# shellcheck disable=SC2206
+ARR=(${VERSION//./ })
+echo "splited version components: ${ARR[*]}"
+if [[ "${#ARR[*]}" -lt 3 ]]; then
+    echo -e "${RED}inputed version should have at least three number${NC}"
+    exit 1
+fi
+MAJOR=${ARR[0]}
+MINOR=${ARR[1]}
+BUG=${ARR[2]}
+NEW_BUG=$(($BUG+1))
+NEW_VERSION="${MAJOR}.${MINOR}.${NEW_BUG}"
 
 sed -i"" -e "s/OPENMLDB_VERSION=[0-9]\.[0-9]\.[0-9]/OPENMLDB_VERSION=${VERSION}/g" demo/Dockerfile
 sed -i"" -e "s/SPARK_VERSION=[0-9]\.[0-9]\.[0-9]/SPARK_VERSION=${VERSION}/g" test/integration-test/openmldb-test-python/install.sh
 
-sh steps/prepare_release.sh "${VERSION}.a0"
+sh steps/prepare_release.sh "${NEW_VERSION}.a0"
