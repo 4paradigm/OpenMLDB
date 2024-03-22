bed02af1ccc6b9f7f1e8290e7bbf0acf19290c7d
yangwucheng <yangwucheng1985@163.com>
2024-03-22 13:15:34 +0800

    feat(open-mysql-db): support dbeaver (#3826)
    * feat(open-mysql-db): dbeaver adapt
    
    1. mock `show charset`
    2. mock `show variables like 'lower_case_table_names'`
    3. mock `select word from information_schema.keywords`
    4. mock `SELECT \* FROM information_schema\.TABLES t`
    5. mock `SELECT SUM\(\(DATA_LENGTH\+INDEX_LENGTH\)\)\s+FROM`
    6. mock `SELECT \* FROM information_schema\.STATISTICS.*`
    7. mock `SELECT .*\s+FROM INFORMATION_SCHEMA\.KEY_COLUMN_USAGE.*`
    8. mock `SELECT DISTINCT .+\.REFERENCED_TABLE_SCHEMA AS PKTABLE_CAT`
    9. mock `session.transaction_isolation`
    10. mock `session.transaction_read_only`
    11. remove leading comment of query string
    12. mock `SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA='xzs' AND TABLE_NAME='t_exam_paper' ORDER BY ORDINAL_POSITION`
    13. fix regex .
    14. mock `SELECT * FROM information_schema.SCHEMATA WHERE SCHEMA_NAME='demo_db'`
    15. mock `SHOW FULL TABLES FROM demo_db`
    16. fix statement.close
    17. mock `SELECT DATABASE()`
    18. remove limit clause of query string
    
    * feat(open-mysql-db): dbeaver adapt
    
    1. mock `SELECT cc.CONSTRAINT_NAME, cc.CHECK_CLAUSE, tc.TABLE_NAME`
    
    ---------
    
    Co-authored-by: yangwucheng <yangwucheng@4paradigm.com>

--- extensions/open-mysql-db/src/main/java/com/_4paradigm/openmldb/mysql/mock/MockResult.java
+++ extensions/open-mysql-db/src/main/java/com/_4paradigm/openmldb/mysql/mock/MockResult.java
@@ -58,6 +58,34 @@ public class MockResult {
     rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
+    query = "show charset";
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Charset", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Description", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Default collation", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Maxlen", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("utf8mb4");
+    row.add("UTF-8 Unicode");
+    row.add("utf8mb4_0900_ai_ci");
+    row.add("4");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query = "show variables like 'lower_case_table_names'";
+    // Variable_name	Value
+    // lower_case_table_names	2
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Variable_name", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Value", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("lower_case_table_names");
+    row.add("2");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
     query = "show character set where charset = 'utf8mb4'";
     columns = new ArrayList<>();
     columns.add(new QueryResultColumn("Charset", "VARCHAR(255)"));
@@ -345,6 +373,19 @@ public class MockResult {
     rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
+    query = "select word from information_schema.keywords where reserved=1 order by word";
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("WORD", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    String keyWords =
+        "ACCESSIBLE,ADD,ALL,ALTER,ANALYZE,AND,AS,ASC,ASENSITIVE,BEFORE,BETWEEN,BIGINT,BINARY,BLOB,BOTH,BY,CALL,CASCADE,CASE,CHANGE,CHAR,CHARACTER,CHECK,COLLATE,COLUMN,CONDITION,CONSTRAINT,CONTINUE,CONVERT,CREATE,CROSS,CUBE,CUME_DIST,CURRENT_DATE,CURRENT_TIME,CURRENT_TIMESTAMP,CURRENT_USER,CURSOR,DATABASE,DATABASES,DAY_HOUR,DAY_MICROSECOND,DAY_MINUTE,DAY_SECOND,DEC,DECIMAL,DECLARE,DEFAULT,DELAYED,DELETE,DENSE_RANK,DESC,DESCRIBE,DETERMINISTIC,DISTINCT,DISTINCTROW,DIV,DOUBLE,DROP,DUAL,EACH,ELSE,ELSEIF,EMPTY,ENCLOSED,ESCAPED,EXCEPT,EXISTS,EXIT,EXPLAIN,FALSE,FETCH,FIRST_VALUE,FLOAT,FLOAT4,FLOAT8,FOR,FORCE,FOREIGN,FROM,FULLTEXT,FUNCTION,GENERATED,GET,GRANT,GROUP,GROUPING,GROUPS,HAVING,HIGH_PRIORITY,HOUR_MICROSECOND,HOUR_MINUTE,HOUR_SECOND,IF,IGNORE,IN,INDEX,INFILE,INNER,INOUT,INSENSITIVE,INSERT,INT,INT1,INT2,INT3,INT4,INT8,INTEGER,INTERVAL,INTO,IO_AFTER_GTIDS,IO_BEFORE_GTIDS,IS,ITERATE,JOIN,JSON_TABLE,KEY,KEYS,KILL,LAG,LAST_VALUE,LATERAL,LEAD,LEADING,LEAVE,LEFT,LIKE,LIMIT,LINEAR,LINES,LOAD,LOCALTIME,LOCALTIMESTAMP,LOCK,LONG,LONGBLOB,LONGTEXT,LOOP,LOW_PRIORITY,MASTER_BIND,MASTER_SSL_VERIFY_SERVER_CERT,MATCH,MAXVALUE,MEDIUMBLOB,MEDIUMINT,MEDIUMTEXT,MIDDLEINT,MINUTE_MICROSECOND,MINUTE_SECOND,MOD,MODIFIES,NATURAL,NO_WRITE_TO_BINLOG,NOT,NTH_VALUE,NTILE,NULL,NUMERIC,OF,ON,OPTIMIZE,OPTIMIZER_COSTS,OPTION,OPTIONALLY,OR,ORDER,OUT,OUTER,OUTFILE,OVER,PARTITION,PERCENT_RANK,PRECISION,PRIMARY,PROCEDURE,PURGE,RANGE,RANK,READ,READ_WRITE,READS,REAL,RECURSIVE,REFERENCES,REGEXP,RELEASE,RENAME,REPEAT,REPLACE,REQUIRE,RESIGNAL,RESTRICT,RETURN,REVOKE,RIGHT,RLIKE,ROW,ROW_NUMBER,ROWS,SCHEMA,SCHEMAS,SECOND_MICROSECOND,SELECT,SENSITIVE,SEPARATOR,SET,SHOW,SIGNAL,SMALLINT,SPATIAL,SPECIFIC,SQL,SQL_BIG_RESULT,SQL_CALC_FOUND_ROWS,SQL_SMALL_RESULT,SQLEXCEPTION,SQLSTATE,SQLWARNING,SSL,STARTING,STORED,STRAIGHT_JOIN,SYSTEM,TABLE,TERMINATED,THEN,TINYBLOB,TINYINT,TINYTEXT,TO,TRAILING,TRIGGER,TRUE,UNDO,UNION,UNIQUE,UNLOCK,UNSIGNED,UPDATE,USAGE,USE,USING,UTC_DATE,UTC_TIME,UTC_TIMESTAMP,VALUES,VARBINARY,VARCHAR,VARCHARACTER,VARYING,VIRTUAL,WHEN,WHERE,WHILE,WINDOW,WITH,WRITE,XOR,YEAR_MONTH,ZEROFILL";
+    for (String keyWord : keyWords.split(",")) {
+      row = new ArrayList<>();
+      row.add(keyWord);
+      rows.add(row);
+    }
+    mockResults.put(query, new Pair<>(columns, rows));
+
     String pattern =
         "(?i)SELECT DISTINCT ROUTINE_SCHEMA, ROUTINE_NAME, PARAMS\\.PARAMETER FROM information_schema\\.ROUTINES LEFT JOIN \\( SELECT SPECIFIC_SCHEMA, SPECIFIC_NAME, GROUP_CONCAT\\(CONCAT\\(DATA_TYPE, ' ', PARAMETER_NAME\\) ORDER BY ORDINAL_POSITION SEPARATOR ', '\\) PARAMETER, ROUTINE_TYPE FROM information_schema\\.PARAMETERS GROUP BY SPECIFIC_SCHEMA, SPECIFIC_NAME, ROUTINE_TYPE \\) PARAMS ON ROUTINES\\.ROUTINE_SCHEMA = PARAMS\\.SPECIFIC_SCHEMA AND ROUTINES\\.ROUTINE_NAME = PARAMS\\.SPECIFIC_NAME AND ROUTINES\\.ROUTINE_TYPE = PARAMS\\.ROUTINE_TYPE WHERE ROUTINE_SCHEMA = '.+' ORDER BY ROUTINE_SCHEMA";
     // ROUTINE_SCHEMA	ROUTINE_NAME	PARAMETER
@@ -372,6 +413,49 @@ public class MockResult {
     rows = new ArrayList<>();
     mockPatternResults.put(pattern, new Pair<>(columns, rows));
 
+    // mock for dbeaver
+    // SELECT * FROM information_schema.TABLES t
+    // WHERE
+    //	t.TABLE_SCHEMA = 'information_schema'
+    //	AND t.TABLE_NAME = 'CHECK_CONSTRAINTS'
+    pattern =
+        "(?i)SELECT \\* FROM information_schema\\.TABLES t\\s+WHERE\\s+t\\.TABLE_SCHEMA = 'information_schema'\\s+AND t\\.TABLE_NAME = 'CHECK_CONSTRAINTS'";
+    // TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	TABLE_TYPE	ENGINE	VERSION	ROW_FORMAT	TABLE_ROWS
+    //	AVG_ROW_LENGTH	DATA_LENGTH	MAX_DATA_LENGTH	INDEX_LENGTH	DATA_FREE	AUTO_INCREMENT	CREATE_TIME
+    //	UPDATE_TIME	CHECK_TIME	TABLE_COLLATION	CHECKSUM	CREATE_OPTIONS	TABLE_COMMENT
+    // def	information_schema	CHECK_CONSTRAINTS	SYSTEM VIEW		10		0	0	0	0	0	0		2022-06-26 19:58:21
+    columns = new ArrayList<>();
+    columnNameStr =
+        "TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE, ENGINE, VERSION, ROW_FORMAT, TABLE_ROWS, AVG_ROW_LENGTH, DATA_LENGTH, MAX_DATA_LENGTH, INDEX_LENGTH, DATA_FREE, AUTO_INCREMENT, CREATE_TIME, UPDATE_TIME, CHECK_TIME, TABLE_COLLATION, CHECKSUM, CREATE_OPTIONS, TABLE_COMMENT";
+    for (String columnName : columnNameStr.split(", ")) {
+      columns.add(new QueryResultColumn(columnName, "VARCHAR(255)"));
+    }
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("def");
+    row.add("information_schema");
+    row.add("CHECK_CONSTRAINTS");
+    row.add("SYSTEM VIEW");
+    row.add(null);
+    row.add("10");
+    row.add(null);
+    row.add("0");
+    row.add("0");
+    row.add("0");
+    row.add("0");
+    row.add("0");
+    row.add("0");
+    row.add(null);
+    row.add("2022-06-26 19:58:21");
+    row.add(null);
+    row.add(null);
+    row.add(null);
+    row.add(null);
+    row.add("");
+    row.add("");
+    rows.add(row);
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
     pattern = "(?i)SHOW INDEX FROM .*";
     // # Table, Non_unique, Key_name, Seq_in_index, Column_name, Collation, Cardinality, Sub_part,
     // Packed, Null, Index_type, Comment, Index_comment, Visible, Expression
@@ -388,13 +472,125 @@ public class MockResult {
     // SELECT TABLE_NAME, CHECK_OPTION, IS_UPDATABLE, SECURITY_TYPE, DEFINER FROM
     // INFORMATION_SCHEMA.VIEWS WHERE TABLE_SCHEMA = 'demo_db' ORDER BY TABLE_NAME ASC
     pattern =
-        "(?i)SELECT .* FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_SCHEMA = '.+' ORDER BY TABLE_NAME ASC";
+        "(?i)SELECT .* FROM INFORMATION_SCHEMA\\.VIEWS WHERE TABLE_SCHEMA = '.+' ORDER BY TABLE_NAME ASC";
     // TABLE_NAME	CHECK_OPTION	IS_UPDATABLE	SECURITY_TYPE	DEFINER
     columns = new ArrayList<>();
     columnNameStr = "TABLE_NAME, CHECK_OPTION, IS_UPDATABLE, SECURITY_TYPE, DEFINER";
     for (String columnName : columnNameStr.split(", ")) {
       columns.add(new QueryResultColumn(columnName, "VARCHAR(255)"));
     }
+    rows = new ArrayList<>();
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
+    // SELECT SUM((DATA_LENGTH+INDEX_LENGTH))
+    // FROM INFORMATION_SCHEMA.TABLES
+    // WHERE TABLE_SCHEMA='demo_db'
+    pattern =
+        "(?i)SELECT SUM\\(\\(DATA_LENGTH\\+INDEX_LENGTH\\)\\)\\s+FROM INFORMATION_SCHEMA\\.TABLES\\s+WHERE TABLE_SCHEMA\\s*=\\s*'.+'";
+    // SUM((DATA_LENGTH+INDEX_LENGTH))
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("SUM((DATA_LENGTH+INDEX_LENGTH))", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
+    // SELECT * FROM information_schema.STATISTICS WHERE TABLE_SCHEMA='xzs' AND
+    // TABLE_NAME='t_exam_paper' ORDER BY TABLE_NAME,INDEX_NAME,SEQ_IN_INDEX
+    pattern = "(?i)SELECT \\* FROM information_schema\\.STATISTICS.*";
+    // TABLE_CATALOG	TABLE_SCHEMA	TABLE_NAME	NON_UNIQUE	INDEX_SCHEMA	INDEX_NAME	SEQ_IN_INDEX
+    //	COLUMN_NAME	COLLATION	CARDINALITY	SUB_PART	PACKED	NULLABLE	INDEX_TYPE	COMMENT	INDEX_COMMENT
+    //	IS_VISIBLE	EXPRESSION
+    // def	xzs	t_exam_paper	0	xzs	PRIMARY	1	id	A	0				BTREE			YES
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("TABLE_CATALOG", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("TABLE_SCHEMA", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("TABLE_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("NON_UNIQUE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("INDEX_SCHEMA", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("INDEX_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("SEQ_IN_INDEX", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("COLUMN_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("COLLATION", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("CARDINALITY", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("SUB_PART", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PACKED", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("NULLABLE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("INDEX_TYPE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("COMMENT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("INDEX_COMMENT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("IS_VISIBLE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("EXPRESSION", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
+    // SELECT kc.CONSTRAINT_NAME,kc.TABLE_NAME,kc.COLUMN_NAME,kc.ORDINAL_POSITION
+    // FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kc WHERE kc.TABLE_SCHEMA='demo_db' AND
+    // kc.REFERENCED_TABLE_NAME IS NULL AND kc.TABLE_NAME='demo_table1'
+    // ORDER BY kc.CONSTRAINT_NAME,kc.ORDINAL_POSITION
+    pattern = "(?i)SELECT .*\\s+FROM INFORMATION_SCHEMA\\.KEY_COLUMN_USAGE.*\\s+ORDER BY.*";
+    // CONSTRAINT_NAME	TABLE_NAME	COLUMN_NAME	ORDINAL_POSITION
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("CONSTRAINT_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("TABLE_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("COLUMN_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("ORDINAL_POSITION", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
+    // SELECT DISTINCT A.REFERENCED_TABLE_SCHEMA AS PKTABLE_CAT,NULL AS PKTABLE_SCHEM,
+    // A.REFERENCED_TABLE_NAME AS PKTABLE_NAME, A.REFERENCED_COLUMN_NAME AS PKCOLUMN_NAME,
+    // A.TABLE_SCHEMA AS FKTABLE_CAT, NULL AS FKTABLE_SCHEM, A.TABLE_NAME AS FKTABLE_NAME,
+    // A.COLUMN_NAME AS FKCOLUMN_NAME, A.ORDINAL_POSITION AS KEY_SEQ,CASE WHEN
+    // R.UPDATE_RULE='CASCADE' THEN 0 WHEN R.UPDATE_RULE='SET NULL' THEN 2 WHEN R.UPDATE_RULE='SET
+    // DEFAULT' THEN 4 WHEN R.UPDATE_RULE='RESTRICT' THEN 1 WHEN R.UPDATE_RULE='NO ACTION' THEN 1
+    // ELSE 1 END  AS UPDATE_RULE,CASE WHEN R.DELETE_RULE='CASCADE' THEN 0 WHEN R.DELETE_RULE='SET
+    // NULL' THEN 2 WHEN R.DELETE_RULE='SET DEFAULT' THEN 4 WHEN R.DELETE_RULE='RESTRICT' THEN 1
+    // WHEN R.DELETE_RULE='NO ACTION' THEN 1 ELSE 1 END  AS DELETE_RULE, A.CONSTRAINT_NAME AS
+    // FK_NAME, R.UNIQUE_CONSTRAINT_NAME AS PK_NAME,7 AS DEFERRABILITY FROM
+    // INFORMATION_SCHEMA.KEY_COLUMN_USAGE A JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS B USING
+    // (CONSTRAINT_NAME, TABLE_NAME) JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS R ON
+    // (R.CONSTRAINT_NAME = B.CONSTRAINT_NAME AND R.TABLE_NAME = B.TABLE_NAME AND
+    // R.CONSTRAINT_SCHEMA = B.TABLE_SCHEMA) WHERE B.CONSTRAINT_TYPE = 'FOREIGN KEY' AND
+    // A.TABLE_SCHEMA = 'demo_db' AND A.TABLE_NAME='demo_table1' AND A.REFERENCED_TABLE_SCHEMA IS
+    // NOT NULL ORDER BY A.REFERENCED_TABLE_SCHEMA, A.REFERENCED_TABLE_NAME, A.ORDINAL_POSITION
+    pattern =
+        "(?i)SELECT DISTINCT .+\\.REFERENCED_TABLE_SCHEMA AS PKTABLE_CAT,NULL AS PKTABLE_SCHEM, .+\\.REFERENCED_TABLE_NAME AS PKTABLE_NAME, .+\\.REFERENCED_COLUMN_NAME.*\\s+FROM INFORMATION_SCHEMA\\.KEY_COLUMN_USAGE.*";
+    // PKTABLE_CAT PKTABLE_SCHEM	PKTABLE_NAME	PKCOLUMN_NAME	FKTABLE_CAT	FKTABLE_SCHEM	FKTABLE_NAME
+    //	FKCOLUMN_NAME	KEY_SEQ	UPDATE_RULE	DELETE_RULE	FK_NAME	PK_NAME	DEFERRABILITY
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("PKTABLE_CAT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PKTABLE_SCHEM", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PKTABLE_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PKCOLUMN_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("FKTABLE_CAT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("FKTABLE_SCHEM", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("FKTABLE_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("FKCOLUMN_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("KEY_SEQ", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("UPDATE_RULE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("DELETE_RULE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("FK_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PK_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("DEFERRABILITY", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    mockPatternResults.put(pattern, new Pair<>(columns, rows));
+
+    // SELECT cc.CONSTRAINT_NAME, cc.CHECK_CLAUSE, tc.TABLE_NAME
+    // FROM (SELECT CONSTRAINT_NAME, CHECK_CLAUSE
+    // FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS
+    // WHERE CONSTRAINT_SCHEMA = 'xzs'
+    // ORDER BY CONSTRAINT_NAME) cc,
+    // (SELECT TABLE_NAME, CONSTRAINT_NAME
+    // FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    // WHERE TABLE_SCHEMA = 'xzs' AND TABLE_NAME='t_exam_paper') tc
+    // WHERE cc.CONSTRAINT_NAME = tc.CONSTRAINT_NAME
+    // ORDER BY cc.CONSTRAINT_NAME
+    pattern =
+        "(?i)(?s)SELECT .*CONSTRAINT_NAME,.*CHECK_CLAUSE,.*TABLE_NAME.*FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS.*FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS.*";
+    // CONSTRAINT_NAME	CHECK_CLAUSE	TABLE_NAME
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("CONSTRAINT_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("CHECK_CLAUSE", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("TABLE_NAME", "VARCHAR(255)"));
     rows = new ArrayList<>();
     mockPatternResults.put(pattern, new Pair<>(columns, rows));
   }
