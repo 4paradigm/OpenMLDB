d44cfe10f7ec47dfb7f531a10bc4c7004cd5852f
oh2024 <oh2024@yeah.net>
2024-05-07 07:28:25 +0000

    fix: use FLAGS_system_table_replica_num for user table

--- src/nameserver/name_server_impl.cc
+++ src/nameserver/name_server_impl.cc
@@ -5592,13 +5592,7 @@ void NameServerImpl::OnLocked() {
     }
     CreateDatabaseOrExit(INTERNAL_DB);
     if (db_table_info_[INTERNAL_DB].count(USER_INFO_NAME) == 0) {
-        auto temp = FLAGS_system_table_replica_num;
-        FLAGS_system_table_replica_num = 1;
-        if (tablets_.size() > 1) {
-            FLAGS_system_table_replica_num = 2;
-        }
         CreateSystemTableOrExit(SystemTableType::kUser);
-        FLAGS_system_table_replica_num = temp;
         InsertUserRecord("%", "root", "1e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
     }
     if (IsClusterMode()) {
