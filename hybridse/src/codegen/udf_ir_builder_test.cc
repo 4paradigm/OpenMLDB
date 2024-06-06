a69ebb0d66b33d8c6cace1c4dceb20bee3d893eb
oh2024 <162292688+oh2024@users.noreply.github.com>
2024-06-06 12:04:15 +0800

    fix: handle repeated sort keys and sort as int (#3947)

--- hybridse/src/codegen/udf_ir_builder_test.cc
+++ hybridse/src/codegen/udf_ir_builder_test.cc
@@ -1199,6 +1199,29 @@ TEST_F(UdfIRBuilderTest, CustUdfs) {
                                                                       false);
 }
 
+TEST_F(UdfIRBuilderTest, JsonArraySortAsInt) {
+    openmldb::base::StringRef json = R"([{"a": "6", "b": "2"}, {"a": "11", "b": "9"}])";
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "9,2", json, "a", "b", 10,
+                                                                        true);
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "2,9", json, "a", "b", 10,
+                                                                        false);
+}
+
+TEST_F(UdfIRBuilderTest, JsonArraySortRepeatedKey) {
+    openmldb::base::StringRef json = R"([{"a": "6", "b": "a"}, {"a": "11", "b": "aaa"}, {"a": "6", "b": "aa"}])";
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "aaa,aa,a", json, "a", "b",
+                                                                        10, true);
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "a,aa,aaa", json, "a", "b",
+                                                                        10, false);
+}
+
+TEST_F(UdfIRBuilderTest, JsonArraySortInvalidKey) {
+    openmldb::base::StringRef json = R"([{"a": "-1", "b": "1"}, {"a": "a", "b": "2"}, {"a": "11", "b": "9"}])";
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "9,2,1", json, "a", "b", 10,
+                                                                        true);
+    CheckUdf<StringRef, StringRef, StringRef, StringRef, int32_t, bool>("json_array_sort", "1,2,9", json, "a", "b", 10,
+                                                                        false);
+}
 }
 }  // namespace codegen
 
