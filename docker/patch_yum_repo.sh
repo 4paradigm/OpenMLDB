60d3ed5fac40095d9341532d88deb05b882a6dab
aceforeverd <teapot@aceforeverd.com>
2024-09-12 04:06:29 +0000

    ci: fix docker image building on arm

--- docker/patch_yum_repo.sh
+++ docker/patch_yum_repo.sh
@@ -6,6 +6,6 @@ sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
 sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
 sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
 
-if [[ "$ARCH" = "aarch64" ]]; then
+if [[ "$(arch)" = "aarch64" ]]; then
     sed -i s/vault.centos.org\\/centos/vault.centos.org\\/altarch/g /etc/yum.repos.d/*.repo
 fi
